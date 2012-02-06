6f71cc1b4bbaaf56865c60a928875451cd898d68
Thomas Woerner <twoerner@redhat.com>
2012-02-06 11:45:04 +0100

    - added fw_icmptype and fw_service providing the FirewallIcmpType and FireallService classes

---
+++ src/firewall/core/fw_icmptype.py
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2011-2012 Red Hat, Inc.
+# Authors:
+# Thomas Woerner <twoerner@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from firewall.errors import *
+
+class FirewallIcmpType:
+    def __init__(self, fw):
+        self._fw = fw
+        self.__init_vars()
+
+    def __init_vars(self):
+        self._icmptypes = { }
+
+    def cleanup(self):
+        self.__init_vars()
+
+    # zones
+
+    def get_icmptypes(self):
+        return self._icmptypes.keys()
+
+    def check_icmptype(self, icmptype):
+        if icmptype not in self._icmptypes:
+            FirewallError(INVALID_ICMPTYPE)
+
+    def get_icmptype(self, icmptype):
+        self.check_icmptype(icmptype)
+        return self._icmptypes[icmptype]
+
+    def add_icmptype(self, obj):
+        self._icmptypes[obj.name] = obj
+
+    def remove_icmptype(self, icmptype):
+        obj = self._icmptypes[icmptype]
+        del self._icmptypes[icmptype]
---
+++ src/firewall/core/fw_service.py
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2011-2012 Red Hat, Inc.
+# Authors:
+# Thomas Woerner <twoerner@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from firewall.errors import *
+
+class FirewallService:
+    def __init__(self, fw):
+        self._fw = fw
+        self.__init_vars()
+
+    def __init_vars(self):
+        self._services = { }
+
+    def cleanup(self):
+        self.__init_vars()
+
+    # zones
+
+    def get_services(self):
+        return self._services.keys()
+
+    def check_service(self, service):
+        if service not in self._services:
+            FirewallError(INVALID_SERVICE)
+
+    def get_service(self, service):
+        self.check_service(service)
+        return self._services[service]
+
+    def add_service(self, obj):
+        self._services[obj.name] = obj
+
+    def remove_service(self, service):
+        obj = self._services[service]
+        del self._services[service]
