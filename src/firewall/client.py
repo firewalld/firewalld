59bac3fe2afee9028685eeb35ccba58b453dbb11
Thomas Woerner <twoerner@redhat.com>
2012-02-06 15:20:05 +0100

    - adapted FirewallClient class, renamed from Firewall_Client - code cleanup

--- src/firewall/client.py
+++ src/firewall/client.py
@@ -19,22 +19,15 @@
 
 import dbus.mainloop.glib
 import slip.dbus
+from dbus.exceptions import DBusException
+from firewall.config import *
+from firewall.config.dbus import *
+from firewall.dbus_utils import dbus_to_python
 import dbus
-import pickle
 
 #
 
-#INTERFACE_VERSION = "2.0"
-
-#
-
-DBUS_INTERFACE = "org.fedoraproject.FirewallD"
-DBUS_PATH = "/org/fedoraproject/FirewallD"
-POLKIT_DOMAIN = "org.fedoraproject.FirewallD"
-
-#
-
-class Firewall_Client(object):
+class FirewallClient(object):
     def __init__(self, bus=None):
         if not bus:
             try:
@@ -47,207 +40,185 @@ class Firewall_Client(object):
             self.bus = bus
         self.dbus_obj = self.bus.get_object(DBUS_INTERFACE, DBUS_PATH)
         self.fw = dbus.Interface(self.dbus_obj, dbus_interface=DBUS_INTERFACE)
+        self.fw_zone = dbus.Interface(self.dbus_obj,
+                                      dbus_interface=DBUS_INTERFACE_ZONE)
+        self.fw_direct = dbus.Interface(self.dbus_obj,
+                                        dbus_interface=DBUS_INTERFACE_DIRECT)
+        self.fw_properties = dbus.Interface(
+            self.dbus_obj, dbus_interface='org.freedesktop.DBus.Properties')
 
     @slip.dbus.polkit.enable_proxy
     def reload(self):
-        return self.fw.reload()
+        self.fw.reload()
 
     @slip.dbus.polkit.enable_proxy
-    def restart(self):
-        return self.fw.restart()
+    def complete_reload(self):
+        self.fw.completeReload()
 
     @slip.dbus.polkit.enable_proxy
-    def status(self):
-        return self.fw.status()
+    def get_property(self, prop):
+        return self.fw_properties.Get(DBUS_INTERFACE, prop)
+
+    @slip.dbus.polkit.enable_proxy
+    def get_properties(self, prop):
+        return self.fw_properties.GetAll(DBUS_INTERFACE)
+
+    @slip.dbus.polkit.enable_proxy
+    def set_property(self, prop, value):
+        return self.fw_properties.Set(DBUS_INTERFACE, prop, value)
 
     # panic mode
 
     @slip.dbus.polkit.enable_proxy
     def enablePanicMode(self):
-        return self.fw.enablePanicMode()
+        self.fw.enablePanicMode()
     
     @slip.dbus.polkit.enable_proxy
     def disablePanicMode(self):
-        return self.fw.disablePanicMode()
+        self.fw.disablePanicMode()
 
     @slip.dbus.polkit.enable_proxy
     def queryPanicMode(self):
-        return self.fw.queryPanicMode()
+        self.fw.queryPanicMode()
+
+    # default zone
+
+    @slip.dbus.polkit.enable_proxy
+    def getDefaultZone(self):
+        return dbus_to_python(self.fw.getDefaultZone())
+
+    @slip.dbus.polkit.enable_proxy
+    def setDefaultZone(self, zone):
+        return dbus_to_python(self.fw.setDefaultZone(zone))
+
+    # zone
+
+    @slip.dbus.polkit.enable_proxy
+    def getZones(self):
+        return dbus_to_python(self.fw_zone.getZones())
+
+    @slip.dbus.polkit.enable_proxy
+    def getActiveZones(self):
+        return dbus_to_python(self.fw_zone.getActiveZones())
+
+    @slip.dbus.polkit.enable_proxy
+    def getZoneOfInterface(self, interface):
+        return dbus_to_python(self.fw_zone.getZoneOfInterface(interface))
+
+    # interfaces
+
+    @slip.dbus.polkit.enable_proxy
+    def addInterface(self, zone, interface):
+        return dbus_to_python(self.fw_zone.addInterface(zone, interface))
+
+    @slip.dbus.polkit.enable_proxy
+    def getInterfaces(self, zone):
+        return dbu_to_python(self.fw_zone.getInterfaces(zone))
+
+    @slip.dbus.polkit.enable_proxy
+    def queryInterface(self, interface):
+        return dbus_to_python(self.fw_zone.queryInterface(zone, interface))
+
+    @slip.dbus.polkit.enable_proxy
+    def removeInterface(self, zone, interface):
+        return dbus_to_python(self.fw_zone.removeInterface(zone, interface))
 
     # services
 
     @slip.dbus.polkit.enable_proxy
-    def enableService(self, service, timeout=0):
-        return self.fw.enableService(service, timeout)
+    def addService(self, zone, service, timeout=0):
+        return dbus_to_python(self.fw_zone.addService(zone, service, timeout))
 
     @slip.dbus.polkit.enable_proxy
-    def getServices(self):
-        (status, services) = self.fw.getServices()
-        if status < 0:
-            return None
-        return [ str(item) for item in services ]
+    def getServices(self, zone):
+        return dbus_to_python(self.fw_zone.getServices(zone))
 
     @slip.dbus.polkit.enable_proxy
-    def queryService(self, service):
-        return self.fw.queryService(service)
+    def queryService(self, zone, service):
+        return dbus_to_python(self.fw_zone.queryService(zone, service))
 
     @slip.dbus.polkit.enable_proxy
-    def disableService(self, service):
-        return self.fw.disableService(service)
+    def removeService(self, zone, service):
+        return dbus_to_python(self.fw_zone.removeService(zone, service))
 
+"""
     # ports
 
     @slip.dbus.polkit.enable_proxy
     def enablePort(self, port, protocol, timeout=0):
-        return self.fw.enablePort(port, protocol, timeout)
+        return dbus_to_python(self._zone.enablePort(port, protocol, timeout))
 
     @slip.dbus.polkit.enable_proxy
     def getPorts(self):
-        (status, ports) = self.fw.getPorts()
-        if status < 0:
-            return None
-        return [ (str(port), str(proto)) for (port, proto) in ports ]
+        return dbus_to_python(self._zone.getPorts())
 
     @slip.dbus.polkit.enable_proxy
     def queryPort(self, port, protocol):
-        return self.fw.queryPort(port, protocol)
+        return dbus_to_python(self._zone.queryPort(port, protocol))
 
     @slip.dbus.polkit.enable_proxy
     def disablePort(self, port, protocol):
-        return self.fw.disablePort(port, protocol)
-
-    # trusted
-
-    @slip.dbus.polkit.enable_proxy
-    def enableTrusted(self, trusted, timeout=0):
-        return self.fw.enableTrusted(trusted, timeout)
-
-    @slip.dbus.polkit.enable_proxy
-    def getTrusted(self):
-        (status, trusted) =  self.fw.getTrusted()
-        if status < 0:
-            return None
-        return [ str(item) for item in trusted ]
-
-    @slip.dbus.polkit.enable_proxy
-    def queryTrusted(self, trusted):
-        return self.fw.queryTrusted(trusted)
-
-    @slip.dbus.polkit.enable_proxy
-    def disableTrusted(self, trusted):
-        return self.fw.disableTrusted(trusted)
+        return dbus_to_python(self._zone.disablePort(port, protocol))
 
     # masquerade
 
     @slip.dbus.polkit.enable_proxy
-    def enableMasquerade(self, masquerade, timeout=0):
-        return self.fw.enableMasquerade(masquerade, timeout)
+    def enableMasquerade(self, timeout=0):
+        return dbus_to_python(self._zone.enableMasquerade(timeout))
 
     @slip.dbus.polkit.enable_proxy
-    def getMasquerades(self):
-        (status, masqueraded) = self.fw.getMasquerades()
-        if status < 0:
-            return None
-        return [ str(item) for item in masqueraded ]
+    def queryMasquerade(self):
+        return dbus_to_python(self._zone.queryMasquerade())
 
     @slip.dbus.polkit.enable_proxy
-    def queryMasquerade(self, masquerade):
-        return self.fw.queryMasquerade(masquerade)
-
-    @slip.dbus.polkit.enable_proxy
-    def disableMasquerade(self, masquerade):
-        return self.fw.disableMasquerade(masquerade)
+    def disableMasquerade(self):
+        return dbus_to_python(self._zone.disableMasquerade())
 
     # forward ports
 
     @slip.dbus.polkit.enable_proxy
-    def enableForwardPort(self, interface, port, protocol, toport, toaddr,
-                          timeout=0):
-        return self.fw.enableForwardPort(interface, port, protocol, toport,
-                                         toaddr, timeout)
+    def enableForwardPort(self, port, protocol, toport, toaddr, timeout=0):
+        if not toport:
+            toport = ""
+        if not toaddr:
+            toaddr = ""
+        return dbus_to_python(self._zone.enableForwardPort(port, protocol,
+                                                           toport, toaddr,
+                                                           timeout))
 
     @slip.dbus.polkit.enable_proxy
     def getForwardPorts(self):
-        (status, ports) = self.fw.getForwardPorts()
-        if status < 0:
-            return None
-        return [ (str(interface), str(port), str(protocol), str(toport),
-                  str(toaddr)) 
-                 for (interface, port, protocol, toport, toaddr) in ports ]
+        return dbus_to_python(self._zone.getForwardPorts())
 
     @slip.dbus.polkit.enable_proxy
-    def queryForwardPort(self, interface, port, protocol, toport, toaddr):
-        return self.fw.queryForwardPort(interface, port, protocol, toport,
-                                        toaddr)
+    def queryForwardPort(self, port, protocol, toport, toaddr):
+        return dbus_to_python(self._zone.queryForwardPort(port, protocol,
+                                                          toport, toaddr))
 
     @slip.dbus.polkit.enable_proxy
-    def disableForwardPort(self, interface, port, protocol, toport, toaddr):
-        return self.fw.disableForwardPort(interface, port, protocol, toport,
-                                          toaddr)
+    def disableForwardPort(self, port, protocol, toport, toaddr):
+        if not toport:
+            toport = ""
+        if not toaddr:
+            toaddr = ""
+        return dbus_to_python(self._zone.disableForwardPort(port, protocol,
+                                                            toport, toaddr))
 
     # icmpblock
 
     @slip.dbus.polkit.enable_proxy
     def enableIcmpBlock(self, icmp, timeout=0):
-        return self.fw.enableIcmpBlock(icmp, timeout)
+        return dbus_to_python(self._zone.enableIcmpBlock(icmp, timeout))
 
     @slip.dbus.polkit.enable_proxy
     def getIcmpBlocks(self):
-        (status, icmp_blocks) = self.fw.getIcmpBlocks()
-        if status < 0:
-            return None
-        return icmp_blocks
+        return dbus_to_python(self._zone.getIcmpBlocks())
 
     @slip.dbus.polkit.enable_proxy
     def queryIcmpBlock(self, icmp):
-        return self.fw.queryIcmpBlock(icmp)
+        return dbus_to_python(self._zone.queryIcmpBlock(icmp))
 
     @slip.dbus.polkit.enable_proxy
     def disableIcmpBlock(self, icmp):
-        return self.fw.disableIcmpBlock(icmp)
-
-    ##############################
-    # custom
-
-    @slip.dbus.polkit.enable_proxy
-    def enableCustom(self, table="filter", chain="INPUT", src=None,
-                     src_port=None, dst=None, dst_port=None,
-                     protocol=None, iface_in=None, out_if=None,
-                     physdev_in=None, physdev_out=None, target="ACCEPT",
-                     timeout=0):
-        return self.fw.enableCustom(
-            table, chain, src, src_port, dst, dst_port, protocol,
-            iface_in, out_if, physdev_in, physdev_out, target, timeout)
-
-    @slip.dbus.polkit.enable_proxy
-    def getCustoms(self):
-        (status, customs) = self.fw.getCustoms()
-        if status < 0:
-            return None
-        return [ (str(table), str(chain), str(src), str(src_port), str(dst),
-                  str(dst_port), str(protocol), str(iface_in), str(out_if),
-                  str(physdev_in), str(physdev_out), str(target)) 
-                 for (table, chain, src, src_port, dst, dst_port, protocol,
-                      iface_in, out_if, physdev_in, physdev_out, target)
-                 in customs ]
-
-    @slip.dbus.polkit.enable_proxy
-    def queryCustom(self,  table="filter", chain="INPUT", src=None,
-                    src_port=None, dst=None, dst_port=None,
-                    protocol=None, iface_in=None, out_if=None,
-                    physdev_in=None, physdev_out=None, target="ACCEPT",
-                    timeout=0):
-        return self.fw.queryCustom(table, chain, src, src_port,
-                                   dst, dst_port, protocol, iface_in, out_if,
-                                   physdev_in, physdev_out, target)
-
-    @slip.dbus.polkit.enable_proxy
-    def disableCustom(self, table="filter", chain="INPUT", src=None,
-                      src_port=None, dst=None, dst_port=None,
-                      protocol=None, iface_in=None, out_if=None,
-                      physdev_in=None, physdev_out=None, target="ACCEPT",
-                      timeout=0):
-        return self.fw.disableCustom(table, chain, src, src_port,
-                                     dst, dst_port, protocol,
-                                     iface_in, out_if,
-                                     physdev_in, physdev_out, target)
+        return dbus_to_python(self._zone.disableIcmpBlock(icmp))
+"""
