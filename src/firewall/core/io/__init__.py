92022dc3634efaad96b4feb9f5f7dddfbb0677ef
Thomas Woerner <twoerner@redhat.com>
2012-03-03 14:30:06 +0100

    - fix xmlplus to be compatible with the python xml sax parser and python 3   by adding __contains__ to xml.sax.xmlreader.AttributesImpl

---
+++ src/firewall/core/io/__init__.py
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2012 Red Hat, Inc.
+#
+# Authors:
+# Thomas Woerner <twoerner@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# fix xmlplus to be compatible with the python xml sax parser and python 3
+# by adding __contains__ to xml.sax.xmlreader.AttributesImpl
+import xml
+if "_xmlplus" in xml.__file__:
+    from xml.sax.xmlreader import AttributesImpl
+    if not hasattr(AttributesImpl, "__contains__"):
+        # this is missing:
+        def __AttributesImpl__contains__(self, name):
+            return name in self._attrs
+        # add it using the name __contains__
+        setattr(AttributesImpl, "__contains__", __AttributesImpl__contains__)
