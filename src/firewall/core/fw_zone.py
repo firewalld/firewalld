firewalld/firewalld

commit cc3101ab70a3997228be7bc9f45a069c7fccfa36

removeInterface() don't require zone
Because interface can always belong only to
one zone it's too strict to require the
zone to be specified. The client also usually
don't care which zone the interface has been
part of, it just wants to tell firewall that
the interface is no longer running.

When removeInterface() is called with empty zone
the interface is now removed from zone it belongs to.

src/firewall/core/fw_zone.py
@@ -334,12 +334,12 @@ class FirewallZone:
 
     def remove_interface(self, zone, interface):
         self._fw.check_panic()
-        _zone = self._fw.check_zone(zone)
+        zoi = self.get_zone_of_interface(interface)
+        _zone = zoi if zone == "" else self._fw.check_zone(zone)
         _obj = self._zones[_zone]
 
         interface_id = self.__interface_id(interface)
         if interface_id not in _obj.interfaces:
-            zoi = self.get_zone_of_interface(interface)
             if zoi == None:
                 raise FirewallError(UNKNOWN_INTERFACE)
             if zoi != _zone:

src/firewall/server/firewalld.py
@@ -313,7 +313,7 @@ class FirewallD(dbus.service.Object):
         :Parameters:
             `interface` : str
                 Name of the interface
-        :Retruns: str. The name of the zone.
+        :Returns: str. The name of the zone.
         """
         log.debug1("zone.getZoneOfInterface('%s')" % interface)
         zone = self.fw.zone.get_zone_of_interface(interface)
@@ -361,7 +361,7 @@ class FirewallD(dbus.service.Object):
     @dbus_handle_exceptions
     def removeInterface(self, zone, interface, sender=None):
         """Remove interface from a zone.
-        If zone is empty, use default zone.
+        If zone is empty, remove from zone the interface belongs to.
         """
         interface = str(interface)
         log.debug1("zone.removeInterface('%s', '%s')" % (zone, interface))

commit dd1fc33eb3a8cbeed32a9c7e64a08594e6242348

Fix regression introduced in commit cc3101a
Calling remove_interface() with empty zone while
the interface had been in no zone resulted in KeyError.

src/firewall/core/fw_zone.py
@@ -296,16 +296,14 @@ class FirewallZone:
     def remove_interface(self, zone, interface):
         self._fw.check_panic()
         zoi = self.get_zone_of_interface(interface)
+        if zoi == None:
+            raise FirewallError(UNKNOWN_INTERFACE, interface)
         _zone = zoi if zone == "" else self._fw.check_zone(zone)
+        if zoi != _zone:
+            raise FirewallError(ZONE_CONFLICT)
+
         _obj = self._zones[_zone]
-
         interface_id = self.__interface_id(interface)
-        if interface_id not in _obj.settings["interfaces"]:
-            if zoi == None:
-                raise FirewallError(UNKNOWN_INTERFACE, interface)
-            if zoi != _zone:
-                raise FirewallError(ZONE_CONFLICT)
-
         self.__interface(False, _zone, interface)
 
         if interface_id in _obj.settings["interfaces"]: