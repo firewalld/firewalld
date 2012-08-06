f9e14ad76ddb8676a7cf7cb6951247b69b68d697
Thomas Woerner <twoerner@redhat.com>
2012-08-06 13:17:47 +0200

    Extension of FirewallClient to support config interface
    New classes:
      FirewallClientConfig
      FirewallClientConfigZone
      FirewallClientConfigService
      FirewallClientConfigIcmpType
      FirewallClientConfigZoneSettings
      FirewallClientConfigServiceSettings
      FirewallClientConfigIcmpTypeSettings
    
    Example:
    
    fw = FirewallClient()
    for zone in fw.config().listZones():
        print(zone)
        z = fw.config().getZone(zone)
        settings = z.getSettings()
        print(" %s %s" % (z.get_property("name"), settings.getShort()))
        settings.addService("mdns")
        z.update(settings)
    
    Use fw.config() to access the config interface. The settings classes are
    providing a simple interface to add and remove settings. Use the settings
    classes in the update functions.
    
    All classes provides the same methods as the D-BUS interfaces, also callback
    handling for the signals.

--- src/firewall/client.py
+++ src/firewall/client.py
@@ -25,6 +25,445 @@ from firewall.config.dbus import *
 from firewall.dbus_utils import dbus_to_python
 import dbus
 
+# zone config setings
+
+class FirewallClientConfigZoneSettings(object):
+    def __init__(self, settings):
+        self.settings = settings
+
+    def getVersion(self):
+        return self.settings[0]
+    def setVersion(self, version):
+        self.settings[0] = version
+
+    def getShort(self):
+        return self.settings[1]
+    def setShort(self, short):
+        self.settings[1] = short
+
+    def getDescription(self):
+        return self.settings[2]
+    def setDescription(self, description):
+        self.settings[2] = description
+
+    def getImmutable(self):
+        return self.settings[3]
+    def setImmutable(self, immutable):
+        self.settings[3] = immutable
+
+    def getTarget(self):
+        return self.settings[4]
+    def setTarget(self, target):
+        self.settings[4] = target
+
+    def getServices(self):
+        return self.settings[5]
+    def setServices(self, services):
+        self.settings[5] = services
+    def addService(self, service):
+        if service not in self.settings[5]:
+            self.settings[5].append(service)
+    def removeService(self, service):
+        if service in self.settings[5]:
+            self.settings[5].remove(service)
+
+    def getPorts(self):
+        return self.settings[6]
+    def setPorts(self, ports):
+        self.settings[6] = ports
+    def addPort(self, port, protocol):
+        if (port,protocol) not in self.settings[6]:
+            self.settings[6].append((port,protocol))
+    def removePort(self, port, protocol):
+        if (port,protocol) in self.settings[6]:
+            self.settings[6].remove((port,protocol))
+
+    def getIcmpBlocks(self):
+        return self.settings[7]
+    def setIcmpBlocks(self, icmpblocks):
+        self.settings[7] = icmpblocks
+    def addIcmBlock(self, icmptype):
+        if icmptype not in self.settings[7]:
+            self.settings[7].append(icmptype)
+    def removeIcmBlock(self, icmptype):
+        if icmptype in self.settings[7]:
+            self.settings[7].remove(icmptype)
+
+    def getMasquerade(self):
+        return self.settings[8]
+    def setMasquerade(self, masquerade):
+        self.settings[8] = masqerade
+
+    def getForwardPorts(self):
+        return self.settings[9]
+    def setForwardPorts(self, ports):
+        self.settings[9] = ports
+    def addForwardPort(self, port, protocol, to_port, to_addr):
+        if (port,protocol,to_port,to_addr) not in self.settings[9]:
+            self.settings[9].append((port,protocol,to_port,to_addr))
+    def removeForwardPort(self, port, protocol):
+        if (port,protocol,to_port,to_addr) in self.settings[9]:
+            self.settings[9].remove((port,protocol,to_port,to_addr))
+
+# zone config
+
+class FirewallClientConfigZone(object):
+    def __init__(self, bus, path):
+        self.bus = bus
+        self.path = path
+        self.dbus_obj = self.bus.get_object(DBUS_INTERFACE, path)
+        self.fw_zone = dbus.Interface(self.dbus_obj,
+                                      dbus_interface=DBUS_INTERFACE_CONFIG)
+        self.fw_properties = dbus.Interface(
+            self.dbus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+        self._updated_cb = None
+        self._removed_cb = None
+        self._renamed_cb = None
+
+    @slip.dbus.polkit.enable_proxy
+    def get_property(self, prop):
+        return dbus_to_python(self.fw_properties.Get(DBUS_INTERFACE_CONFIG,
+                                                     prop))
+
+    @slip.dbus.polkit.enable_proxy
+    def get_properties(self):
+        return dbus_to_python(self.fw_properties.GetAll(DBUS_INTERFACE_CONFIG))
+
+    @slip.dbus.polkit.enable_proxy
+    def set_property(self, prop, value):
+        self.fw_properties.Set(DBUS_INTERFACE_CONFIG, prop, value)
+
+    @slip.dbus.polkit.enable_proxy
+    def getSettings(self):
+        return FirewallClientConfigZoneSettings(dbus_to_python(\
+                self.fw_zone.getSettings()))
+
+    @slip.dbus.polkit.enable_proxy
+    def update(self, settings):
+        self.fw_zone.update(settings.settings)
+
+    @slip.dbus.polkit.enable_proxy
+    def loadDefaults(self):
+        self.fw_zone.loadDefaults()
+
+    @slip.dbus.polkit.enable_proxy
+    def remove(self):
+        self.fw_zone.remove()
+
+    @slip.dbus.polkit.enable_proxy
+    def rename(self, name):
+        self.fw_zone.rename(name)
+
+    @slip.dbus.polkit.enable_proxy
+    def setUpdatedCallback(self, callback):
+        self._updated_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRemovedCallback(self, callback):
+        self._removed_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRenamedCallback(self, callback):
+        self._renamed_cb = callback
+
+# service config settings
+
+class FirewallClientConfigServiceSettings(object):
+    def __init__(self, settings):
+        self.settings = settings
+
+    def getVersion(self):
+        return self.settings[0]
+    def setVersion(self, version):
+        self.settings[0] = version
+
+    def getShort(self):
+        return self.settings[1]
+    def setShort(self, short):
+        self.settings[1] = short
+
+    def getDescription(self):
+        return self.settings[2]
+    def setDescription(self, description):
+        self.settings[2] = description
+
+    def getPorts(self):
+        return self.settings[3]
+    def setPorts(self, ports):
+        self.settings[3] = ports
+    def addPort(self, port, protocol):
+        if (port,protocol) not in self.settings[3]:
+            self.settings[3].append((port,protocol))
+    def removePort(self, port, protocol):
+        if (port,protocol) in self.settings[3]:
+            self.settings[3].remove((port,protocol))
+
+    def getModules(self):
+        return self.settings[4]
+    def setModules(self, modules):
+        self.settings[4] = modules
+    def addModule(self, module):
+        if module not in self.settings[4]:
+            self.settings[4].append(module)
+    def removeModule(self, module):
+        if module in self.settings[4]:
+            self.settings[4].remove(module)
+
+    def getDestinations(self):
+        return self.settings[5]
+    def setDestinations(self, destinations):
+        self.settings[5] = destinations
+    def setDestination(self, dest_type, address):
+        self.settings[5][dest_type] = address
+    def removeModule(self, module):
+        if dest_type in self.settings[5]:
+            del self.settings[5][dest_type]
+
+# service config
+
+class FirewallClientConfigService(object):
+    def __init__(self, bus, path):
+        self.bus = bus
+        self.path = path
+        self.dbus_obj = self.bus.get_object(DBUS_INTERFACE, path)
+        self.fw_service = dbus.Interface(self.dbus_obj,
+                                         dbus_interface=DBUS_INTERFACE_CONFIG)
+        self.fw_properties = dbus.Interface(
+            self.dbus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+        self._updated_cb = None
+        self._removed_cb = None
+        self._renamed_cb = None
+
+    @slip.dbus.polkit.enable_proxy
+    def get_property(self, prop):
+        return dbus_to_python(self.fw_properties.Get(DBUS_INTERFACE_CONFIG,
+                                                     prop))
+
+    @slip.dbus.polkit.enable_proxy
+    def get_properties(self):
+        return dbus_to_python(self.fw_properties.GetAll(DBUS_INTERFACE_CONFIG))
+
+    @slip.dbus.polkit.enable_proxy
+    def set_property(self, prop, value):
+        self.fw_properties.Set(DBUS_INTERFACE_CONFIG, prop, value)
+
+    @slip.dbus.polkit.enable_proxy
+    def getSettings(self):
+        return FirewallClientConfigServiceSettings(dbus_to_python(\
+                self.fw_service.getSettings()))
+
+    @slip.dbus.polkit.enable_proxy
+    def update(self, settings):
+        self.fw_service.update(settings.settings)
+
+    @slip.dbus.polkit.enable_proxy
+    def loadDefaults(self):
+        self.fw_service.loadDefaults()
+
+    @slip.dbus.polkit.enable_proxy
+    def remove(self):
+        self.fw_service.remove()
+
+    @slip.dbus.polkit.enable_proxy
+    def rename(self, name):
+        self.fw_service.rename(name)
+
+    @slip.dbus.polkit.enable_proxy
+    def setUpdatedCallback(self, callback):
+        self._updated_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRemovedCallback(self, callback):
+        self._removed_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRenamedCallback(self, callback):
+        self._renamed_cb = callback
+
+# icmptype config settings
+
+class FirewallClientConfigIcmpTypeSettings(object):
+    def __init__(self, settings):
+        self.settings = settings
+
+    def getVersion(self):
+        return self.settings[0]
+    def setVersion(self, version):
+        self.settings[0] = version
+
+    def getShort(self):
+        return self.settings[1]
+    def setShort(self, short):
+        self.settings[1] = short
+
+    def getDescription(self):
+        return self.settings[2]
+    def setDescription(self, description):
+        self.settings[2] = description
+
+    def getDestinations(self):
+        return self.settings[3]
+    def setDestinations(self, destinations):
+        self.settings[3] = destinations
+    def addDestination(self, destination):
+        if destination not in self.settings[3]:
+            self.settings[3].append(destination)
+    def removeDestination(self, destination):
+        if destination in self.settings[3]:
+            self.settings[3].remove(destination)
+
+
+# icmptype config
+
+class FirewallClientConfigIcmpType(object):
+    def __init__(self, bus, path):
+        self.bus = bus
+        self.path = path
+        self.dbus_obj = self.bus.get_object(DBUS_INTERFACE, path)
+        self.fw_icmptype = dbus.Interface(self.dbus_obj,
+                                          dbus_interface=DBUS_INTERFACE_CONFIG)
+        self.fw_properties = dbus.Interface(
+            self.dbus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+        self._updated_cb = None
+        self._removed_cb = None
+        self._renamed_cb = None
+
+    @slip.dbus.polkit.enable_proxy
+    def get_property(self, prop):
+        return dbus_to_python(self.fw_properties.Get(DBUS_INTERFACE_CONFIG,
+                                                     prop))
+
+    @slip.dbus.polkit.enable_proxy
+    def get_properties(self):
+        return dbus_to_python(self.fw_properties.GetAll(DBUS_INTERFACE_CONFIG))
+
+    @slip.dbus.polkit.enable_proxy
+    def set_property(self, prop, value):
+        self.fw_properties.Set(DBUS_INTERFACE_CONFIG, prop, value)
+
+    @slip.dbus.polkit.enable_proxy
+    def getSettings(self):
+        return FirewallClientConfigIcmpTypeSettings(dbus_to_python(\
+                self.fw_icmptype.getSettings()))
+
+    @slip.dbus.polkit.enable_proxy
+    def update(self, settings):
+        self.fw_icmptype.update(settings.settings)
+
+    @slip.dbus.polkit.enable_proxy
+    def loadDefaults(self):
+        self.fw_icmptype.loadDefaults()
+
+    @slip.dbus.polkit.enable_proxy
+    def remove(self):
+        self.fw_icmptype.remove()
+
+    @slip.dbus.polkit.enable_proxy
+    def rename(self, name):
+        self.fw_icmptype.rename(name)
+
+    @slip.dbus.polkit.enable_proxy
+    def setUpdatedCallback(self, callback):
+        self._updated_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRemovedCallback(self, callback):
+        self._removed_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setRenamedCallback(self, callback):
+        self._renamed_cb = callback
+
+# config
+
+class FirewallClientConfig(object):
+    def __init__(self, bus):
+        self.bus = bus
+        self.dbus_obj = self.bus.get_object(DBUS_INTERFACE,
+                                            DBUS_PATH_CONFIG)
+        self.fw_config = dbus.Interface(self.dbus_obj,
+                                        dbus_interface=DBUS_INTERFACE_CONFIG)
+        self._zone_added_cb = None
+        self._service_added_cb = None
+        self._icmptype_added_cb = None
+
+    # zone
+
+    @slip.dbus.polkit.enable_proxy
+    def listZones(self):
+        return dbus_to_python(self.fw_config.listZones())
+
+    @slip.dbus.polkit.enable_proxy
+    def getZone(self, path):
+        z = FirewallClientConfigZone(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def getZoneByName(self, name):
+        path = dbus_to_python(self.fw_config.getZoneByName(name))
+        z = FirewallClientConfigZone(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def addZone(self, name, settings):
+        self.fw_config.addZone(name, settings)
+
+    # service
+
+    @slip.dbus.polkit.enable_proxy
+    def listServices(self):
+        return dbus_to_python(self.fw_config.listServices())
+
+    @slip.dbus.polkit.enable_proxy
+    def getService(self, path):
+        z = FirewallClientConfigService(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def getServiceByName(self, name):
+        path = dbus_to_python(self.fw_config.getServiceByName(name))
+        z = FirewallClientConfigService(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def addService(self, name, settings):
+        self.fw_config.addService(name, settings)
+
+    # icmptype
+
+    @slip.dbus.polkit.enable_proxy
+    def listIcmpTypes(self):
+        return dbus_to_python(self.fw_config.listIcmpTypes())
+
+    @slip.dbus.polkit.enable_proxy
+    def getIcmpType(self, path):
+        z = FirewallClientConfigIcmpType(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def getIcmpTypeByName(self, name):
+        path = dbus_to_python(self.fw_config.getIcmpTypeByName(name))
+        z = FirewallClientConfigIcmpType(self.bus, path)
+        return z
+
+    @slip.dbus.polkit.enable_proxy
+    def addIcmpType(self, name, settings):
+        self.fw_config.addIcmpType(name, settings)
+
+    # callbacks
+
+    @slip.dbus.polkit.enable_proxy
+    def setZoneAddedCallback(self, callback):
+        self._zone_added_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setServiceAddedCallback(self, callback):
+        self._service_added_cb = callback
+
+    @slip.dbus.polkit.enable_proxy
+    def setIcmpTypeAddedCallback(self, callback):
+        self._icmptype_added_cb = callback
+
 #
 
 class FirewallClient(object):
@@ -46,6 +485,11 @@ class FirewallClient(object):
                                         dbus_interface=DBUS_INTERFACE_DIRECT)
         self.fw_properties = dbus.Interface(
             self.dbus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+        self._config = FirewallClientConfig(self.bus)
+
+    @slip.dbus.polkit.enable_proxy
+    def config(self):
+        return self._config
 
     @slip.dbus.polkit.enable_proxy
     def reload(self):
@@ -60,7 +504,7 @@ class FirewallClient(object):
         return dbus_to_python(self.fw_properties.Get(DBUS_INTERFACE, prop))
 
     @slip.dbus.polkit.enable_proxy
-    def get_properties(self, prop):
+    def get_properties(self):
         return dbus_to_python(self.fw_properties.GetAll(DBUS_INTERFACE))
 
     @slip.dbus.polkit.enable_proxy
