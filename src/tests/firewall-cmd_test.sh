52c218619052deb44cc5250abab4e07cb7d80684
Jiri Popelka <jpopelka@redhat.com>
2013-02-07 14:05:03 +0100

    firewall-cmd_test.sh: various improvements
    - allow to test system as well as local (git repo) instance of firewall-cmd
    - better FAILED messages
    - add various advanced tests:
      - missing arguments
      - impossible combinations of options

--- src/tests/firewall-cmd_test.sh
+++ src/tests/firewall-cmd_test.sh
@@ -6,37 +6,38 @@
 # some tests modify default zone and will fail if default zone is immutable,
 # i.e. block, drop, trusted
 
-path="/usr/bin/firewall-cmd"
+#path="/usr/bin/"
+path="../"
 
 assert_good() {
   args="${1}"
-  firewall-cmd ${args} > /dev/null
+  ${path}firewall-cmd ${args} > /dev/null
   if [ $? == 0 ]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: zero exit status)"
+    echo "${args} ... FAILED (non-zero exit status)"
     exit 2
   fi
 }
 
 assert_good_notempty() {
   args="${1}"
-  ret=$(firewall-cmd ${args}) > /dev/null
+  ret=$(${path}firewall-cmd ${args}) > /dev/null
   if [ $? == 0 -a -n "${ret}" ]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: zero exit status and not-empty return value)"
+    echo "${args} ... FAILED (non-zero exit status or empty return value)"
     exit 2
   fi
 }
 
 assert_good_empty() {
   args="${1}"
-  ret=$(firewall-cmd ${args}) > /dev/null
+  ret=$(${path}firewall-cmd ${args}) > /dev/null
   if [ $? == 0 -a -z "${ret}" ]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: zero exit status and empty return value)"
+    echo "${args} ... FAILED (non-zero exit status or non-empty return value)"
     exit 2
   fi
 }
@@ -44,11 +45,11 @@ assert_good_empty() {
 assert_good_equals() {
   args="${1}"
   value="${2}"
-  ret=$(firewall-cmd ${args}) > /dev/null
+  ret=$(${path}firewall-cmd ${args}) > /dev/null
   if [ $? == 0 -a "${ret}" == "${value}" ]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: zero exit status and ${ret} == ${value})"
+    echo "${args} ... FAILED (non-zero exit status or '${ret}' != '${value}')"
     exit 2
   fi
 }
@@ -56,22 +57,22 @@ assert_good_equals() {
 assert_good_contains() {
   args="${1}"
   value="${2}"
-  ret=$(firewall-cmd ${args}) > /dev/null
+  ret=$(${path}firewall-cmd ${args}) > /dev/null
   if [[ ( $? == 0 ) && ( "${ret}" = *${value}* ) ]]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: zero exit status and ${ret} contains ${value})"
+    echo "${args} ... FAILED (non-zero exit status or '${ret}' does not contain '${value}')"
     exit 2
   fi
 }
 
 assert_bad() {
   args="${1}"
-  firewall-cmd ${args} > /dev/null
+  ${path}firewall-cmd ${args} 1> /dev/null 2>&1
   if [ $? != 0 ]; then
     echo "${args} ... OK"
   else
-    echo "${args} ... FAILED (expected: non-zero exit status)"
+    echo "${args} ... FAILED (zero exit status)"
     exit 2
   fi
 }
@@ -81,8 +82,8 @@ assert_bad() {
 assert_good "-h"
 assert_good "--help"
 assert_good "-V"
-#assert_good "--reload"
-#assert_good "--complete-reload"
+assert_good "--reload"
+assert_good "--complete-reload"
 assert_good "--enable-panic"
 assert_good "--query-panic"
 assert_good "--disable-panic"
@@ -93,6 +94,7 @@ assert_good_notempty "--get-default-zone"
 assert_good          "--set-default-zone=home"
 assert_good_equals   "--get-default-zone" "home"
 assert_good          "--set-default-zone=${old}"
+assert_bad           "--set-default-zone" # missing argument
 
 assert_good_notempty "--get-zones"
 assert_good_notempty "--get-services"
@@ -117,15 +119,22 @@ assert_good_contains "--zone=${zone} --list-interfaces" "${iface}"
 assert_good          "--zone=${zone} --remove-interface=${iface}"
 assert_bad           "--zone=${zone} --query-interface ${iface}"
 assert_good_empty    "--get-zone-of-interface=${iface}"
+assert_bad           "--get-zone-of-interface" # missing argument
+assert_bad           "--zone=${zone} --get-zones" # impossible combination
+assert_bad           "--zone=${zone} --get-services" # impossible combination
+assert_bad           "--zone=${zone} --get-default-zone" # impossible combination
+assert_bad           "--zone=${zone} --set-default-zone" # impossible combination
+assert_bad           "--zone=${zone} --get-zone-of-interface" # impossible combination
 
-
-assert_good "--add-service=dns --timeout 60"
+assert_good    "--add-service=dns --timeout 60"
 assert_good  "--query-service dns"
 assert_good "--remove-service=dns"
 assert_bad   "--query-service=dns"
+assert_bad     "--add-service=dns --timeout" # missing argument
+assert_bad     "--add-service=dns --add-interface=dummy0" # impossible combination
 
-assert_bad     "--add-port=666"
-assert_bad     "--add-port=666/dummy"
+assert_bad     "--add-port=666" # no protocol
+assert_bad     "--add-port=666/dummy" # bad protocol
 assert_good    "--add-port=666/tcp"
 assert_good "--remove-port=666/tcp"
 assert_good    "--add-port=111-222/udp"
@@ -144,10 +153,10 @@ assert_good "--zone=external --query-icmp-block=redirect"
 assert_good "--remove-icmp-block redirect --zone=external"
 assert_bad  "--zone=external --query-icmp-block=redirect"
 
-assert_bad  "--add-forward-port=666"
+assert_bad  "--add-forward-port=666" # no protocol
 assert_good    "--add-forward-port=port=11:proto=tcp:toport=22"
 assert_good "--remove-forward-port=port=11:proto=tcp:toport=22"
-assert_bad  "--add-forward-port=port=33:proto=tcp:toaddr=4444"
+assert_bad  "--add-forward-port=port=33:proto=tcp:toaddr=4444" # bad address
 assert_good    "--add-forward-port=port=33:proto=tcp:toaddr=4.4.4.4"
 assert_good "--remove-forward-port=port=33:proto=tcp:toaddr=4.4.4.4"
 assert_good    "--add-forward-port=port=55:proto=tcp:toport=66:toaddr=7.7.7.7"
@@ -170,9 +179,20 @@ assert_good          "--permanent --list-forward-ports"
 
 assert_good          "--permanent --zone=external --add-service=pop3s"
 assert_good_contains "--permanent --zone=external --list-services" "pop3s"
+assert_good          "--permanent --zone=external --remove-service=pop3s"
+assert_bad           "--permanent --zone=external --add-service=dns --timeout 10" # impossible combination
+assert_good          "--permanent --zone=external --add-port=123-456/tcp"
+assert_good          "--permanent --zone=external --remove-port=123-456/tcp"
+
+assert_bad           "--permanent --complete-reload" # impossible combination
+assert_bad           "--permanent --zone=work --add-interface=dummy0" # impossible combination
+assert_bad           "--permanent --add-interface=dummy0" # impossible combination
+assert_bad           "--permanent --list-all" # impossible combination
 
 # ... --direct  ...
 assert_good_contains "--direct --passthrough ipv4 -nvL" "IN_ZONE_home_allow"
+assert_bad           "--direct --passthrough ipv5 -nvL" # ipv5
+assert_bad           "--direct --passthrough ipv4" # missing argument
 assert_good_empty    "--direct --get-chains ipv4 filter"
 assert_good          "--direct --add-chain ipv4 filter mychain"
 assert_good_equals   "--direct --get-chains ipv4 filter" "mychain"
@@ -191,5 +211,15 @@ assert_bad           "--direct --query-chain ipv4 filter mychain"
 assert_good_empty    "--direct --get-chains ipv4 filter"
 assert_good          "--direct --remove-chain ipv4 filter dummy" # removing nonexisting chain is just warning
 
+assert_bad           "--direct --reload" # impossible combination
+assert_bad           "--direct --list-all" # impossible combination
+assert_bad           "--direct --get-services" # impossible combination
+assert_bad           "--direct --get-default-zone" # impossible combination
+assert_bad           "--direct --zone=home --list-services" # impossible combination
+assert_bad           "--direct --permanent --list-all" # impossible combination
+assert_bad           "--direct --passthrough --get-chains ipv4 filter" # impossible combination
+
 echo "----------------------------------------------------------------------"
 echo "Everything's OK, you rock :-)"
+
+exit 0
