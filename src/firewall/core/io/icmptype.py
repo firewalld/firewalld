firewalld/firewalld

commit 6d57254d2e625886bea919906e58178e389eab7e

Allow non-ascii chars in 'short' and 'description'
of zones, services and icmp-types.

src/firewall/core/io/icmptype.py
@@ -126,6 +126,8 @@ def icmptype_writer(icmptype, path=None):
 
     # short
     if icmptype.short and icmptype.short != "":
+        if isinstance(zone.short, bytes):
+            zone.short = zone.short.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("short", { })
         handler.characters(icmptype.short)
@@ -134,6 +136,8 @@ def icmptype_writer(icmptype, path=None):
 
     # description
     if icmptype.description and icmptype.description != "":
+        if isinstance(zone.description, bytes):
+            zone.description = zone.description.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("description", { })
         handler.characters(icmptype.description)

src/firewall/core/io/service.py
@@ -154,6 +154,8 @@ def service_writer(service, path=None):
 
     # short
     if service.short and service.short != "":
+        if isinstance(zone.short, bytes):
+            zone.short = zone.short.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("short", { })
         handler.characters(service.short)
@@ -162,6 +164,8 @@ def service_writer(service, path=None):
 
     # description
     if service.description and service.description != "":
+        if isinstance(zone.description, bytes):
+            zone.description = zone.description.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("description", { })
         handler.characters(service.description)

src/firewall/core/io/io_object.py
@@ -179,15 +179,21 @@ class IO_Object_ContentHandler(sax.handler.ContentHandler):
 
     def characters(self, content):
         if self._element != None:
-            self._element += str(content.replace('\n', ' '))
+            self._element += content.replace('\n', ' ')
 
 class IO_Object_XMLGenerator(saxutils.XMLGenerator):
     def __init__(self, out):
         saxutils.XMLGenerator.__init__(self, out, "utf-8")
 
     def simpleElement(self, name, attrs):
+        if isinstance(name, bytes):
+            name = name.decode('utf-8')
         self._write(u'<' + name)
         for (name, value) in attrs.items():
+            if isinstance(name, bytes):
+                name = name.decode('utf-8')
+            if isinstance(value, bytes):
+                value = value.decode('utf-8')
             self._write(u' %s=%s' % (name, saxutils.quoteattr(value)))
         self._write(u'/>')
 

src/firewall/core/io/zone.py
@@ -476,6 +476,8 @@ def zone_writer(zone, path=None):
 
     # short
     if zone.short and zone.short != "":
+        if isinstance(zone.short, bytes):
+            zone.short = zone.short.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("short", { })
         handler.characters(zone.short)
@@ -484,6 +486,8 @@ def zone_writer(zone, path=None):
 
     # description
     if zone.description and zone.description != "":
+        if isinstance(zone.description, bytes):
+            zone.description = zone.description.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("description", { })
         handler.characters(zone.description)

commit 241bad8b71a99e8ed5f1138ec2eb9e8a7ce4a7ac

Fix copy-paste error introduced with 6d57254d2e6

src/firewall/core/io/icmptype.py
@@ -126,8 +126,8 @@ def icmptype_writer(icmptype, path=None):
 
     # short
     if icmptype.short and icmptype.short != "":
-        if isinstance(zone.short, bytes):
-            zone.short = zone.short.decode('utf-8')
+        if isinstance(icmptype.short, bytes):
+            icmptype.short = icmptype.short.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("short", { })
         handler.characters(icmptype.short)
@@ -136,8 +136,8 @@ def icmptype_writer(icmptype, path=None):
 
     # description
     if icmptype.description and icmptype.description != "":
-        if isinstance(zone.description, bytes):
-            zone.description = zone.description.decode('utf-8')
+        if isinstance(icmptype.description, bytes):
+            icmptype.description = icmptype.description.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("description", { })
         handler.characters(icmptype.description)

src/firewall/core/io/service.py
@@ -154,8 +154,8 @@ def service_writer(service, path=None):
 
     # short
     if service.short and service.short != "":
-        if isinstance(zone.short, bytes):
-            zone.short = zone.short.decode('utf-8')
+        if isinstance(service.short, bytes):
+            service.short = service.short.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("short", { })
         handler.characters(service.short)
@@ -164,8 +164,8 @@ def service_writer(service, path=None):
 
     # description
     if service.description and service.description != "":
-        if isinstance(zone.description, bytes):
-            zone.description = zone.description.decode('utf-8')
+        if isinstance(service.description, bytes):
+            service.description = service.description.decode('utf-8')
         handler.ignorableWhitespace("  ")
         handler.startElement("description", { })
         handler.characters(service.description)