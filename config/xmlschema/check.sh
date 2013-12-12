5255299a25873d2fa8f66a85011979740d3e0dcc
Jiri Popelka <jpopelka@redhat.com>
2013-12-12 15:08:41 +0100

    update xmlschema/zone.xsd

--- config/xmlschema/check.sh
+++ config/xmlschema/check.sh
@@ -5,7 +5,7 @@ cd ..
 for keyword in zone service icmptype; do
   echo "  ---  ${keyword}s  ---"
   cd "${keyword}s"
-  for i in `ls *.xml`; do
+  for i in *.xml; do
     # for each XML add a reference to an XML Schema, copy output to backup
     sed -e "s,^<${keyword},<${keyword}\nxmlns=\"http://www.w3schools.com\"\nxmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\nxsi:schemaLocation=\"http://www.w3schools.com ${keyword}.xsd\",i" "${i}" > "_${i}"
     # validate backup
