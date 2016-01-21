firewalld/firewalld

commit b5f45cc29e8b9845241e6a1802b9159bf5767af9

Zone: Fix port string according to fw_zone.__port_id() and fw_zone.__forward_port_id()

src/firewall/core/io/zone.py
@@ -25,7 +25,7 @@ import shutil
 
 from firewall.config import ETC_FIREWALLD
 from firewall.errors import *
-from firewall.functions import checkIP, checkIPnMask, checkIP6nMask, checkInterface, uniqify, max_zone_name_len, u2b_if_py2, check_mac
+from firewall.functions import checkIP, checkIPnMask, checkIP6nMask, checkInterface, uniqify, max_zone_name_len, u2b_if_py2, check_mac, portStr
 from firewall.core.base import DEFAULT_ZONE_TARGET, ZONE_TARGETS
 from firewall.core.io.io_object import *
 from firewall.core.rich import *
@@ -308,8 +308,7 @@ class zone_ContentHandler(IO_Object_ContentHandler):
                 self._rule.element = Rich_Port(attrs["port"],
                                                attrs["protocol"])
                 return
-            # TODO: fix port string according to fw_zone.__port_id()
-            entry = (attrs["port"], attrs["protocol"])
+            entry = (portStr(port, "-"), protocol)
             if entry not in self.item.ports:
                 self.item.ports.append(entry)
         elif name == "protocol":
@@ -361,9 +360,8 @@ class zone_ContentHandler(IO_Object_ContentHandler):
                                                       attrs["protocol"],
                                                       to_port, to_addr)
                 return
-            # TODO: fix port string according to fw_zone.__forward_port_id()
-            entry = (attrs["port"], attrs["protocol"], to_port,
-                     to_addr)
+            entry = (portStr(attrs["port"], "-"), attrs["protocol"],
+                     portStr(to_port, "-"), str(to_addr))
             if entry not in self.item.forward_ports:
                 self.item.forward_ports.append(entry)
 

commit 32d6bf391c58a9c5843589c2d0d052d60fe1bf4f

Zone: Fix port string according to fw_zone.__port_id()
Fixes: b5f45cc29e8b9845241e6a1802b9159bf5767af9

src/firewall/core/io/zone.py
@@ -314,7 +314,7 @@ class zone_ContentHandler(IO_Object_ContentHandler):
                 self._rule.element = Rich_Port(attrs["port"],
                                                attrs["protocol"])
                 return
-            entry = (portStr(port, "-"), protocol)
+            entry = (portStr(attrs["port"], "-"), attrs["protocol"])
             if entry not in self.item.ports:
                 self.item.ports.append(entry)
             else: