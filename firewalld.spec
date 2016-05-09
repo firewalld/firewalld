firewalld/firewalld

commit 469f735e57f4c02362c0d16aa5315f9e4f3d518d

Include testsuite scripts firewall-*cmd_test.sh in releases
These are the scripts src/tests/firewall-cmd_test.sh and
src/tests/firewall-offline-cmd_test.sh

src/Makefile.am
@@ -55,7 +55,9 @@ nobase_dist_python_DATA = \
 	firewall/server/decorators.py \
 	firewall/server/firewalld.py \
 	firewall/server/__init__.py \
-	firewall/server/server.py
+	firewall/server/server.py \
+	tests/firewall-cmd_test.sh \
+	tests/firewall-offline-cmd_test.sh
 
 EXTRA_DIST = \
 	firewall/config/__init__.py.in

commit 46b6158e93c7f8d2b2ea7212e36867fca59bb5b3

src/Makefile.am: Move test suite into EXTRA_DIST
Do not install the test suite into python site-packages, but into
$pkgdatadir/tests

Fixes: 469f735e

firewalld.spec
@@ -227,6 +227,9 @@ fi
 %{_mandir}/man1/firewall*cmd*.1*
 %{_mandir}/man1/firewalld*.1*
 %{_mandir}/man5/firewall*.5*
+%dir %{_datadir}/firewalld
+%dir %{_datadir}/firewalld/tests
+%{_datadir}/firewalld/tests
 
 %files -n python-firewall
 %attr(0755,root,root) %dir %{python2_sitelib}/firewall

src/Makefile.am
@@ -9,6 +9,11 @@ dist_glade_DATA = firewall-config.glade
 gtkextradir = $(pkgdatadir)
 dist_gtkextra_DATA = gtk3_chooserbutton.py
 
+testsdir = $(pkgdatadir)/tests
+dist_tests_DATA = \
+	tests/firewall-cmd_test.sh \
+	tests/firewall-offline-cmd_test.sh
+
 nobase_dist_python_DATA = \
 	firewall/client.py \
 	firewall/dbus_utils.py \
@@ -55,16 +60,14 @@ nobase_dist_python_DATA = \
 	firewall/server/decorators.py \
 	firewall/server/firewalld.py \
 	firewall/server/__init__.py \
-	firewall/server/server.py \
-	tests/firewall-cmd_test.sh \
-	tests/firewall-offline-cmd_test.sh
+	firewall/server/server.py
 
 EXTRA_DIST = \
 	firewall/config/__init__.py.in
 
 CLEANFILES = *~ *\# .\#* *.py?
 
-install-data-local: install-dist_sbinSCRIPTS install-dist_binSCRIPTS
+install-data-local: install-dist_sbinSCRIPTS install-dist_binSCRIPTS install-dist_testsDATA
 	@echo "Fixing python shebang"
 	@list='$(dist_bin_SCRIPTS)'; \
 	for p in $$list; do \
@@ -75,4 +78,10 @@ install-data-local: install-dist_sbinSCRIPTS install-dist_binSCRIPTS
 	for p in $$list; do \
 	  echo $(top_srcdir)/fix_python_shebang.sh "$(DESTDIR)$(sbindir)/$$p"; \
 	  $(top_srcdir)/fix_python_shebang.sh "$(DESTDIR)$(sbindir)/$$p"; \
+	done
+	@echo "Fixing path for test suite scripts"
+	@list='$(dist_tests_DATA)'; \
+	for p in $$list; do \
+	  echo sed -i -e 's@^readonly path=.*@readonly path=\"$(bindir)\"@' "$(DESTDIR)$(pkgdatadir)/$$p"; \
+	  sed -i -e 's@^readonly path=.*@readonly path=\"$(bindir)\"@' "$(DESTDIR)$(pkgdatadir)/$$p"; \
 	done