firewalld/firewalld

commit 0ab7673fd666e93c09309cb06481745b0ae5d1a5

Fix build from spec without fedorahosted.org archives

firewalld.spec
@@ -115,6 +115,7 @@ firewalld.
 
 %prep
 %setup -q
+./autogen.sh
 
 %if 0%{?with_python3}
 rm -rf %{py3dir}
@@ -127,13 +128,12 @@ sed -i 's|/usr/bin/python|%{__python3}|' %{py3dir}/config/lockdown-whitelist.xml
 
 %build
 %configure --enable-sysconfig --enable-rpmmacros
-# Enable the make line if there are patches affecting man pages to
-# regenerate them
-# make %{?_smp_mflags}
+make %{?_smp_mflags}
 
 %if 0%{?with_python3}
 pushd %{py3dir}
 %configure --enable-sysconfig --enable-rpmmacros PYTHON=%{__python3}
+make %{?_smp_mflags}
 popd
 %endif #0%{?with_python3}
 

commit 3ae7dd39ac1df9a7eef81663db6c49ac160f44c9

firewalld.spec: Add missing autotools dependencies
Commit 0ab7673fd666 ("Fix build from spec without fedorahosted.org
archives") added 'autogen.sh' to the %prep phase but this script calls
'autoreconf' so we need to add the necessary autotools dependencies to
the spec file as well.

Fixes: 0ab7673fd666 ("Fix build from spec without fedorahosted.org archives")
Signed-off-by: Markos Chandras <mchandras@suse.de>

firewalld.spec
@@ -13,6 +13,8 @@ URL:     http://www.firewalld.org
 License: GPLv2+
 Source0: https://github.com/t-woerner/firewalld/archive/v%{version}.tar.gz#/%{name}-%{version}.tar.gz
 BuildArch: noarch
+BuildRequires: autoconf
+BuildRequires: automake
 BuildRequires: desktop-file-utils
 BuildRequires: gettext
 BuildRequires: intltool