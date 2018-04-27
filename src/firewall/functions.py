firewalld/firewalld

commit 68cbe02f3db4e8d58a7a08180fdefab6969dcde5

functions: Allow nf_conntrack_proto_* helpers
This fixes using the nf_conntrack_proto_gre module/helper. Currently GRE
is the only one of these that remains on modern kernels. Other (DCCP,
SCTP) have been changed to be built ins.

Fixes: #263
Closes: #323

src/firewall/functions.py
@@ -343,6 +343,15 @@ def get_nf_conntrack_helpers():
             (status, ret) = runProg(COMMANDS["modinfo"], [ module, ])
             if status != 0:
                 continue
+            # If module name matches "nf_conntrack_proto_*"
+            # the we add it to helpers list and goto next module
+            if filename.startswith("nf_conntrack_proto_"):
+                helper = filename.split(".")[0].strip()
+                helper = helper.replace("_", "-")
+                helpers.setdefault(module, [ ]).append(helper)
+                continue
+            # Else we get module alias and if "-helper" in the "alias:" line of modinfo
+            # then we add it to helpers list and goto next module
             for line in ret.split("\n"):
                 if line.startswith("alias:") and "-helper-" in line:
                     helper = line.split(":")[1].strip()
@@ -363,6 +372,15 @@ def get_nf_nat_helpers():
             (status, ret) = runProg(COMMANDS["modinfo"], [ module, ])
             if status != 0:
                 continue
+            # If module name matches "nf_nat_proto_*"
+            # the we add it to helpers list and goto next module
+            if filename.startswith("nf_nat_proto_"):
+                helper = filename.split(".")[0].strip()
+                helper = helper.replace("_", "-")
+                helpers.setdefault(module, [ ]).append(helper)
+                continue
+            # Else we get module alias and if "NAT helper" in "description:" line of modinfo
+            # then we add it to helpers list and goto next module
             alias = None
             for line in ret.split("\n"):
                 if line.startswith("description:") and "NAT helper" in line:

commit 7c2f4b6b33668cdce3c7b8866d42a5c5c6e17cf1

helpers: nf_conntrack_proto-* helpers needs name cropped
Fixes: 68cbe02f3db4 ("functions: Allow nf_conntrack_proto_* helpers")

src/firewall/functions.py
@@ -350,6 +350,7 @@ def get_nf_conntrack_helpers():
             if filename.startswith("nf_conntrack_proto_"):
                 helper = filename.split(".")[0].strip()
                 helper = helper.replace("_", "-")
+                helper = helper.replace("nf-conntrack-", "")
                 helpers.setdefault(module, [ ]).append(helper)
                 continue
             # Else we get module alias and if "-helper" in the "alias:" line of modinfo
@@ -381,6 +382,7 @@ def get_nf_nat_helpers():
                 if filename.startswith("nf_nat_proto_"):
                     helper = filename.split(".")[0].strip()
                     helper = helper.replace("_", "-")
+                    helper = helper.replace("nf-nat-", "")
                     helpers.setdefault(module, [ ]).append(helper)
                     continue
                 # Else we get module alias and if "NAT helper" in "description:" line of modinfo