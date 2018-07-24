firewalld/firewalld

commit 1fea41d5e8d806db5c2380cffde13059d9fa9e7a

ipXtables: Move reverse_rule() to backend classes

src/firewall/core/ipXtables.py
@@ -150,6 +150,45 @@ OUR_CHAINS["filter"] = set(["INPUT_direct", "INPUT_ZONES_SOURCE", "INPUT_ZONES",
                             "FORWARD_IN_ZONES", "FORWARD_OUT_ZONES_SOURCE",
                             "FORWARD_OUT_ZONES", "OUTPUT_direct"])
 
+# ipv ebtables also uses this
+#
+def common_reverse_rule(self, args):
+    """ Inverse valid rule """
+
+    replace_args = {
+        # Append
+        "-A": "-D",
+        "--append": "--delete",
+        # Insert
+        "-I": "-D",
+        "--insert": "--delete",
+        # New chain
+        "-N": "-X",
+        "--new-chain": "--delete-chain",
+    }
+
+    ret_args = args[:]
+
+    for arg in replace_args:
+        try:
+            idx = ret_args.index(arg)
+        except Exception:
+            continue
+
+        if arg in [ "-I", "--insert" ]:
+            # With insert rulenum, then remove it if it is a number
+            # Opt at position idx, chain at position idx+1, [rulenum] at
+            # position idx+2
+            try:
+                int(ret_args[idx+2])
+            except Exception:
+                pass
+            else:
+                ret_args.pop(idx+2)
+
+        ret_args[idx] = replace_args[arg]
+    return ret_args
+
 def common_reverse_passthrough(args):
     """ Reverse valid passthough rule """
 
@@ -311,6 +350,9 @@ class ip4tables(object):
             rule += [ "-D", chain ]
         rule += args
         return rule
+
+    def reverse_rule(self, args):
+        return common_reverse_rule(args)
 
     def check_passthrough(self, args):
         common_check_passthrough(args)
@@ -611,42 +653,3 @@ class ip6tables(ip4tables):
                                        "-m", "rpfilter", "--invert",
                                        "-j", "LOG",
                                        "--log-prefix", "rpfilter_DROP: " ])
-
-# ipv ebtables also uses this
-#
-def reverse_rule(self, args):
-    """ Inverse valid rule """
-
-    replace_args = {
-        # Append
-        "-A": "-D",
-        "--append": "--delete",
-        # Insert
-        "-I": "-D",
-        "--insert": "--delete",
-        # New chain
-        "-N": "-X",
-        "--new-chain": "--delete-chain",
-    }
-
-    ret_args = args[:]
-
-    for arg in replace_args:
-        try:
-            idx = ret_args.index(arg)
-        except Exception:
-            continue
-
-        if arg in [ "-I", "--insert" ]:
-            # With insert rulenum, then remove it if it is a number
-            # Opt at position idx, chain at position idx+1, [rulenum] at
-            # position idx+2
-            try:
-                int(ret_args[idx+2])
-            except Exception:
-                pass
-            else:
-                ret_args.pop(idx+2)
-
-        ret_args[idx] = replace_args[arg]
-    return ret_args

src/firewall/core/ebtables.py
@@ -134,6 +134,9 @@ class ebtables(object):
         rule += args
         return rule
 
+    def reverse_rule(self, args):
+        return ipXtables.common_reverse_rule(args)
+
     def check_passthrough(self, args):
         ipXtables.common_check_passthrough(args)
 

src/firewall/core/fw.py
@@ -815,7 +815,7 @@ class Firewall(object):
                     log.error(msg)
                     for rule in reversed(_rules[:i]):
                         try:
-                            backend.set_rule(ipXtables.reverse_rule(rule))
+                            backend.set_rule(backend.reverse_rule(rule))
                         except Exception:
                             # ignore errors here
                             pass

src/firewall/core/fw_transaction.py
@@ -27,7 +27,6 @@ from firewall.core.logger import log
 from firewall import errors
 from firewall.errors import FirewallError
 from firewall.fw_types import LastUpdatedOrderedDict
-from firewall.core.ipXtables import reverse_rule
 
 class SimpleFirewallTransaction(object):
     """Base class for FirewallTransaction and FirewallZoneTransaction"""
@@ -76,7 +75,8 @@ class SimpleFirewallTransaction(object):
             # reverse rule order for cleanup
             for ipv in self.rules:
                 for rule in reversed(self.rules[ipv]):
-                    rules.setdefault(ipv, [ ]).append(reverse_rule(rule))
+                    rules.setdefault(ipv, [ ]).append(
+                        self.fw.get_ipv_backend(ipv).reverse_rule(rule))
         else:
             for ipv in self.rules:
                 rules.setdefault(ipv, [ ]).extend(self.rules[ipv])
@@ -121,7 +121,8 @@ class SimpleFirewallTransaction(object):
             for ipv in done:
                 undo_rules[ipv] = [ ]
                 for rule in reversed(rules[ipv]):
-                    undo_rules[ipv].append(reverse_rule(rule))
+                    undo_rules[ipv].append(
+                        self.fw.get_ipv_backend(ipv).reverse_rule(rule))
             for ipv in undo_rules:
                 try:
                     self.fw.rules(ipv, undo_rules[ipv])

commit f739a49a1050226039ab3d1bd530249e8965427c

ipXtables: fix definition of common_reverse_rule()
Fixes: 1fea41d5e8d8 ("ipXtables: Move reverse_rule() to backend classes")

src/firewall/core/ipXtables.py
@@ -156,7 +156,7 @@ OUR_CHAINS["filter"] = set(["INPUT_direct", "INPUT_ZONES_SOURCE", "INPUT_ZONES",
 
 # ipv ebtables also uses this
 #
-def common_reverse_rule(self, args):
+def common_reverse_rule(args):
     """ Inverse valid rule """
 
     replace_args = {