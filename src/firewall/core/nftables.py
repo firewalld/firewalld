firewalld/firewalld

commit eb4e8b4cede861f2c5313e3b3e9108fe69faea7d

nftables: add AUDIT support
Adds support for logging to auditd.

src/firewall/core/nftables.py
@@ -696,8 +696,15 @@ class nftables(object):
     def _rich_rule_audit(self, rich_rule, enable, table, target, rule_fragment):
         if not rich_rule.audit:
             return []
-        # FIXME: nftables does not support AUDIT, need to complain loudly
-        raise FirewallError(UNKNOWN_ERROR, "not implemented")
+
+        add_del = { True: "add", False: "delete" }[enable]
+
+        rule = [add_del, "rule", "inet", "%s" % TABLE_NAME,
+                "%s_%s_allow" % (table, target)]
+        rule += rule_fragment + ["log", "level", "audit"]
+        rule += self._rich_rule_limit_fragment(rich_rule.log.limit)
+
+        return rule
 
     def _rich_rule_action(self, zone, rich_rule, enable, table, target, rule_fragment):
         if not rich_rule.action:

commit 0562394185c60e8fce899319fb8b48862df0bb3c

nftables: fix rich rule audit log
Fixes: eb4e8b4cede8 ("nftables: add AUDIT support")

src/firewall/core/nftables.py
@@ -700,7 +700,7 @@ class nftables(object):
         rule = [add_del, "rule", "inet", "%s" % TABLE_NAME,
                 "%s_%s_allow" % (table, target)]
         rule += rule_fragment + ["log", "level", "audit"]
-        rule += self._rich_rule_limit_fragment(rich_rule.log.limit)
+        rule += self._rich_rule_limit_fragment(rich_rule.audit.limit)
 
         return rule
 