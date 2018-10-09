firewalld/firewalld

commit e7c00a4063ff88171436cb8d0329abcd3d923765

ifcfg: Modify ZONE= on permanent config changes
Only touch the ZONE= in ifcfg files with permanent configuration
changes. We should not be touching these for runtime changes.

src/firewall/server/firewalld.py
@@ -53,6 +53,7 @@ from firewall.core.io.icmptype import IcmpType
 from firewall.core.io.helper import Helper
 from firewall.core.fw_nm import nm_get_bus_name, nm_get_connection_of_interface, \
                                 nm_set_zone_of_connection
+from firewall.core.fw_ifcfg import ifcfg_set_zone_of_interface
 from firewall import errors
 from firewall.errors import FirewallError
 
@@ -461,6 +462,9 @@ class FirewallD(slip.dbus.service.Object):
                 if changed:
                     del conf
                     conf = settings.settings
+            # For the remaining try to update the ifcfg files
+            for interface in settings.getInterfaces():
+                ifcfg_set_zone_of_interface(name, interface)
             try:
                 if name in config_names:
                     conf_obj = self.config.getZoneByName(name)

src/firewall/core/fw_zone.py
@@ -31,7 +31,6 @@ from firewall.core.rich import Rich_Rule, Rich_Accept, \
     Rich_IcmpType
 from firewall.core.fw_transaction import FirewallTransaction, \
     FirewallZoneTransaction
-from firewall.core.fw_ifcfg import ifcfg_set_zone_of_interface
 from firewall import errors
 from firewall.errors import FirewallError
 from firewall.fw_types import LastUpdatedOrderedDict
@@ -431,8 +430,6 @@ class FirewallZone(object):
         zone_transaction.add_fail(self.__unregister_interface, _obj,
                                   interface_id)
 
-        zone_transaction.add_post(ifcfg_set_zone_of_interface, zone, interface)
-
         if use_zone_transaction is None:
             zone_transaction.execute(True)
 
@@ -502,12 +499,6 @@ class FirewallZone(object):
 
         zone_transaction.add_post(self.__unregister_interface, _obj,
                                   interface_id)
-
-        # Do not reset ZONE with ifdown
-        # On reboot or shutdown the zone has been reset to default
-        # if the network service is enabled and controlling the
-        # interface (RHBZ#1381314)
-        #zone_transaction.add_post(ifcfg_set_zone_of_interface, "", interface)
 
         if use_zone_transaction is None:
             zone_transaction.execute(True)

src/firewall/server/config_zone.py
@@ -33,6 +33,7 @@ from firewall.dbus_utils import dbus_to_python, \
     dbus_introspection_prepare_properties, \
     dbus_introspection_add_properties
 from firewall.core.io.zone import Zone
+from firewall.core.fw_ifcfg import ifcfg_set_zone_of_interface
 from firewall.core.base import DEFAULT_ZONE_TARGET
 from firewall.core.rich import Rich_Rule
 from firewall.core.logger import log
@@ -878,6 +879,8 @@ class FirewallDConfigZone(slip.dbus.service.Object):
         settings[10].append(interface)
         self.update(settings)
 
+        ifcfg_set_zone_of_interface(self.obj.name, interface)
+
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
                          in_signature='s')
     @dbus_handle_exceptions
@@ -890,6 +893,8 @@ class FirewallDConfigZone(slip.dbus.service.Object):
             raise FirewallError(errors.NOT_ENABLED, interface)
         settings[10].remove(interface)
         self.update(settings)
+
+        ifcfg_set_zone_of_interface("", interface)
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
                          in_signature='s',

commit 17adfe4137cfd1c1734ff1b77304f70e163313fa

firewalld: fix --runtime-to-permanent if NM not in use.
Due to scope "settings" was not defined.

Fixes: #404
Fixes: e7c00a4063ff ("ifcfg: Modify ZONE= on permanent config changes")

src/firewall/server/firewalld.py
@@ -441,8 +441,8 @@ class FirewallD(slip.dbus.service.Object):
         nm_bus_name = nm_get_bus_name()
         for name in self.fw.zone.get_zones():
             conf = self.getZoneSettings(name)
+            settings = FirewallClientZoneSettings(conf)
             if nm_bus_name is not None:
-                settings = FirewallClientZoneSettings(conf)
                 changed = False
                 for interface in settings.getInterfaces():
                     if self.fw.zone.interface_get_sender(name, interface) == nm_bus_name: