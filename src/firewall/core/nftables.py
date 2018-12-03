firewalld/firewalld

commit a0f683dfef2c43bb12aa3b7d4028d142ec981559

nftables: fix policy
These temporary chains need to use a different table otherwise flush()
operations may accidentally destroy them.

src/firewall/core/nftables.py
@@ -322,28 +322,23 @@ class nftables(object):
             for chain in IPTABLES_TO_NFT_HOOK[table]:
                 table_chains.append((table, chain))
 
-        table_suffix = "policy_drop"
+        table_name = TABLE_NAME + "_" + "policy_drop"
 
         def _policy_drop_helper(table, chain, family, rules):
-            _chain = "%s_%s_%s" % (table, chain, table_suffix)
+            _chain = "%s_%s" % (table, chain)
             _hook = IPTABLES_TO_NFT_HOOK[table][chain][0]
             # add hooks with priority -1, only contain drop rule
             _priority = IPTABLES_TO_NFT_HOOK[table][chain][1] - 1
             _add_chain = "add chain %s %s %s '{ type filter hook %s priority %d ; }'" % \
-                         (family, TABLE_NAME, _chain, _hook, _priority)
+                         (family, table_name, _chain, _hook, _priority)
             rules.append(splitArgs(_add_chain))
-            rules.append(["add", "rule", family, TABLE_NAME, _chain, "drop"])
-
-        def _policy_accept_helper(table, chain, family, rules):
-            _chain = "%s_%s_%s" % (table, chain, table_suffix)
-            _rule = ["delete", "rule", family, TABLE_NAME, _chain, "drop"]
-            _rule_key = " ".join(_rule)
-            if _rule_key in self.rule_to_handle:
-                rules.append(_rule)
-                rules.append(["delete", "chain", family, TABLE_NAME, _chain])
+            rules.append(["add", "rule", family, table_name, _chain, "drop"])
 
         rules = []
         if policy == "DROP":
+            for family in ["inet", "ip", "ip6"]:
+                rules.append(["add", "table", family, table_name])
+
             for table,chain in table_chains:
                 if table == "nat":
                     # nat requires two families
@@ -352,13 +347,8 @@ class nftables(object):
                 else:
                     _policy_drop_helper(table, chain, "inet", rules)
         elif policy == "ACCEPT":
-            for table,chain in table_chains:
-                if table == "nat":
-                    # nat requires two families
-                    for family in ["ip", "ip6"]:
-                        _policy_accept_helper(table, chain, family, rules)
-                else:
-                    _policy_accept_helper(table, chain, "inet", rules)
+            for family in ["inet", "ip", "ip6"]:
+                rules.append(["delete", "table", family, table_name])
         else:
             FirewallError(UNKNOWN_ERROR, "not implemented")
 

commit 2f5608b4897ff99afbb1c2425a94df035031c1a2

nftables: fix panic mode not filtering output packets
This simplifies policy in the nftables backend by filtering only on the
prerouting and output hooks. The others hooks are unnecessary since
we're using a higher precedence.

Also fixes an issue when re-enabling panic mode multiple times. Due to
rule de-duplication the policy drop rule was not being re-added.

Fixes: rhbz 1579740
Fixes: a0f683dfef2c ("nftables: fix policy")

src/firewall/core/nftables.py
@@ -314,38 +314,20 @@ class nftables(object):
         # packets while initially starting and for panic mode. As such, using
         # hooks with a higher priority than our base chains is sufficient.
         #
-        table_chains = []
-        for table in list(IPTABLES_TO_NFT_HOOK.keys()):
-            for chain in IPTABLES_TO_NFT_HOOK[table]:
-                table_chains.append((table, chain))
-
         table_name = TABLE_NAME + "_" + "policy_drop"
-
-        def _policy_drop_helper(table, chain, family, rules):
-            _chain = "%s_%s" % (table, chain)
-            _hook = IPTABLES_TO_NFT_HOOK[table][chain][0]
-            # add hooks with priority -1, only contain drop rule
-            _priority = IPTABLES_TO_NFT_HOOK[table][chain][1] - 1
-            _add_chain = "add chain %s %s %s '{ type filter hook %s priority %d ; }'" % \
-                         (family, table_name, _chain, _hook, _priority)
-            rules.append(splitArgs(_add_chain))
-            rules.append(["add", "rule", family, table_name, _chain, "drop"])
 
         rules = []
         if policy == "DROP":
-            for family in ["inet", "ip", "ip6"]:
-                rules.append(["add", "table", family, table_name])
+            rules.append(["add", "table", "inet", table_name])
 
-            for table,chain in table_chains:
-                if table == "nat":
-                    # nat requires two families
-                    for family in ["ip", "ip6"]:
-                        _policy_drop_helper(table, chain, family, rules)
-                else:
-                    _policy_drop_helper(table, chain, "inet", rules)
+            # To drop everything we need to use the "raw" priority. These occur
+            # before conntrack, mangle, nat, etc
+            for hook in ["prerouting", "output"]:
+                _add_chain = "add chain inet %s %s_%s '{ type filter hook %s priority %d ; policy drop ; }'" % \
+                             (table_name, "raw", hook, hook, -300 + NFT_HOOK_OFFSET - 1)
+                rules.append(splitArgs(_add_chain))
         elif policy == "ACCEPT":
-            for family in ["inet", "ip", "ip6"]:
-                rules.append(["delete", "table", family, table_name])
+            rules.append(["delete", "table", "inet", table_name])
         else:
             FirewallError(UNKNOWN_ERROR, "not implemented")
 