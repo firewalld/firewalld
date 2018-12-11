firewalld/firewalld

commit e6188ec98ff478f8eab9d24b6105d056c6c6ddf6

FirewallIPSet: Support restore in apply_ipsets, use it in Firewall
The apply_ipsets method has the new argument individual, which derfaults to False. In the individual mode the method behaves as before and uses individual calls for creation of the set and addition of the entries. Otherwise the new restore method in the ipset class is used.

The ipset creation in Firewall has been adapted and has moved right before the creation of zones in netfilter.

src/firewall/core/fw_ipset.py
@@ -68,26 +68,30 @@ class FirewallIPSet(object):
 
     def remove_ipset(self, name):
         obj = self._ipsets[name]
-        try:
-            self._fw._ipset.destroy(name)
-        except Exception as msg:
-            log.error("Failed to destroy ipset '%s'" % name)
-            log.error(msg)
+        if obj.applied:
+            try:
+                self._fw._ipset.destroy(name)
+            except Exception as msg:
+                log.error("Failed to destroy ipset '%s'" % name)
+                log.error(msg)
         del self._ipsets[name]
 
-    def apply_ipsets(self):
+    def apply_ipsets(self, individual=False):
         for ipset in self.get_ipsets():
             obj = self._ipsets[ipset]
+            obj.applied = False
 
-            try:
-                self._fw._ipset.create(obj.name, obj.type, obj.options)
-            except Exception as msg:
-                log.error("Failed to create ipset '%s'" % obj.name)
-                log.error(msg)
-            else:
-                if "timeout" not in obj.options:
-                    # no entries visible for ipsets with timeout
-                    continue
+            if individual:
+                try:
+                    self._fw._ipset.create(obj.name, obj.type, obj.options)
+                except Exception as msg:
+                    log.error("Failed to create ipset '%s'" % obj.name)
+                    log.error(msg)
+                else:
+                    obj.applied = True
+                    if "timeout" not in obj.options:
+                        # no entries visible for ipsets with timeout
+                        continue
 
                 for entry in obj.entries:
                     try:
@@ -96,6 +100,16 @@ class FirewallIPSet(object):
                         log.error("Failed to add entry '%s' to ipset '%s'" % \
                                   (entry, obj.name))
                         log.error(msg)
+            else:
+                try:
+                    self._fw._ipset.restore(obj.name, obj.type, obj.entries,
+                                            obj.options, None)
+                except Exception as msg:
+                    log.error("Failed to add entries to ipset '%s'" % \
+                              obj.name)
+                    log.error(msg)
+                else:
+                    obj.applied = True
 
     # TYPE
 

src/firewall/core/fw.py
@@ -200,8 +200,6 @@ class Firewall(object):
         self._loader(FIREWALLD_IPSETS, "ipset")
         self._loader(ETC_FIREWALLD_IPSETS, "ipset")
 
-        self.ipset.apply_ipsets()
-
         # load icmptype files
         self._loader(FIREWALLD_ICMPTYPES, "icmptype")
         self._loader(ETC_FIREWALLD_ICMPTYPES, "icmptype")
@@ -232,6 +230,9 @@ class Firewall(object):
                 error = True
         if error:
             sys.exit(1)
+
+        # apply settings for loaded ipsets
+        self.ipset.apply_ipsets(self._individual_calls)
 
         # apply settings for loaded zones
         self.zone.apply_zones()

commit 4157393136bbaff53e812029376b2a0a5113cedb

ipset: fix set apply if IndividualCalls=yes
Fixes: rhbz 1644834
Fixes: e6188ec98ff4 ("FirewallIPSet: Support restore in apply_ipsets, use it in Firewall")

src/firewall/core/fw_ipset.py
@@ -125,7 +125,7 @@ class FirewallIPSet(object):
                         raise FirewallError(errors.COMMAND_FAILED, msg)
                     else:
                         obj.applied = True
-                        if "timeout" not in obj.options or \
+                        if "timeout" in obj.options and \
                            obj.options["timeout"] != "0":
                             # no entries visible for ipsets with timeout
                             continue

src/tests/regression/rhbz1601610.at
@@ -57,5 +57,48 @@ FWD_CHECK([-q --permanent --ipset=foobar --remove-entry=10.1.1.0/22])
 FWD_CHECK([--permanent --ipset=foobar --get-entries], 0, [
 ])
 
+dnl rhbz 1644834
+FWD_CHECK([-q --ipset=foobar --add-entry=10.1.0.0/16])
+FWD_CHECK([-q --runtime-to-permanent])
+FWD_RELOAD
+m4_if(nftables, FIREWALL_BACKEND, [
+NFT_LIST_SET([foobar], 0, [dnl
+table inet firewalld {
+set foobar {
+type ipv4_addr
+flags interval
+elements = { 10.1.0.0/16, 10.2.0.0/22 }
+}
+}
+])], [
+IPSET_LIST_SET([foobar], 0, [dnl
+Name: foobar
+Type: hash:net
+Members:
+10.1.0.0/16
+10.2.0.0/22
+])])
+
+dnl rhbz 1644834, again with IndividualCalls=yes
+AT_CHECK([sed -i 's/^IndividualCalls.*/IndividualCalls=yes/' ./firewalld.conf])
+FWD_RELOAD
+m4_if(nftables, FIREWALL_BACKEND, [
+NFT_LIST_SET([foobar], 0, [dnl
+table inet firewalld {
+set foobar {
+type ipv4_addr
+flags interval
+elements = { 10.1.0.0/16, 10.2.0.0/22 }
+}
+}
+])], [
+IPSET_LIST_SET([foobar], 0, [dnl
+Name: foobar
+Type: hash:net
+Members:
+10.1.0.0/16
+10.2.0.0/22
+])])
+
 FWD_END_TEST([-e '/ERROR: COMMAND_FAILED:.*already added.*/d'dnl
               -e '/ERROR: COMMAND_FAILED:.*element.*exists/d'])