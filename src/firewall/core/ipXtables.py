firewalld/firewalld

commit 7235f76a818329e3aaba2f8c1444f15b4af586c2

improvement: iptables: simplify forward port rules
Replace three rules in mangle/nat/filter with one in nat and a generic
top-level "dnat" accept. We no longer use marks for forward ports.

src/firewall/core/ipXtables.py
@@ -652,7 +652,7 @@ class ip4tables(object):
             "-N INPUT_ZONES_SOURCE",
             "-N INPUT_ZONES",
 
-            "-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT",
+            "-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT",
             "-A INPUT -i lo -j ACCEPT",
             "-A INPUT -j INPUT_direct",
             "-A INPUT -j INPUT_ZONES_SOURCE",
@@ -672,7 +672,7 @@ class ip4tables(object):
             "-N FORWARD_OUT_ZONES_SOURCE",
             "-N FORWARD_OUT_ZONES",
 
-            "-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT",
+            "-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT",
             "-A FORWARD -i lo -j ACCEPT",
             "-A FORWARD -j FORWARD_direct",
             "-A FORWARD -j FORWARD_IN_ZONES_SOURCE",
@@ -1132,9 +1132,6 @@ class ip4tables(object):
                                       protocol, toport, toaddr, mark_id, rich_rule=None):
         add_del = { True: "-A", False: "-D" }[enable]
 
-        mark_str = "0x%x" % mark_id
-        mark = [ "-m", "mark", "--mark", mark_str ]
-
         to = ""
         if toaddr:
             if check_single_address("ipv6", toaddr):
@@ -1147,11 +1144,10 @@ class ip4tables(object):
         target = DEFAULT_ZONE_TARGET.format(chain=SHORTCUTS["PREROUTING"],
                                             zone=zone)
 
-        rule_fragment = [ "-p", protocol, "--dport", portStr(port) ]
-        rich_rule_priority_fragment = []
+        rule_fragment = []
         if rich_rule:
             chain_suffix = self._rich_rule_chain_suffix(rich_rule)
-            rich_rule_priority_fragment = self._rich_rule_priority_fragment(rich_rule)
+            rule_fragment = self._rich_rule_priority_fragment(rich_rule)
             rule_fragment += self._rich_rule_destination_fragment(rich_rule.destination)
             rule_fragment += self._rich_rule_source_fragment(rich_rule.source)
         else:
@@ -1159,24 +1155,11 @@ class ip4tables(object):
 
         rules = []
         if rich_rule:
-            rules.append(self._rich_rule_log(rich_rule, enable, "mangle", target, rule_fragment))
-        rules.append(["-t", "mangle", add_del, "%s_%s" % (target, chain_suffix)]
-                     + rich_rule_priority_fragment + rule_fragment +
-                     [ "-j", "MARK", "--set-mark", mark_str ])
-
-        # local and remote
+            rules.append(self._rich_rule_log(rich_rule, enable, "nat", target, rule_fragment))
         rules.append(["-t", "nat", add_del, "%s_%s" % (target, chain_suffix)]
-                     + rich_rule_priority_fragment +
-                     ["-p", protocol ] + mark +
-                     [ "-j", "DNAT", "--to-destination", to ])
-
-        target = DEFAULT_ZONE_TARGET.format(chain=SHORTCUTS[filter_chain],
-                                            zone=zone)
-        rules.append(["-t", "filter", add_del, "%s_%s" % (target, chain_suffix)]
-                     + rich_rule_priority_fragment +
-                     ["-m", "conntrack", "--ctstate", "NEW,UNTRACKED" ]
-                     + mark +
-                     [ "-j", "ACCEPT" ])
+                     + rule_fragment +
+                     ["-p", protocol, "--dport", portStr(port),
+                      "-j", "DNAT", "--to-destination", to])
 
         return rules
 

src/tests/features/rfc3964_ipv4.at
@@ -53,7 +53,7 @@ m4_if(nftables, FIREWALL_BACKEND, [
         REJECT all ::/0 ::/96 reject-with icmp6-addr-unreachable
     ])
     IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
-        ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED
+        ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
         ACCEPT all ::/0 ::/0
         FORWARD_direct all ::/0 ::/0
         RFC3964_IPv4 all ::/0 ::/0
@@ -104,7 +104,7 @@ m4_if(nftables, FIREWALL_BACKEND, [
 ], [
     NS_CHECK([ip6tables -w -n -t filter -L RFC3964_IPv4], 1, [ignore], [ignore])
     IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
-        ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED
+        ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
         ACCEPT all ::/0 ::/0
         FORWARD_direct all ::/0 ::/0
         FORWARD_IN_ZONES_SOURCE all ::/0 ::/0

src/tests/firewall-cmd.at
@@ -1194,18 +1194,14 @@ FWD_START_TEST([rich rules priority])
         }
     ])], [
     IPTABLES_LIST_RULES([filter], [IN_public_pre], 0, [dnl
-        ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate NEW,UNTRACKED mark match 0x66
         DROP all -- 10.1.0.0/16 0.0.0.0/0
     ])
     IPTABLES_LIST_RULES([filter], [IN_public_allow], 0, [dnl
         ACCEPT tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:22 ctstate NEW,UNTRACKED
-        ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate NEW,UNTRACKED mark match 0x64
     ])
     IPTABLES_LIST_RULES([filter], [FWDI_public_pre], 0, [dnl
-        ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate NEW,UNTRACKED mark match 0x67
     ])
     IPTABLES_LIST_RULES([filter], [FWDI_public_allow], 0, [dnl
-        ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate NEW,UNTRACKED mark match 0x65
     ])
     IPTABLES_LIST_RULES([filter], [FWDO_public_pre], 0, [dnl
         ACCEPT all -- 10.1.1.0/24 0.0.0.0/0 ctstate NEW,UNTRACKED
@@ -1214,12 +1210,12 @@ FWD_START_TEST([rich rules priority])
         ACCEPT all -- 10.10.0.0/16 0.0.0.0/0 ctstate NEW,UNTRACKED
     ])
     IPTABLES_LIST_RULES([nat], [PRE_public_pre], 0, [dnl
-        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 mark match 0x66 to::80
-        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 mark match 0x67 to:10.1.1.1:80
+        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:8888 to::80
+        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:8080 to:10.1.1.1:80
     ])
     IPTABLES_LIST_RULES([nat], [PRE_public_allow], 0, [dnl
-        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 mark match 0x64 to::22
-        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 mark match 0x65 to:10.1.1.1:22
+        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:222 to::22
+        DNAT tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:2222 to:10.1.1.1:22
     ])
     IPTABLES_LIST_RULES([nat], [POST_public_pre], 0, [dnl
         MASQUERADE all -- 10.1.1.0/24 0.0.0.0/0
@@ -1228,39 +1224,29 @@ FWD_START_TEST([rich rules priority])
         MASQUERADE all -- 10.10.0.0/16 0.0.0.0/0
     ])
     IPTABLES_LIST_RULES([mangle], [PRE_public_pre], 0, [dnl
-        MARK tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:8888 MARK set 0x66
-        MARK tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:8080 MARK set 0x67
     ])
     IPTABLES_LIST_RULES([mangle], [PRE_public_allow], 0, [dnl
-        MARK tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:222 MARK set 0x64
-        MARK tcp -- 0.0.0.0/0 0.0.0.0/0 tcp dpt:2222 MARK set 0x65
     ])
     IP6TABLES_LIST_RULES([filter], [IN_public_pre], 0, [dnl
-        ACCEPT all ::/0 ::/0 ctstate NEW,UNTRACKED mark match 0x69
     ])
     IP6TABLES_LIST_RULES([filter], [IN_public_allow], 0, [dnl
         ACCEPT tcp ::/0 ::/0 tcp dpt:22 ctstate NEW,UNTRACKED
         ACCEPT udp ::/0 fe80::/64 udp dpt:546 ctstate NEW,UNTRACKED
-        ACCEPT all ::/0 ::/0 ctstate NEW,UNTRACKED mark match 0x68
     ])
     IP6TABLES_LIST_RULES([filter], [FWDI_public_pre], 0, [dnl
-        ACCEPT all ::/0 ::/0 ctstate NEW,UNTRACKED mark match 0x6a
     ])
     IP6TABLES_LIST_RULES([filter], [FWDI_public_allow], 0, [dnl
     ])
     IP6TABLES_LIST_RULES([nat], [PRE_public_pre], 0,
-        [[DNAT tcp ::/0 ::/0 mark match 0x69 to::99
-        DNAT tcp ::/0 ::/0 mark match 0x6a to:[1234::4321]:9999
+        [[DNAT tcp ::/0 ::/0 tcp dpt:999 to::99
+        DNAT tcp ::/0 ::/0 tcp dpt:9999 to:[1234::4321]:9999
     ]])
     IP6TABLES_LIST_RULES([nat], [PRE_public_allow], 0, [dnl
-        DNAT tcp ::/0 ::/0 mark match 0x68 to::90
+        DNAT tcp ::/0 ::/0 tcp dpt:9090 to::90
     ])
     IP6TABLES_LIST_RULES([mangle], [PRE_public_pre], 0, [dnl
-        MARK tcp ::/0 ::/0 tcp dpt:999 MARK set 0x69
-        MARK tcp ::/0 ::/0 tcp dpt:9999 MARK set 0x6a
     ])
     IP6TABLES_LIST_RULES([mangle], [PRE_public_allow], 0, [dnl
-        MARK tcp ::/0 ::/0 tcp dpt:9090 MARK set 0x68
     ])])
     FWD_RELOAD
 

commit b08695e622cc5d586d675384be7c80c0222a39bf

fix: ipXtables: using "mangle" in zone not dependent on "nat"
Since we removed the use of "mangle" in forward ports, the use of
"mangle" in a zone is no longer dependent on "nat" also being usable.
The only thing that uses "mangle" at the moment is the rich rule mark
action, which is not dependent on other tables.

Fixes: 7235f76a8183 ("improvement: iptables: simplify forward port rules")

src/firewall/core/ipXtables.py
@@ -720,8 +720,7 @@ class ip4tables(object):
         if table == "filter":
             return { "INPUT", "FORWARD_IN", "FORWARD_OUT" }
         if table == "mangle":
-            if "mangle" in self.get_available_tables() and \
-               "nat" in self.get_available_tables():
+            if "mangle" in self.get_available_tables():
                 return { "PREROUTING" }
         if table == "nat":
             if "nat" in self.get_available_tables():