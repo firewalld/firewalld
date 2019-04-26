firewalld/firewalld

commit ef0c790dfb484ce088a89d0bf95fde38136327c5

improvement: nftables: only add helper object if adding the port/rule
No noticeable change. We were also adding them on a delete, but nftables
handles the duplicate add without error. We're still not deleting the
object on a service delete because that would require reference counting
as multiple services use the same helper. It's not worth the complexity
when we can simply leave the object around. Users can --reload if they
really want to get rid of them.

src/firewall/core/nftables.py
@@ -1016,10 +1016,13 @@ class nftables(object):
         rule += [proto, "dport", "%s" % portStr(port, "-")]
         rule += ["ct", "helper", "set", "\"helper-%s-%s\"" % (helper_name, proto)]
 
-        helper_object = ["ct", "helper", "inet", TABLE_NAME,
-                         "helper-%s-%s" % (helper_name, proto),
-                         "{", "type", "\"%s\"" % (helper_name), "protocol",
-                         proto, ";", "}"]
+        if add_del:
+            helper_object = ["add", "ct", "helper", "inet", TABLE_NAME,
+                             "helper-%s-%s" % (helper_name, proto),
+                             "{", "type", "\"%s\"" % (helper_name), "protocol",
+                             proto, ";", "}"]
+        else:
+            helper_object = []
 
         return [helper_object, rule]
 

commit 8410ee6a2d44a805fe2d14e6c24104bfd6c5ecb4

fix: trigger helper object off enable variable
Instead of "add_del", which is always true.

Fixes: ef0c790dfb48 ("improvement: nftables: only add helper object if adding the port/rule")

src/firewall/core/nftables.py
@@ -1016,7 +1016,7 @@ class nftables(object):
         rule += [proto, "dport", "%s" % portStr(port, "-")]
         rule += ["ct", "helper", "set", "\"helper-%s-%s\"" % (helper_name, proto)]
 
-        if add_del:
+        if enable:
             helper_object = ["add", "ct", "helper", "inet", TABLE_NAME,
                              "helper-%s-%s" % (helper_name, proto),
                              "{", "type", "\"%s\"" % (helper_name), "protocol",