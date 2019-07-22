firewalld/firewalld

commit 1fc208bf93171920196ed771b6a4c709ce6607aa

feat: service includes
Syntax in service definitions is:

    <include service="foobar" />

We support any number of includes. They can be nested (included service
includes yet another). Recursive includes are also protected against.

Fixes: #273

src/firewall/client.py
@@ -821,7 +821,7 @@ class FirewallClientServiceSettings(object):
         if settings:
             self.settings = settings
         else:
-            self.settings = ["", "", "", [], [], {}, [], []]
+            self.settings = ["", "", "", [], [], {}, [], [], []]
 
     @handle_exceptions
     def __repr__(self):
@@ -967,6 +967,28 @@ class FirewallClientServiceSettings(object):
     def queryDestination(self, dest_type, address):
         return (dest_type in self.settings[5] and \
                     address == self.settings[5][dest_type])
+
+    @handle_exceptions
+    def getIncludes(self):
+        return self.settings[8]
+    @handle_exceptions
+    def setIncludes(self, includes):
+        self.settings[8] = includes
+    @handle_exceptions
+    def addInclude(self, include):
+        if include not in self.settings[8]:
+            self.settings[8].append(include)
+        else:
+            raise FirewallError(errors.ALREADY_ENABLED, include)
+    @handle_exceptions
+    def removeInclude(self, include):
+        if include in self.settings[8]:
+            self.settings[8].remove(include)
+        else:
+            raise FirewallError(errors.NOT_ENABLED, include)
+    @handle_exceptions
+    def queryInclude(self, include):
+        return include in self.settings[8]
 
 # ipset config settings
 
@@ -1647,6 +1669,32 @@ class FirewallClientConfigService(object):
     def queryDestination(self, destination, address):
         return self.fw_service.queryDestination(destination, address)
 
+    # include
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def getIncludes(self):
+        return self.fw_service.getIncludes()
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def setIncludes(self, includes):
+        self.fw_service.setIncludes(includes)
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def addInclude(self, include):
+        self.fw_service.addInclude(include)
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def removeInclude(self, include):
+        self.fw_service.removeInclude(include)
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def queryInclude(self, include):
+        return self.fw_service.queryInclude(include)
 
 
 # icmptype config settings

doc/xml/firewalld.dbus.xml
@@ -242,12 +242,12 @@
             </listitem>
           </varlistentry>
 	  <varlistentry id="FirewallD1.Methods.getServiceSettings">
-            <term><methodname>getServiceSettings</methodname>(s: <parameter>service</parameter>) &rarr; (sssa(ss)asa{ss}asa(ss))</term>
+            <term><methodname>getServiceSettings</methodname>(s: <parameter>service</parameter>) &rarr; (sssa(ss)asa{ss}asa(ss)as)</term>
             <listitem>
               <para>
                 Return runtime settings of given <replaceable>service</replaceable>.
                 For getting permanent settings see <link linkend="FirewallD1.config.service.Methods.getSettings">org.fedoraproject.FirewallD1.config.service.Methods.getSettings</link>.
-                Settings are in format: <parameter>version</parameter>, <parameter>name</parameter>, <parameter>description</parameter>, array of <parameter>ports</parameter> (port, protocol), array of <parameter>module names</parameter>, dictionary of <parameter>destinations</parameter>, array of <parameter>protocols</parameter> and array of <parameter>source-ports</parameter> (port, protocol).
+                Settings are in format: <parameter>version</parameter>, <parameter>name</parameter>, <parameter>description</parameter>, array of <parameter>ports</parameter> (port, protocol), array of <parameter>module names</parameter>, dictionary of <parameter>destinations</parameter>, array of <parameter>protocols</parameter>, array of <parameter>source-ports</parameter> (port, protocol) and array of service <parameter>includes</parameter>.
               </para>
               <para>
                 <variablelist>
@@ -259,6 +259,7 @@
                   <varlistentry><term><parameter>destinations (a{ss})</parameter>: dictionary of {IP family : IP address} where 'IP family' key can be either 'ipv4' or 'ipv6'. See <literal>destination</literal> tag in <citerefentry><refentrytitle>firewalld.service</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.service</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
 		  <varlistentry><term><parameter>source-ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.service</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>includes (as)</parameter>: array of service includes, see <literal>include</literal> tag in <citerefentry><refentrytitle>firewalld.service</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
 	      <para>

doc/xml/firewalld.service.xml
@@ -73,6 +73,7 @@
   &lt;protocol value="<replaceable>igmp</replaceable>"/&gt;
   &lt;module name="<replaceable>nf_conntrack_netbios_ns</replaceable>"/&gt;
   &lt;destination ipv4="<replaceable>224.0.0.251</replaceable>" ipv6="<replaceable>ff02::fb</replaceable>"/&gt;
+  &lt;include service="<replaceable>ssdp</replaceable>"/&gt;
 &lt;/service&gt;
       </programlisting>
     </para>
@@ -234,6 +235,27 @@
 	    </para>
 	  </listitem>
 	</varlistentry>
+      </variablelist>
+    </refsect2>
+
+    <refsect2 id="options_include">
+      <title>include</title>
+      <para>
+        Is an optional empty-element tag and can be used several times to have more than one include entry. An include entry has exactly one attribute:
+      </para>
+
+      <variablelist>
+      <varlistentry>
+      <term>service="<replaceable>string</replaceable>"</term>
+      <listitem>
+        <para>
+          The include can be any service supported by firewalld.
+        </para>
+        <para>
+          <emphasis role="bold">Warning:</emphasis>Firewalld will only check that the included <emphasis>service</emphasis> is a valid service if it's applied to a zone.
+        </para>
+      </listitem>
+      </varlistentry>
       </variablelist>
     </refsect2>
 

src/firewall/config/dbus.py
@@ -20,7 +20,7 @@
 #
 
 DBUS_INTERFACE_VERSION = 1
-DBUS_INTERFACE_REVISION = 12
+DBUS_INTERFACE_REVISION = 13
 
 DBUS_INTERFACE = "org.fedoraproject.FirewallD%d" % DBUS_INTERFACE_VERSION
 DBUS_INTERFACE_ZONE = DBUS_INTERFACE+".zone"

src/firewall/core/fw_zone.py
@@ -1762,9 +1762,19 @@ class FirewallZone(object):
                 raise FirewallError(errors.INVALID_RULE, "Unknown element %s" %
                                     type(rule.element))
 
-    def _service(self, enable, zone, service, zone_transaction):
+    def _service(self, enable, zone, service, zone_transaction, included_services=None):
         svc = self._fw.service.get_service(service)
         helpers = self.get_helpers_for_service_modules(svc.modules, enable)
+
+        # First apply any services this service may include
+        if included_services is None:
+            included_services = [service]
+        for include in svc.includes:
+            if include in included_services:
+                continue
+            self.check_service(include)
+            included_services.append(include)
+            self._service(enable, zone, include, zone_transaction, included_services=included_services)
 
         if enable:
             if self._fw.nf_conntrack_helper_setting == 0:

src/firewall/core/io/service.py
@@ -45,8 +45,9 @@ class Service(IO_Object):
         ( "destination", { "": "", }, ),      # a{ss}
         ( "protocols", [ "", ], ),            # as
         ( "source_ports", [ ( "", "" ), ], ), # a(ss)
+        ( "includes", [ "" ], ),              # as
         )
-    DBUS_SIGNATURE = '(sssa(ss)asa{ss}asa(ss))'
+    DBUS_SIGNATURE = '(sssa(ss)asa{ss}asa(ss)as)'
     ADDITIONAL_ALNUM_CHARS = [ "_", "-" ]
     PARSER_REQUIRED_ELEMENT_ATTRS = {
         "short": None,
@@ -60,6 +61,7 @@ class Service(IO_Object):
         "module": [ "name" ],
         "destination": [ "ipv4", "ipv6" ],
         "source-port": [ "port", "protocol" ],
+        "include": [ "service" ],
         }
 
     def __init__(self):
@@ -72,6 +74,7 @@ class Service(IO_Object):
         self.modules = [ ]
         self.destination = { }
         self.source_ports = [ ]
+        self.includes = [ ]
 
     def cleanup(self):
         self.version = ""
@@ -82,6 +85,7 @@ class Service(IO_Object):
         del self.modules[:]
         self.destination.clear()
         del self.source_ports[:]
+        del self.includes[:]
 
     def encode_strings(self):
         """ HACK. I haven't been able to make sax parser return
@@ -96,6 +100,7 @@ class Service(IO_Object):
         self.protocols = [u2b_if_py2(pr) for pr in self.protocols]
         self.source_ports = [(u2b_if_py2(po),u2b_if_py2(pr)) for (po,pr)
                              in self.source_ports]
+        self.includes = [u2b_if_py2(s) for s in self.includes]
 
     def _check_config(self, config, item):
         if item == "ports":
@@ -132,6 +137,9 @@ class Service(IO_Object):
                         module = module.replace("_", "-")
                 if len(module) < 2:
                     raise FirewallError(errors.INVALID_MODULE, module)
+
+        elif item == "includes":
+            pass
 
 # PARSER
 
@@ -202,6 +210,12 @@ class service_ContentHandler(IO_Object_ContentHandler):
             else:
                 log.warning("Module '%s' already set, ignoring.",
                             module)
+        elif name == "include":
+            if attrs["service"] not in self.item.includes:
+                self.item.includes.append(attrs["service"])
+            else:
+                log.warning("Include '%s' already set, ignoring.",
+                            attrs["service"])
 
 
 def service_reader(filename, path):
@@ -310,6 +324,12 @@ def service_writer(service, path=None):
     if len(service.destination) > 0:
         handler.ignorableWhitespace("  ")
         handler.simpleElement("destination", service.destination)
+        handler.ignorableWhitespace("\n")
+
+    # includes
+    for include in service.includes:
+        handler.ignorableWhitespace("  ")
+        handler.simpleElement("include", { "service": include })
         handler.ignorableWhitespace("\n")
 
     # end service element

commit 632380e6efcdfd2282216002bd92f8f6d96467b9

fix: client: service: use dict based dbus APIs
Fixes: 1fc208bf9317 ("feat: service includes")

src/firewall/client.py
@@ -818,14 +818,45 @@ class FirewallClientConfigZone(object):
 class FirewallClientServiceSettings(object):
     @handle_exceptions
     def __init__(self, settings=None):
+        self.settings = ["", "", "", [], [], {}, [], [], []]
+        self.settings_name = ["version", "short", "description", "ports",
+                              "modules", "destination", "protocols",
+                              "source_ports", "includes"]
+        self.settings_dbus_type = ["s", "s", "s", "(ss)",
+                                   "s", "ss", "s",
+                                   "(ss)", "s"]
         if settings:
-            self.settings = settings
-        else:
-            self.settings = ["", "", "", [], [], {}, [], [], []]
+            if type(settings) is list:
+                for i,v in enumerate(settings):
+                    self.settings[i] = settings[i]
+            elif type(settings) is dict:
+                self.setSettingsDict(settings)
 
     @handle_exceptions
     def __repr__(self):
         return '%s(%r)' % (self.__class__, self.settings)
+
+    @handle_exceptions
+    def getSettingsDict(self):
+        settings = {}
+        for key,value in zip(self.settings_name, self.settings):
+            settings[key] = value
+        return settings
+    @handle_exceptions
+    def setSettingsDict(self, settings):
+        for key in settings:
+            self.settings[self.settings_name.index(key)] = settings[key]
+    @handle_exceptions
+    def getSettingsDbusDict(self):
+        settings = {}
+        for key,value,sig in zip(self.settings_name, self.settings, self.settings_dbus_type):
+            if type(value) is list:
+                settings[key] = dbus.Array(value, signature=sig)
+            elif type(value) is dict:
+                settings[key] = dbus.Dictionary(value, signature=sig)
+            else:
+                settings[key] = value
+        return settings
 
     @handle_exceptions
     def getVersion(self):
@@ -1467,13 +1498,13 @@ class FirewallClientConfigService(object):
     @slip.dbus.polkit.enable_proxy
     @handle_exceptions
     def getSettings(self):
-        return FirewallClientServiceSettings(list(dbus_to_python(\
-                    self.fw_service.getSettings())))
+        return FirewallClientServiceSettings(dbus_to_python(
+                    self.fw_service.getSettings2()))
 
     @slip.dbus.polkit.enable_proxy
     @handle_exceptions
     def update(self, settings):
-        self.fw_service.update(tuple(settings.settings))
+        self.fw_service.update2(settings.getSettingsDbusDict())
 
     @slip.dbus.polkit.enable_proxy
     @handle_exceptions
@@ -2431,7 +2462,9 @@ class FirewallClientConfig(object):
     @handle_exceptions
     def addService(self, name, settings):
         if isinstance(settings, FirewallClientServiceSettings):
-            path = self.fw_config.addService(name, tuple(settings.settings))
+            path = self.fw_config.addService2(name, settings.getSettingsDbusDict())
+        elif type(settings) is dict:
+            path = self.fw_config.addService2(name, settings)
         else:
             path = self.fw_config.addService(name, tuple(settings))
         return FirewallClientConfigService(self.bus, path)
@@ -2899,8 +2932,8 @@ class FirewallClient(object):
     @slip.dbus.polkit.enable_proxy
     @handle_exceptions
     def getServiceSettings(self, service):
-        return FirewallClientServiceSettings(list(dbus_to_python(\
-                    self.fw.getServiceSettings(service))))
+        return FirewallClientServiceSettings(dbus_to_python(
+                    self.fw.getServiceSettings2(service)))
 
     @slip.dbus.polkit.enable_proxy
     @handle_exceptions