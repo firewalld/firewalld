firewalld/firewalld

commit fce80236bf38dcdfa4a66cd86e6dc03dd08d7f03

Add "RFC3964_IPv4" config option
As per RFC 3964, filter IPv6 traffic with 6to4 destination addresses
that correspond to IPv4 addresses that should not be routed over the
public internet.

Defaults to "yes".

src/firewall/core/fw.py
@@ -309,6 +309,15 @@ class Firewall(object):
                 log.debug1("FlushAllOnReload is set to '%s'",
                            self._flush_all_on_reload)
 
+            if self._firewalld_conf.get("RFC3964_IPv4"):
+                value = self._firewalld_conf.get("RFC3964_IPv4")
+                if value.lower() in [ "no", "false" ]:
+                    self._rfc3964_ipv4 = False
+                else:
+                    self._rfc3964_ipv4 = True
+                log.debug1("RFC3964_IPv4 is set to '%s'",
+                           self._rfc3964_ipv4)
+
         self.config.set_firewalld_conf(copy.deepcopy(self._firewalld_conf))
 
         self._select_firewall_backend(self._firewall_backend)

config/firewalld.conf
@@ -70,3 +70,10 @@ FirewallBackend=nftables
 # behavior set this to "no".
 # Default: yes
 FlushAllOnReload=yes
+
+# RFC3964_IPv4
+# As per RFC 3964, filter IPv6 traffic with 6to4 destination addresses that
+# correspond to IPv4 addresses that should not be routed over the public
+# internet.
+# Defaults to "yes".
+RFC3964_IPv4=yes

doc/xml/firewalld.conf.xml
@@ -171,6 +171,18 @@
             </listitem>
         </varlistentry>
 
+        <varlistentry>
+            <term><option>RFC3964_IPv4</option></term>
+            <listitem>
+                <para>
+                As per RFC 3964, filter IPv6 traffic with 6to4 destination
+                addresses that correspond to IPv4 addresses that should not
+                be routed over the public internet.
+                Defaults to "yes".
+                </para>
+            </listitem>
+        </varlistentry>
+
     </variablelist>
 
   </refsect1>

doc/xml/firewalld.dbus.xml
@@ -2635,6 +2635,16 @@
               </para>
             </listitem>
           </varlistentry>
+          <varlistentry id="FirewallD1.config.Properties.RFC3964_IPv4">
+            <term>FirewallBackend - s - (rw)</term>
+            <listitem>
+              <para>
+                As per RFC 3964, filter IPv6 traffic with 6to4 destination
+                addresses that correspond to IPv4 addresses that should not be
+                routed over the public internet. Valid options are; yes, no.
+              </para>
+            </listitem>
+          </varlistentry>
         </variablelist>
       </refsect3>
     </refsect2>

src/firewall/config/__init__.py.in
@@ -131,3 +131,4 @@ FALLBACK_LOG_DENIED = "off"
 FALLBACK_AUTOMATIC_HELPERS = "system"
 FALLBACK_FIREWALL_BACKEND = "nftables"
 FALLBACK_FLUSH_ALL_ON_RELOAD = True
+FALLBACK_RFC3964_IPV4 = True

src/firewall/core/io/firewalld_conf.py
@@ -30,7 +30,8 @@ from firewall.functions import b2u, u2b, PY2
 
 valid_keys = [ "DefaultZone", "MinimalMark", "CleanupOnExit", "Lockdown", 
                "IPv6_rpfilter", "IndividualCalls", "LogDenied",
-               "AutomaticHelpers", "FirewallBackend", "FlushAllOnReload" ]
+               "AutomaticHelpers", "FirewallBackend", "FlushAllOnReload",
+               "RFC3964_IPv4" ]
 
 class firewalld_conf(object):
     def __init__(self, filename):
@@ -81,6 +82,7 @@ class firewalld_conf(object):
             self.set("AutomaticHelpers", config.FALLBACK_AUTOMATIC_HELPERS)
             self.set("FirewallBackend", config.FALLBACK_FIREWALL_BACKEND)
             self.set("FlushAllOnReload", "yes" if config.FALLBACK_FLUSH_ALL_ON_RELOAD else "no")
+            self.set("RFC3964_IPv4", "yes" if config.FALLBACK_RFC3964_IPV4 else "no")
             raise
 
         for line in f:
@@ -191,6 +193,14 @@ class firewalld_conf(object):
                             "value %s", value if value else '',
                             config.FALLBACK_FLUSH_ALL_ON_RELOAD)
             self.set("FlushAllOnReload", str(config.FALLBACK_FLUSH_ALL_ON_RELOAD))
+
+        value = self.get("RFC3964_IPv4")
+        if not value or value.lower() not in [ "yes", "true", "no", "false" ]:
+            if value is not None:
+                log.warning("RFC3964_IPv4 '%s' is not valid, using default "
+                            "value %s", value if value else '',
+                            config.FALLBACK_RFC3964_IPV4)
+            self.set("RFC3964_IPv4", str(config.FALLBACK_RFC3964_IPV4))
 
     # save to self.filename if there are key/value changes
     def write(self):

src/firewall/server/config.py
@@ -107,6 +107,7 @@ class FirewallDConfig(slip.dbus.service.Object):
                                                 "AutomaticHelpers": "readwrite",
                                                 "FirewallBackend": "readwrite",
                                                 "FlushAllOnReload": "readwrite",
+                                                "RFC3964_IPv4": "readwrite",
                                               })
 
     @handle_exceptions
@@ -487,7 +488,7 @@ class FirewallDConfig(slip.dbus.service.Object):
         if prop not in [ "DefaultZone", "MinimalMark", "CleanupOnExit",
                          "Lockdown", "IPv6_rpfilter", "IndividualCalls",
                          "LogDenied", "AutomaticHelpers", "FirewallBackend",
-                         "FlushAllOnReload" ]:
+                         "FlushAllOnReload", "RFC3964_IPv4" ]:
             raise dbus.exceptions.DBusException(
                 "org.freedesktop.DBus.Error.InvalidArgs: "
                 "Property '%s' does not exist" % prop)
@@ -536,6 +537,10 @@ class FirewallDConfig(slip.dbus.service.Object):
             if value is None:
                 value = "yes" if config.FALLBACK_FLUSH_ALL_ON_RELOAD else "no"
             return dbus.String(value)
+        elif prop == "RFC3964_IPv4":
+            if value is None:
+                value = "yes" if config.FALLBACK_RFC3964_IPV4 else "no"
+            return dbus.String(value)
 
     @dbus_handle_exceptions
     def _get_dbus_property(self, prop):
@@ -558,6 +563,8 @@ class FirewallDConfig(slip.dbus.service.Object):
         elif prop == "FirewallBackend":
             return dbus.String(self._get_property(prop))
         elif prop == "FlushAllOnReload":
+            return dbus.String(self._get_property(prop))
+        elif prop == "RFC3964_IPv4":
             return dbus.String(self._get_property(prop))
         else:
             raise dbus.exceptions.DBusException(
@@ -599,7 +606,7 @@ class FirewallDConfig(slip.dbus.service.Object):
             for x in [ "DefaultZone", "MinimalMark", "CleanupOnExit",
                        "Lockdown", "IPv6_rpfilter", "IndividualCalls",
                        "LogDenied", "AutomaticHelpers", "FirewallBackend",
-                       "FlushAllOnReload" ]:
+                       "FlushAllOnReload", "RFC3964_IPv4" ]:
                 ret[x] = self._get_property(x)
         elif interface_name in [ config.dbus.DBUS_INTERFACE_CONFIG_DIRECT,
                                  config.dbus.DBUS_INTERFACE_CONFIG_POLICIES ]:
@@ -626,7 +633,8 @@ class FirewallDConfig(slip.dbus.service.Object):
             if property_name in [ "MinimalMark", "CleanupOnExit", "Lockdown",
                                   "IPv6_rpfilter", "IndividualCalls",
                                   "LogDenied", "AutomaticHelpers",
-                                  "FirewallBackend", "FlushAllOnReload" ]:
+                                  "FirewallBackend", "FlushAllOnReload",
+                                  "RFC3964_IPv4" ]:
                 if property_name == "MinimalMark":
                     try:
                         int(new_value)
@@ -665,6 +673,12 @@ class FirewallDConfig(slip.dbus.service.Object):
                         raise FirewallError(errors.INVALID_VALUE,
                                             "'%s' for %s" % \
                                             (new_value, property_name))
+                if property_name == "RFC3964_IPv4":
+                    if new_value.lower() not in ["yes", "true", "no", "false"]:
+                        raise FirewallError(errors.INVALID_VALUE,
+                                            "'%s' for %s" % \
+                                            (new_value, property_name))
+
                 self.config.get_firewalld_conf().set(property_name, new_value)
                 self.config.get_firewalld_conf().write()
                 self.PropertiesChanged(interface_name,

src/tests/dbus/firewalld.conf.at
@@ -14,6 +14,7 @@ string "IndividualCalls" : variant string "no"
 string "Lockdown" : variant string "no"
 string "LogDenied" : variant string "off"
 string "MinimalMark" : variant int32 100
+string "RFC3964_IPv4" : variant string "yes"
 ])
 
 m4_define([_helper], [
@@ -33,6 +34,7 @@ _helper([IndividualCalls], [string:"yes"], [variant string "yes"])
 _helper([FirewallBackend], [string:"iptables"], [variant string "iptables"])
 _helper([FlushAllOnReload], [string:"no"], [variant string "no"])
 _helper([CleanupOnExit], [string:"yes"], [variant string "yes"])
+_helper([RFC3964_IPv4], [string:"no"], [variant string "no"])
 dnl Note: DefaultZone is RO
 m4_undefine([_helper])
 

commit 56a6e4a5431198a46e919e7adabc3013dfb91c1f

fix: fw: initialize _rfc3964_ipv4
If not initialized, this missing value throws an exception when no
config file is present.

Fixes: fce80236bf38 ("Add "RFC3964_IPv4" config option")
Closes: #509

src/firewall/core/fw.py
@@ -124,6 +124,7 @@ class Firewall(object):
         self._automatic_helpers = config.FALLBACK_AUTOMATIC_HELPERS
         self._firewall_backend = config.FALLBACK_FIREWALL_BACKEND
         self._flush_all_on_reload = config.FALLBACK_FLUSH_ALL_ON_RELOAD
+        self._rfc3964_ipv4 = config.FALLBACK_RFC3964_IPV4
         self.nf_conntrack_helper_setting = 0
         self.nf_conntrack_helpers = { }
         self.nf_nat_helpers = { }