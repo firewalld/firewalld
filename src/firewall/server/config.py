firewalld/firewalld

commit 174005b15059db054b2f8dcf3b35c23fcbaf44ec

fix: direct: removeRules() was mistakenly removing all rules
Only remove the rules that match the specified criteria (ipv, table,
chain).

Fixes: #385
Fixes: rhbz 1723610

src/firewall/server/config.py
@@ -1422,7 +1422,9 @@ class FirewallDConfig(slip.dbus.service.Object):
                    (ipv, table, chain, ))
         self.accessCheck(sender)
         settings = list(self.getSettings())
-        settings[1] = []
+        for rule in settings[1]:
+            if (ipv, table, chain) == (rule[0], rule[1], rule[2]):
+                settings[1].remove(rule)
         self.update(tuple(settings))
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_DIRECT,

commit 083d6527ad9c60442e424172e223b65132bc6d17

fix: direct: removeRules() not removing all rules in chain
Fixes: 174005b15059 ("fix: direct: removeRules() was mistakenly removing all rules")

src/firewall/server/config.py
@@ -1422,7 +1422,7 @@ class FirewallDConfig(slip.dbus.service.Object):
                    (ipv, table, chain, ))
         self.accessCheck(sender)
         settings = list(self.getSettings())
-        for rule in settings[1]:
+        for rule in settings[1][:]:
             if (ipv, table, chain) == (rule[0], rule[1], rule[2]):
                 settings[1].remove(rule)
         self.update(tuple(settings))