firewalld/firewalld

commit 8c65bda2a750c1b1a15851a6030dfef8cdb74d15

fix: allow custom helpers using standard helper modules
e.g. a helper called "ftp-foobar" using module "nf_conntrack_ftp"

src/firewall/core/fw_zone.py
@@ -1588,8 +1588,9 @@ class FirewallZone(object):
                         modules = [ ]
                         for helper in helpers:
                             module = helper.module
+                            _module_short_name = module.replace("-","_").replace("nf_conntrack_", "")
                             if self._fw.nf_conntrack_helper_setting == 0:
-                                if helper.name not in \
+                                if _module_short_name not in \
                                    self._fw.nf_conntrack_helpers[module]:
                                     raise FirewallError(
                                         errors.INVALID_HELPER,
@@ -1606,7 +1607,7 @@ class FirewallZone(object):
                                     for (port,proto) in helper.ports:
                                         rules = backend.build_zone_helper_ports_rules(
                                                         enable, zone, proto, port,
-                                                        destination, helper.name)
+                                                        destination, helper.name, _module_short_name)
                                         zone_transaction.add_rules(backend, rules)
                             else:
                                 if helper.module not in modules:
@@ -1803,7 +1804,8 @@ class FirewallZone(object):
             if self._fw.nf_conntrack_helper_setting == 0:
                 for helper in helpers:
                     module = helper.module
-                    if helper.name not in \
+                    _module_short_name = module.replace("-","_").replace("nf_conntrack_", "")
+                    if _module_short_name not in \
                        self._fw.nf_conntrack_helpers[module]:
                         raise FirewallError(
                             errors.INVALID_HELPER,
@@ -1820,7 +1822,7 @@ class FirewallZone(object):
                         for (port,proto) in helper.ports:
                             rules = backend.build_zone_helper_ports_rules(
                                             enable, zone, proto, port,
-                                            destination, helper.name)
+                                            destination, helper.name, _module_short_name)
                             zone_transaction.add_rules(backend, rules)
 
             for (port,proto) in svc.ports:

src/firewall/core/ipXtables.py
@@ -1120,7 +1120,7 @@ class ip4tables(object):
         return rules
 
     def build_zone_helper_ports_rules(self, enable, zone, proto, port,
-                                      destination, helper_name):
+                                      destination, helper_name, module_short_name):
         add_del = { True: "-A", False: "-D" }[enable]
         target = DEFAULT_ZONE_TARGET.format(chain=SHORTCUTS["PREROUTING"],
                                             zone=zone)
@@ -1129,7 +1129,7 @@ class ip4tables(object):
             rule += [ "--dport", "%s" % portStr(port) ]
         if destination:
             rule += [ "-d",  destination ]
-        rule += [ "-j", "CT", "--helper", helper_name ]
+        rule += [ "-j", "CT", "--helper", module_short_name ]
 
         return [rule]
 

src/firewall/core/nftables.py
@@ -1213,7 +1213,7 @@ class nftables(object):
         return rules
 
     def build_zone_helper_ports_rules(self, enable, zone, proto, port,
-                                      destination, helper_name):
+                                      destination, helper_name, module_short_name):
         add_del = { True: "add", False: "delete" }[enable]
         rules = []
 
@@ -1221,7 +1221,7 @@ class nftables(object):
             rules.append({"add": {"ct helper": {"family": "inet",
                                                 "table": TABLE_NAME,
                                                 "name": "helper-%s-%s" % (helper_name, proto),
-                                                "type": helper_name,
+                                                "type": module_short_name,
                                                 "protocol": proto}}})
 
         target = DEFAULT_ZONE_TARGET.format(chain=SHORTCUTS["INPUT"],

commit 28f3e6a83167ca2798157fd6e2c752b296c72830

fix: service: usage of helpers with '-' in name
Fixes: 8c65bda2a750 ("fix: allow custom helpers using standard helper modules")

src/firewall/core/fw_zone.py
@@ -25,7 +25,7 @@ from firewall.core.base import SHORTCUTS, DEFAULT_ZONE_TARGET, \
 from firewall.core.logger import log
 from firewall.functions import portStr, checkIPnMask, checkIP6nMask, \
     checkProtocol, enable_ip_forwarding, check_single_address, check_mac, \
-    portInPortRange
+    portInPortRange, get_nf_conntrack_short_name
 from firewall.core.rich import Rich_Rule, Rich_Accept, \
     Rich_Mark, Rich_Service, Rich_Port, Rich_Protocol, \
     Rich_Masquerade, Rich_ForwardPort, Rich_SourcePort, Rich_IcmpBlock, \
@@ -1588,7 +1588,7 @@ class FirewallZone(object):
                         modules = [ ]
                         for helper in helpers:
                             module = helper.module
-                            _module_short_name = module.replace("-","_").replace("nf_conntrack_", "")
+                            _module_short_name = get_nf_conntrack_short_name(module)
                             if self._fw.nf_conntrack_helper_setting == 0:
                                 if _module_short_name not in \
                                    self._fw.nf_conntrack_helpers[module]:
@@ -1804,7 +1804,7 @@ class FirewallZone(object):
             if self._fw.nf_conntrack_helper_setting == 0:
                 for helper in helpers:
                     module = helper.module
-                    _module_short_name = module.replace("-","_").replace("nf_conntrack_", "")
+                    _module_short_name = get_nf_conntrack_short_name(module)
                     if _module_short_name not in \
                        self._fw.nf_conntrack_helpers[module]:
                         raise FirewallError(

src/firewall/functions.py
@@ -346,6 +346,9 @@ def enable_ip_forwarding(ipv):
         return writefile("/proc/sys/net/ipv6/conf/all/forwarding", "1\n")
     return False
 
+def get_nf_conntrack_short_name(module):
+    return module.replace("_","-").replace("nf-conntrack-", "")
+
 def get_modinfos(path_templates, prefix):
     kver = os.uname()[2]
     modules = []
@@ -376,9 +379,7 @@ def get_nf_conntrack_helpers():
         # If module name matches "nf_conntrack_proto_*"
         # the we add it to helpers list and goto next module
         if filename.startswith("nf_conntrack_proto_"):
-            helper = name
-            helper = helper.replace("_", "-")
-            helper = helper.replace("nf-conntrack-", "")
+            helper = get_nf_conntrack_short_name(name)
             helpers.setdefault(name, [ ]).append(helper)
             continue
         # Else we get module alias and if "-helper" in the "alias:" line of modinfo