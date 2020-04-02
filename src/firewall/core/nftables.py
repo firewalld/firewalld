firewalld/firewalld

commit 2d1b0fe9fe74b9453714f03b0c118a29d4851b76

fix: nftables: allow set intervals with concatenations
nftables recently gained support for intervals + concatenations. This
allows the equivalent of ipset hash:net,port and others. Now that
nftables supports these we can lift the restriction in firewalld.

We now use the "interval" flag for any type that uses any of: net, port.

We now also do _not_ use it for types that were mistakenly using it,
e.g. hash:ip. We don't allow the ipset option "netmask" so hash:ip can't
actually result in ranges/intervals.

nftables set intervals with concatenations requires:
    - requires kernel >= 5.6
    - requires nftables >= 0.9.4

We're not bumping firewalld's required versions for these dependencies
because it's only relevant to multidimensional ipsets.

Fixes: rhbz 1685256

src/firewall/core/nftables.py
@@ -1589,15 +1589,18 @@ class nftables(object):
         set_dict = {"table": TABLE_NAME,
                     "name": name,
                     "type": self._set_type_list(ipv, type)}
+
+        # Some types need the interval flag
+        for t in type.split(":")[1].split(","):
+            if t in ["net", "port"]:
+                set_dict["flags"] = ["interval"]
+                break
+
         if options:
             if "timeout" in options:
                 set_dict["timeout"] = options["timeout"]
             if "maxelem" in options:
                 set_dict["size"] = options["maxelem"]
-        # flag "interval" currently does not work with concatenations.
-        # See rhbz 1576426.
-        if "," not in type: # e.g. hash:net,port
-            set_dict["flags"] = ["interval"]
 
         rules = []
         for family in ["inet", "ip", "ip6"]:
@@ -1660,10 +1663,18 @@ class nftables(object):
                 except ValueError:
                     # no protocol means default tcp
                     fragment.append("tcp")
-                    fragment.append(entry_tokens[i])
+                    port_str = entry_tokens[i]
                 else:
                     fragment.append(entry_tokens[i][:index])
-                    fragment.append(entry_tokens[i][index+1:])
+                    port_str = entry_tokens[i][index+1:]
+
+                try:
+                    index = entry_tokens[i].index("-")
+                except ValueError:
+                    fragment.append(port_str)
+                else:
+                    fragment.append({"range": [port_str[:index], port_str[index+1:]]})
+
             elif type_format[i] in ["ip", "net"]:
                 try:
                     index = entry_tokens[i].index("/")

src/tests/cli/firewall-cmd.at
@@ -735,6 +735,7 @@ FWD_START_TEST([ipset])
         table inet firewalld {
             set foobar {
                 type ipv4_addr . inet_proto . inet_service
+                flags interval
                 elements = { 10.10.10.10 . sctp . 1234,
                              20.20.20.20 . tcp . 8080 }
             }

src/tests/regression/gh330.at
@@ -17,7 +17,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4 }
         }
     }
@@ -44,7 +43,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4, 10.10.10.10 }
         }
     }
@@ -62,7 +60,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4, 10.10.10.10 }
         }
     }
@@ -83,7 +80,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4, 4.3.2.1,
                          10.10.10.10 }
         }
@@ -108,7 +104,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4, 4.3.2.1,
                          6.6.6.6, 10.10.10.10 }
         }
@@ -134,7 +129,6 @@ NFT_LIST_SET([foobar], 0, [dnl
     table inet firewalld {
         set foobar {
             type ipv4_addr
-            flags interval
             elements = { 1.2.3.4 }
         }
     }

src/tests/regression/rhbz1734765.at
@@ -47,7 +47,6 @@ NFT_LIST_SET([ipsetv4], 0, [dnl
     table inet firewalld {
         set ipsetv4 {
             type ipv4_addr
-            flags interval
             elements = { 192.0.2.12 }
         }
     }
@@ -56,7 +55,6 @@ NFT_LIST_SET([ipsetv6], 0, [dnl
     table inet firewalld {
         set ipsetv6 {
             type ipv6_addr
-            flags interval
             elements = { ::2 }
         }
     }

commit e80f4fccfc771128affdc578ed37842d5d469ca9

fix: nftables: ipset: port ranges for non-default protocols
Fixes: 2d1b0fe9fe74 ("fix: nftables: allow set intervals with concatenations")

src/firewall/core/nftables.py
@@ -1669,7 +1669,7 @@ class nftables(object):
                     port_str = entry_tokens[i][index+1:]
 
                 try:
-                    index = entry_tokens[i].index("-")
+                    index = port_str.index("-")
                 except ValueError:
                     fragment.append(port_str)
                 else: