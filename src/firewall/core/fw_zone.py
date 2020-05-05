firewalld/firewalld

commit 9bcf735a46f99a163f47738944ea6c6782195aa7

feat(zone): allow forwarding between interfaces and sources in a zone
This introduces a new boolean flag called "forward" on zones, similar to
the existing "masquerade" setting.

When the "forward" setting is enabled for a zone, the firewall rules
will allow packets to be forwarded between interfaces within that zone,
but not to other zones. Unlike the "masquerade" setting, no other NAT
rules are applied.

This gives a rudimentary kind of configurable forwarding. You can
control whether packets will be forwarded by arranging your interfaces
into zones and turning on this flag.

Closes: #586

src/firewall/core/fw_zone.py
@@ -119,7 +119,8 @@ class FirewallZone(object):
     def add_zone(self, obj):
         obj.settings = { x : LastUpdatedOrderedDict()
                          for x in ["interfaces", "sources",
-                                   "icmp_block_inversion"] }
+                                   "icmp_block_inversion",
+                                   "forward"] }
         self._zones[obj.name] = obj
         self._zone_policies[obj.name] = []
 
@@ -147,6 +148,8 @@ class FirewallZone(object):
             self.add_interface(zone, arg, allow_apply=False)
         for arg in obj.sources:
             self.add_source(zone, arg, allow_apply=False)
+        if obj.forward:
+            self.add_forward(zone)
         if obj.icmp_block_inversion:
             self.add_icmp_block_inversion(zone)
 
@@ -255,6 +258,8 @@ class FirewallZone(object):
                     self._source(enable, zone, args[0], args[1], transaction)
                 elif key == "icmp_block_inversion":
                     continue
+                elif key == "forward":
+                    self._forward(enable, zone, args, transaction)
                 else:
                     log.warning("Zone '%s': Unknown setting '%s:%s', "
                                 "unable to apply", zone, key, args)
@@ -336,6 +341,7 @@ class FirewallZone(object):
                             "protocols": self.list_protocols(zone),
                             "source_ports": self.list_source_ports(zone),
                             "icmp_block_inversion": self.query_icmp_block_inversion(zone),
+                            "forward": self.query_forward(zone),
                             }.items():
             # omit empty entries
             if value or isinstance(value, bool):
@@ -362,6 +368,7 @@ class FirewallZone(object):
             "protocols": (self.add_protocol, self.remove_protocol),
             "source_ports": (self.add_source_port, self.remove_source_port),
             "icmp_block_inversion": (self.add_icmp_block_inversion, self.remove_icmp_block_inversion),
+            "forward": (self.add_forward, self.remove_forward),
         }
 
         old_settings = self.get_config_with_settings_dict(zone)
@@ -691,6 +698,18 @@ class FirewallZone(object):
                                     zone, policy, interface, table, chain, append)
                     transaction.add_rules(backend, rules)
 
+            # intra zone forward
+            policy = self.policy_name_from_zones(zone, "ANY")
+            if enable:
+                transaction.add_chain(policy, "filter", "FORWARD_IN")
+            # Skip adding wildcard/catch-all interface (for default
+            # zone). Otherwise it would allow forwarding from interface
+            # in default zone -> interface not in default zone (but in
+            # a different zone).
+            if self.get_settings(zone)["forward"] and interface not in ["+", "*"]:
+                rules = backend.build_zone_forward_rules(enable, zone, policy, "filter", interface=interface)
+                transaction.add_rules(backend, rules)
+
     # IPSETS
 
     def _ipset_family(self, name):
@@ -730,6 +749,14 @@ class FirewallZone(object):
                     rules = backend.build_zone_source_address_rules(enable, zone,
                                             policy, source, table, chain)
                     transaction.add_rules(backend, rules)
+
+            # intra zone forward
+            policy = self.policy_name_from_zones(zone, "ANY")
+            if enable:
+                transaction.add_chain(policy, "filter", "FORWARD_IN")
+            if self.get_settings(zone)["forward"]:
+                rules = backend.build_zone_forward_rules(enable, zone, policy, "filter", source=source)
+                transaction.add_rules(backend, rules)
 
     def add_service(self, zone, service, timeout=0, sender=None):
         zone = self._fw.check_zone(zone)
@@ -971,3 +998,89 @@ class FirewallZone(object):
         p_name_fwd = self.policy_name_from_zones(zone, "ANY")
         return self._fw.policy.query_icmp_block_inversion(p_name_host) and \
                self._fw.policy.query_icmp_block_inversion(p_name_fwd)
+
+    def _forward(self, enable, zone, transaction):
+        p_name = self.policy_name_from_zones(zone, "ANY")
+        if enable:
+            transaction.add_chain(p_name, "filter", "FORWARD_IN")
+
+            for interface in self._zones[zone].settings["interfaces"]:
+                for backend in self._fw.enabled_backends():
+                    if not backend.policies_supported:
+                        continue
+                    rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", interface=interface)
+                    transaction.add_rules(backend, rules)
+
+            for ipv,source in self._zones[zone].settings["sources"]:
+                for backend in [self._fw.get_backend_by_ipv(ipv)] if ipv else self._fw.enabled_backends():
+                    if not backend.policies_supported:
+                        continue
+                    rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", source=source)
+                    transaction.add_rules(backend, rules)
+
+    def __forward_id(self):
+        return True
+
+    def add_forward(self, zone, timeout=0, sender=None,
+                    use_transaction=None):
+        _zone = self._fw.check_zone(zone)
+        self._fw.check_timeout(timeout)
+        self._fw.check_panic()
+        _obj = self._zones[_zone]
+
+        forward_id = self.__forward_id()
+        if forward_id in _obj.settings["forward"]:
+            raise FirewallError(errors.ALREADY_ENABLED,
+                                "forward already enabled in '%s'" % _zone)
+
+        if use_transaction is None:
+            transaction = self.new_transaction()
+        else:
+            transaction = use_transaction
+
+        if _obj.applied:
+            self._forward(True, _zone, transaction)
+
+        self.__register_forward(_obj, forward_id, timeout, sender)
+        transaction.add_fail(self.__unregister_forward, _obj, forward_id)
+
+        if use_transaction is None:
+            transaction.execute(True)
+
+        return _zone
+
+    def __register_forward(self, _obj, forward_id, timeout, sender):
+        _obj.settings["forward"][forward_id] = \
+            self.__gen_settings(timeout, sender)
+
+    def remove_forward(self, zone, use_transaction=None):
+        _zone = self._fw.check_zone(zone)
+        self._fw.check_panic()
+        _obj = self._zones[_zone]
+
+        forward_id = self.__forward_id()
+        if forward_id not in _obj.settings["forward"]:
+            raise FirewallError(errors.NOT_ENABLED,
+                                "forward not enabled in '%s'" % _zone)
+
+        if use_transaction is None:
+            transaction = self.new_transaction()
+        else:
+            transaction = use_transaction
+
+        if _obj.applied:
+            self._forward(False, _zone, transaction)
+
+        transaction.add_post(self.__unregister_forward, _obj, forward_id)
+
+        if use_transaction is None:
+            transaction.execute(True)
+
+        return _zone
+
+    def __unregister_forward(self, _obj, forward_id):
+        if forward_id in _obj.settings["forward"]:
+            del _obj.settings["forward"][forward_id]
+
+    def query_forward(self, zone):
+        return self.__forward_id() in self.get_settings(zone)["forward"]

doc/xml/firewalld.dbus.xml
@@ -1300,6 +1300,7 @@
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>source_ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>icmp_block_inversion (b)</parameter>: see <literal>icmp-block-inversion</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>forward (b)</parameter>: see <literal>forward</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
               <para>
@@ -1328,6 +1329,7 @@
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>source_ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>icmp_block_inversion (b)</parameter>: see <literal>icmp-block-inversion</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>forward (b)</parameter>: see <literal>forward</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
               <para>
@@ -2415,6 +2417,7 @@
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>source_ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>icmp_block_inversion (b)</parameter>: see <literal>icmp-block-inversion</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>forward (b)</parameter>: see <literal>forward</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
               <para>
@@ -3897,6 +3900,7 @@
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>source_ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>icmp_block_inversion (b)</parameter>: see <literal>icmp-block-inversion</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>forward (b)</parameter>: see <literal>forward</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
             </listitem>
@@ -4404,6 +4408,7 @@
                   <varlistentry><term><parameter>protocols (as)</parameter>: array of protocols, see <literal>protocol</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>source_ports (a(ss))</parameter>: array of port and protocol pairs. See <literal>source-port</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                   <varlistentry><term><parameter>icmp_block_inversion (b)</parameter>: see <literal>icmp-block-inversion</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
+                  <varlistentry><term><parameter>forward (b)</parameter>: see <literal>forward</literal> tag in <citerefentry><refentrytitle>firewalld.zone</refentrytitle><manvolnum>5</manvolnum></citerefentry>.</term></varlistentry>
                 </variablelist>
               </para>
           <para>

shell-completion/bash/firewall-cmd
@@ -53,6 +53,7 @@ OPTIONS_ZONE_ACTION_ACTION="--add-service= --remove-service= --query-service= \
 OPTIONS_ZONE_ADAPT_QUERY="--add-rich-rule= --remove-rich-rule= --query-rich-rule= \
                     --add-icmp-block-inversion --remove-icmp-block-inversion \
                     --query-icmp-block-inversion \
+                    --add-forward --remove-forward --query-forward \
                     --add-masquerade --remove-masquerade --query-masquerade \
                     --list-services --list-ports --list-protocols \
                     --list-source-ports --list-icmp-blocks \
@@ -183,6 +184,7 @@ _firewall_cmd()
     --list-forward-ports|--add-forward-port=*|--remove-forward-port=*|--query-forward-port=*|\
     --list-interfaces|--add-interface=*|--remove-interface=*|--query-interface=*|\
     --list-sources|--add-source=*|--remove-source=*|--query-source=*|\
+    --add-forward|--remove-forward|--query-forward|\
     --add-masquerade|--remove-masquerade|--query-masquerade|--list-all|\
     --get-description|--get-short|--set-description=*|--set-short=*)
         opts=""

shell-completion/zsh/_firewalld
@@ -182,6 +182,9 @@ _arguments -C -s $args $direct \
   '*--add-forward-port=[add the IPv4 forward port for a zone]: :->port-forwards' \
   '*--remove-forward-port=[remove the IPv4 forward port from a zone]: :->port-forwards' \
   '*--query-forward-port=[return whether the IPv4 forward port has been added for a zone]: :->port-forwards' \
+  '--add-forward[enable forwarding between interfaces and sources in a zone]' \
+  '--remove-forward[disable forwarding between interfaces and sources in a zone]' \
+  '--query-forward[return whether forwarding has been enabled for a zone]' \
   '--add-masquerade[enable IPv4 masquerade for a zone]' \
   '--remove-masquerade[disable IPv4 masquerade for a zone]' \
   '--query-masquerade[return whether IPv4 masquerading has been enabled for a zone]' \

src/firewall-cmd.in
@@ -293,6 +293,12 @@ Options to Adapt and Query Zones
   --query-forward-port=port=<portid>[-<portid>]:proto=<protocol>[:toport=<portid>[-<portid>]][:toaddr=<address>[/<mask>]]
                        Return whether the IPv4 forward port has been added for
                        a zone [P] [Z]
+  --add-forward        Enable forwarding of packets between interfaces and
+                       sources in a zone [P] [Z] [T]
+  --remove-forward     Disable forwarding of packets between interfaces and
+                       sources in a zone [P] [Z]
+  --query-forward      Return whether forwarding of packets between interfaces
+                       and sources has been enabled for a zone [P] [Z]
   --add-masquerade     Enable IPv4 masquerade for a zone [P] [Z] [T]
   --remove-masquerade  Disable IPv4 masquerade for a zone [P] [Z]
   --query-masquerade   Return whether IPv4 masquerading has been enabled for a
@@ -617,6 +623,9 @@ parser_group_zone.add_argument("--query-protocol", metavar="<protocol>", action=
 parser_group_zone.add_argument("--add-source-port", metavar="<port>", action='append')
 parser_group_zone.add_argument("--remove-source-port", metavar="<port>", action='append')
 parser_group_zone.add_argument("--query-source-port", metavar="<port>", action='append')
+parser_group_zone.add_argument("--add-forward", action="store_true")
+parser_group_zone.add_argument("--remove-forward", action="store_true")
+parser_group_zone.add_argument("--query-forward", action="store_true")
 parser_group_zone.add_argument("--add-masquerade", action="store_true")
 parser_group_zone.add_argument("--remove-masquerade", action="store_true")
 parser_group_zone.add_argument("--query-masquerade", action="store_true")
@@ -825,6 +834,7 @@ options_zone_interfaces_sources = \
 
 options_zone_adapt_query = \
     a.add_rich_rule or a.remove_rich_rule or a.query_rich_rule or \
+    a.add_forward or a.remove_forward or a.query_forward or \
     a.add_masquerade or a.remove_masquerade or a.query_masquerade or \
     a.list_services or a.list_ports or a.list_protocols or \
     a.list_source_ports or \
@@ -1012,7 +1022,8 @@ else:
 
 if a.timeout and not (a.add_service or a.add_port or a.add_protocol or \
                       a.add_icmp_block or a.add_forward_port or \
-                      a.add_source_port or a.add_masquerade or a.add_rich_rule):
+                      a.add_source_port or a.add_forward or \
+                      a.add_masquerade or a.add_rich_rule):
     cmd.fail(parser.format_usage() + "Wrong --timeout usage")
 
 if a.permanent:
@@ -1999,6 +2010,14 @@ if a.permanent:
             cmd.query_sequence(a.query_source_port, fw_zone.querySourcePort,
                                cmd.parse_port, "%s/%s")
 
+        # forward
+        elif a.add_forward:
+            fw_zone.addForward()
+        elif a.remove_forward:
+            fw_zone.removeForward()
+        elif a.query_forward:
+            cmd.print_query_result(fw_zone.queryForward())
+
         # masquerade
         elif a.add_masquerade:
             fw_zone.addMasquerade()
@@ -2552,6 +2571,14 @@ elif a.remove_source_port:
 elif a.query_source_port:
     cmd.x_query_sequence(zone, a.query_source_port, fw.querySourcePort,
                          cmd.parse_port, "'%s/%s'")
+
+# forward
+elif a.add_forward:
+    fw.addForward(zone)
+elif a.remove_forward:
+    fw.removeForward(zone)
+elif a.query_forward:
+    cmd.print_query_result(fw.queryForward(zone))
 
 # masquerade
 elif a.add_masquerade:

src/firewall-offline-cmd.in
@@ -327,6 +327,12 @@ Options to Adapt and Query Zones
   --query-forward-port=port=<portid>[-<portid>]:proto=<protocol>[:toport=<portid>[-<portid>]][:toaddr=<address>[/<mask>]]
                        Return whether the IPv4 forward port has been added for
                        a zone [Z]
+  --add-forward        Enable forwarding of packets between interfaces and
+                       sources in a zone [P] [Z] [T]
+  --remove-forward     Disable forwarding of packets between interfaces and
+                       sources in a zone [P] [Z]
+  --query-forward      Return whether forwarding of packets between interfaces
+                       and sources has been enabled for a zone [P] [Z]
   --add-masquerade     Enable IPv4 masquerade for a zone [Z]
   --remove-masquerade  Disable IPv4 masquerade for a zone [Z]
   --query-masquerade   Return whether IPv4 masquerading has been enabled for a
@@ -653,6 +659,9 @@ parser_group_zone.add_argument("--query-protocol", metavar="<protocol>", action=
 parser_group_zone.add_argument("--add-source-port", metavar="<port>", action='append')
 parser_group_zone.add_argument("--remove-source-port", metavar="<port>", action='append')
 parser_group_zone.add_argument("--query-source-port", metavar="<port>", action='append')
+parser_group_zone.add_argument("--add-forward", action="store_true")
+parser_group_zone.add_argument("--remove-forward", action="store_true")
+parser_group_zone.add_argument("--query-forward", action="store_true")
 parser_group_zone.add_argument("--add-masquerade", action="store_true")
 parser_group_zone.add_argument("--remove-masquerade", action="store_true")
 parser_group_zone.add_argument("--query-masquerade", action="store_true")
@@ -898,6 +907,7 @@ options_zone_interfaces_sources = \
 
 options_zone_adapt_query = \
     a.add_rich_rule or a.remove_rich_rule or a.query_rich_rule or \
+    a.add_forward or a.remove_forward or a.query_forward or \
     a.add_masquerade or a.remove_masquerade or a.query_masquerade or \
     a.list_services or a.list_ports or a.list_protocols or \
     a.list_source_ports or \
@@ -2208,6 +2218,14 @@ try:
         elif a.query_source_port:
             cmd.query_sequence(a.query_source_port, fw_settings.querySourcePort,
                                cmd.parse_port, "%s/%s")
+
+        # forward
+        elif a.add_forward:
+            fw_settings.setForward(True)
+        elif a.remove_forward:
+            fw_settings.setForward(False)
+        elif a.query_forward:
+            cmd.print_query_result(fw_settings.getForward())
 
         # masquerade
         elif a.add_masquerade:

src/firewall/client.py
@@ -86,17 +86,19 @@ class FirewallClientZoneSettings(object):
     @handle_exceptions
     def __init__(self, settings = None):
         self.settings = ["", "", "", False, DEFAULT_ZONE_TARGET, [], [],
-                         [], False, [], [], [], [], [], [], False]
+                         [], False, [], [], [], [], [], [], False, False]
         self.settings_name = ["version", "short", "description", "UNUSED",
                               "target", "services", "ports",
                               "icmp_blocks", "masquerade", "forward_ports",
                               "interfaces", "sources", "rules_str",
-                              "protocols", "source_ports", "icmp_block_inversion"]
+                              "protocols", "source_ports", "icmp_block_inversion",
+                              "forward"]
         self.settings_dbus_type = ["s", "s", "s", "b",
                                    "s", "s", "(ss)",
                                    "s", "b", "(ssss)",
                                    "s", "s", "s",
-                                   "s", "(ss)", "b"]
+                                   "s", "(ss)", "b",
+                                   "b"]
         if settings:
             if isinstance(settings, list):
                 for i,v in enumerate(settings):
@@ -298,6 +300,31 @@ class FirewallClientZoneSettings(object):
     @handle_exceptions
     def queryIcmpBlockInversion(self):
         return self.settings[15]
+
+    @handle_exceptions
+    def getForward(self):
+        return self.settings[16]
+    @handle_exceptions
+    def setForward(self, forward):
+        self.settings[16] = forward
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def addForward(self):
+        if not self.settings[16]:
+            self.settings[16] = True
+        else:
+            FirewallError(errors.ALREADY_ENABLED, "forward")
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def removeForward(self):
+        if self.settings[16]:
+            self.settings[16] = False
+        else:
+            FirewallError(errors.NOT_ENABLED, "forward")
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def queryForward(self):
+        return self.settings[16]
 
     @handle_exceptions
     def getMasquerade(self):
@@ -698,6 +725,33 @@ class FirewallClientConfigZone(object):
     @handle_exceptions
     def queryIcmpBlockInversion(self):
         return self.fw_zone.queryIcmpBlockInversion()
+
+    # forward
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def getForward(self):
+        return self.fw_zone.getSettings2()["forward"]
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def setForward(self, forward):
+        self.fw_zone.update2({"forward": forward})
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def addForward(self):
+        self.fw_zone.update2({"forward": True})
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def removeForward(self):
+        self.fw_zone.update2({"forward": False})
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def queryForward(self):
+        return self.fw_zone.getSettings2()["forward"]
 
     # masquerade
 
@@ -3234,6 +3288,23 @@ class FirewallClient(object):
     @handle_exceptions
     def removeProtocol(self, zone, protocol):
         return dbus_to_python(self.fw_zone.removeProtocol(zone, protocol))
+
+    # forward
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def addForward(self, zone):
+        self.fw_zone.setZoneSettings2(zone, {"forward": True})
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def queryForward(self, zone):
+        return dbus_to_python(self.fw_zone.getZoneSettings2(zone))["forward"]
+
+    @slip.dbus.polkit.enable_proxy
+    @handle_exceptions
+    def removeForward(self, zone):
+        self.fw_zone.setZoneSettings2(zone, {"forward": False})
 
     # masquerade
 

src/firewall/command.py
@@ -385,6 +385,7 @@ class FirewallCommand(object):
         services = settings.getServices()
         ports = settings.getPorts()
         protocols = settings.getProtocols()
+        forward = settings.getForward()
         masquerade = settings.getMasquerade()
         forward_ports = settings.getForwardPorts()
         source_ports = settings.getSourcePorts()
@@ -427,6 +428,7 @@ class FirewallCommand(object):
         self.print_msg("  ports: " + " ".join(["%s/%s" % (port[0], port[1])
                                                for port in ports]))
         self.print_msg("  protocols: " + " ".join(sorted(protocols)))
+        self.print_msg("  forward: %s" % ("yes" if forward else "no"))
         self.print_msg("  masquerade: %s" % ("yes" if masquerade else "no"))
         self.print_msg("  forward-ports: " +
                        "\n\t".join(["port=%s:proto=%s:toport=%s:toaddr=%s" % \

commit 02766418401d9b01db11a2aaf51917b4a6dbdc41

fix(forward): should remove rules when disabled
Fixes: 9bcf735a46f9 ("feat(zone): allow forwarding between interfaces and sources in a zone")

src/firewall/core/fw_zone.py
@@ -1006,19 +1006,19 @@ class FirewallZone(object):
         if enable:
             transaction.add_chain(p_name, "filter", "FORWARD_IN")
 
-            for interface in self._zones[zone].settings["interfaces"]:
-                for backend in self._fw.enabled_backends():
-                    if not backend.policies_supported:
-                        continue
-                    rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", interface=interface)
-                    transaction.add_rules(backend, rules)
+        for interface in self._zones[zone].settings["interfaces"]:
+            for backend in self._fw.enabled_backends():
+                if not backend.policies_supported:
+                    continue
+                rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", interface=interface)
+                transaction.add_rules(backend, rules)
 
-            for ipv,source in self._zones[zone].settings["sources"]:
-                for backend in [self._fw.get_backend_by_ipv(ipv)] if ipv else self._fw.enabled_backends():
-                    if not backend.policies_supported:
-                        continue
-                    rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", source=source)
-                    transaction.add_rules(backend, rules)
+        for ipv,source in self._zones[zone].settings["sources"]:
+            for backend in [self._fw.get_backend_by_ipv(ipv)] if ipv else self._fw.enabled_backends():
+                if not backend.policies_supported:
+                    continue
+                rules = backend.build_zone_forward_rules(enable, zone, p_name, "filter", source=source)
+                transaction.add_rules(backend, rules)
 
     def __forward_id(self):
         return True