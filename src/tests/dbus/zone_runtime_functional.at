firewalld/firewalld

commit 67d057508f81bcb4d052473732a1bf182786378b

improvement(dbus): new dict based APIs for zones

src/firewall/core/fw_zone.py
@@ -306,21 +306,110 @@ class FirewallZone(object):
         """
         :return: exported config updated with runtime settings
         """
-        conf = list(self.get_zone(zone).export_config())
-        if conf[4] == DEFAULT_ZONE_TARGET:
-            conf[4] = "default"
-        conf[5] = self.list_services(zone)
-        conf[6] = self.list_ports(zone)
-        conf[7] = self.list_icmp_blocks(zone)
-        conf[8] = self.query_masquerade(zone)
-        conf[9] = self.list_forward_ports(zone)
-        conf[10] = self.list_interfaces(zone)
-        conf[11] = self.list_sources(zone)
-        conf[12] = self.list_rules(zone)
-        conf[13] = self.list_protocols(zone)
-        conf[14] = self.list_source_ports(zone)
-        conf[15] = self.query_icmp_block_inversion(zone)
-        return tuple(conf)
+        obj = self.get_zone(zone)
+        conf_dict = self.get_config_with_settings_dict(zone)
+        conf_list = []
+        for i in range(16): # tuple based API has 16 elements
+            if obj.IMPORT_EXPORT_STRUCTURE[i][0] not in conf_dict:
+                # old API needs the empty elements as well. Grab it from the
+                # class otherwise we don't know the type.
+                conf_list.append(copy.deepcopy(getattr(obj, obj.IMPORT_EXPORT_STRUCTURE[i][0])))
+            else:
+                conf_list.append(conf_dict[obj.IMPORT_EXPORT_STRUCTURE[i][0]])
+        return tuple(conf_list)
+
+    def get_config_with_settings_dict(self, zone):
+        """
+        :return: exported config updated with runtime settings
+        """
+        conf = self.get_zone(zone).export_config_dict()
+        if conf["target"] == DEFAULT_ZONE_TARGET:
+            conf["target"] = "default"
+        for key,value in {  "services": self.list_services(zone),
+                            "ports": self.list_ports(zone),
+                            "icmp_blocks": self.list_icmp_blocks(zone),
+                            "masquerade": self.query_masquerade(zone),
+                            "forward_ports": self.list_forward_ports(zone),
+                            "interfaces": self.list_interfaces(zone),
+                            "sources": self.list_sources(zone),
+                            "rules_str": self.list_rules(zone),
+                            "protocols": self.list_protocols(zone),
+                            "source_ports": self.list_source_ports(zone),
+                            "icmp_block_inversion": self.query_icmp_block_inversion(zone),
+                            }.items():
+            # omit empty entries
+            if value or isinstance(value, bool):
+                conf[key] = value
+        return conf
+
+    def set_config_with_settings_dict(self, zone, settings, sender):
+        # stupid wrappers to convert rich rule string to rich rule object
+        from firewall.core.rich import Rich_Rule
+        def add_rule_wrapper(zone, rule_str, timeout=0, sender=None):
+            self.add_rule(zone, Rich_Rule(rule_str=rule_str), timeout=0, sender=sender)
+        def remove_rule_wrapper(zone, rule_str):
+            self.remove_rule(zone, Rich_Rule(rule_str=rule_str))
+
+        setting_to_fn = {
+            "services": (self.add_service, self.remove_service),
+            "ports": (self.add_port, self.remove_port),
+            "icmp_blocks": (self.add_icmp_block, self.remove_icmp_block),
+            "masquerade": (self.add_masquerade, self.remove_masquerade),
+            "forward_ports": (self.add_forward_port, self.remove_forward_port),
+            "interfaces": (self.add_interface, self.remove_interface),
+            "sources": (self.add_source, self.remove_source),
+            "rules_str": (add_rule_wrapper, remove_rule_wrapper),
+            "protocols": (self.add_protocol, self.remove_protocol),
+            "source_ports": (self.add_source_port, self.remove_source_port),
+            "icmp_block_inversion": (self.add_icmp_block_inversion, self.remove_icmp_block_inversion),
+        }
+
+        old_settings = self.get_config_with_settings_dict(zone)
+        add_settings = {}
+        remove_settings = {}
+        for key in (set(old_settings.keys()) | set(settings.keys())):
+            if key in settings:
+                if isinstance(settings[key], list):
+                    old = set(old_settings[key] if key in old_settings else [])
+                    add_settings[key] = list(set(settings[key]) - old)
+                    remove_settings[key] = list((old ^ set(settings[key])) & old)
+                # check for bool or int because dbus.Boolean is a subclass of
+                # int (because bool can't be subclassed).
+                elif isinstance(settings[key], bool) or isinstance(settings[key], int):
+                    if not old_settings[key] and settings[key]:
+                        add_settings[key] = True
+                    elif old_settings[key] and not settings[key]:
+                        remove_settings[key] = False
+                else:
+                    raise FirewallError(errors.INVALID_SETTING, "Unhandled setting type {} key {}".format(type(settings[key]), key))
+
+        for key in remove_settings:
+            if isinstance(remove_settings[key], list):
+                for args in remove_settings[key]:
+                    if isinstance(args, tuple):
+                        setting_to_fn[key][1](zone, *args)
+                    else:
+                        setting_to_fn[key][1](zone, args)
+            else: # bool
+                setting_to_fn[key][1](zone)
+
+        for key in add_settings:
+            if isinstance(add_settings[key], list):
+                for args in add_settings[key]:
+                    if key in ["interfaces", "sources"]:
+                        # no timeout arg
+                        setting_to_fn[key][0](zone, args, sender=sender)
+                    else:
+                        if isinstance(args, tuple):
+                            setting_to_fn[key][0](zone, *args, timeout=0, sender=sender)
+                        else:
+                            setting_to_fn[key][0](zone, args, timeout=0, sender=sender)
+            else: # bool
+                if key in ["icmp_block_inversion"]:
+                    # no timeout arg
+                    setting_to_fn[key][0](zone, sender=sender)
+                else:
+                    setting_to_fn[key][0](zone, timeout=0, sender=sender)
 
     # INTERFACES
 

src/firewall/core/fw_config.py
@@ -781,13 +781,49 @@ class FirewallConfig(object):
         return self._builtin_zones[obj.name]
 
     def get_zone_config(self, obj):
-        return obj.export_config()
+        conf_dict = obj.export_config_dict()
+        conf_list = []
+        for i in range(16): # tuple based dbus API has 16 elements
+            if obj.IMPORT_EXPORT_STRUCTURE[i][0] not in conf_dict:
+                # old API needs the empty elements as well. Grab it from the
+                # object otherwise we don't know the type.
+                conf_list.append(copy.deepcopy(getattr(obj, obj.IMPORT_EXPORT_STRUCTURE[i][0])))
+            else:
+                conf_list.append(conf_dict[obj.IMPORT_EXPORT_STRUCTURE[i][0]])
+        return tuple(conf_list)
+
+    def get_zone_config_dict(self, obj):
+        return obj.export_config_dict()
 
     def set_zone_config(self, obj, conf):
+        conf_dict = {}
+        for i,value in enumerate(conf):
+            conf_dict[obj.IMPORT_EXPORT_STRUCTURE[i][0]] = value
+
+        if obj.builtin:
+            x = copy.copy(obj)
+            x.cleanup()
+            x.fw_config = self
+            x.import_config_dict(conf_dict)
+            x.path = config.ETC_FIREWALLD_ZONES
+            x.builtin = False
+            if obj.path != x.path:
+                x.default = False
+            self.add_zone(x)
+            zone_writer(x)
+            return x
+        else:
+            obj.cleanup()
+            obj.fw_config = self
+            obj.import_config_dict(conf_dict)
+            zone_writer(obj)
+            return obj
+
+    def set_zone_config_dict(self, obj, conf):
         if obj.builtin:
             x = copy.copy(obj)
             x.fw_config = self
-            x.import_config(conf)
+            x.import_config_dict(conf)
             x.path = config.ETC_FIREWALLD_ZONES
             x.builtin = False
             if obj.path != x.path:
@@ -797,7 +833,7 @@ class FirewallConfig(object):
             return x
         else:
             obj.fw_config = self
-            obj.import_config(conf)
+            obj.import_config_dict(conf)
             zone_writer(obj)
             return obj
 
@@ -805,10 +841,33 @@ class FirewallConfig(object):
         if name in self._zones or name in self._builtin_zones:
             raise FirewallError(errors.NAME_CONFLICT, "new_zone(): '%s'" % name)
 
+        conf_dict = {}
+        for i,value in enumerate(conf):
+            conf_dict[Zone.IMPORT_EXPORT_STRUCTURE[i][0]] = value
+
         x = Zone()
         x.check_name(name)
         x.fw_config = self
-        x.import_config(conf)
+        x.import_config_dict(conf_dict)
+        x.name = name
+        x.filename = "%s.xml" % name
+        x.path = config.ETC_FIREWALLD_ZONES
+        # It is not possible to add a new one with a name of a buitin
+        x.builtin = False
+        x.default = True
+
+        zone_writer(x)
+        self.add_zone(x)
+        return x
+
+    def new_zone_dict(self, name, conf):
+        if name in self._zones or name in self._builtin_zones:
+            raise FirewallError(errors.NAME_CONFLICT, "new_zone(): '%s'" % name)
+
+        x = Zone()
+        x.check_name(name)
+        x.fw_config = self
+        x.import_config_dict(conf)
         x.name = name
         x.filename = "%s.xml" % name
         x.path = config.ETC_FIREWALLD_ZONES
@@ -930,7 +989,7 @@ class FirewallConfig(object):
         return new_zone
 
     def _copy_zone(self, obj, name):
-        return self.new_zone(name, obj.export_config())
+        return self.new_zone_dict(name, obj.export_config_dict())
 
     # helper
 

src/firewall/core/io/zone.py
@@ -58,7 +58,6 @@ class Zone(IO_Object):
         ( "source_ports", [ ( "", "" ), ], ),          # a(ss)
         ( "icmp_block_inversion", False ),             # b
         )
-    DBUS_SIGNATURE = '(sssbsasa(ss)asba(ssss)asasasasa(ss)b)'
     ADDITIONAL_ALNUM_CHARS = [ "_", "-", "/" ]
     PARSER_REQUIRED_ELEMENT_ATTRS = {
         "short": None,
@@ -176,6 +175,11 @@ class Zone(IO_Object):
             self.rules = [rich.Rich_Rule(rule_str=s) for s in value]
         else:
             super(Zone, self).__setattr__(name, value)
+
+    def export_config_dict(self):
+        conf = super(Zone, self).export_config_dict()
+        del conf["UNUSED"]
+        return conf
 
     def _check_config(self, config, item):
         if item == "services" and self.fw_config:

src/firewall/server/config.py
@@ -40,7 +40,6 @@ from firewall.server.config_service import FirewallDConfigService
 from firewall.server.config_zone import FirewallDConfigZone
 from firewall.server.config_ipset import FirewallDConfigIPSet
 from firewall.server.config_helper import FirewallDConfigHelper
-from firewall.core.io.zone import Zone
 from firewall.core.io.icmptype import IcmpType
 from firewall.core.io.ipset import IPSet
 from firewall.core.io.helper import Helper
@@ -1187,7 +1186,7 @@ class FirewallDConfig(slip.dbus.service.Object):
         return ret[0] if ret else ""
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG,
-                         in_signature='s'+Zone.DBUS_SIGNATURE,
+                         in_signature="s(sssbsasa(ss)asba(ssss)asasasasa(ss)b)",
                          out_signature='o')
     @dbus_handle_exceptions
     def addZone(self, zone, settings, sender=None):
@@ -1203,6 +1202,23 @@ class FirewallDConfig(slip.dbus.service.Object):
             _settings[4] = DEFAULT_ZONE_TARGET
             settings = tuple(_settings)
         obj = self.config.new_zone(zone, settings)
+        config_zone = self._addZone(obj)
+        return config_zone
+
+    @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG,
+                         in_signature="sa{sv}",
+                         out_signature='o')
+    @dbus_handle_exceptions
+    def addZone2(self, zone, settings, sender=None):
+        """add zone with given name and settings
+        """
+        zone = dbus_to_python(zone, str)
+        settings = dbus_to_python(settings)
+        log.debug1("config.addZone('%s')", zone)
+        self.accessCheck(sender)
+        if "target" in settings and settings["target"] == "default":
+            settings["target"] = DEFAULT_ZONE_TARGET
+        obj = self.config.new_zone_dict(zone, settings)
         config_zone = self._addZone(obj)
         return config_zone
 

src/firewall/server/config_zone.py
@@ -178,7 +178,7 @@ class FirewallDConfigZone(slip.dbus.service.Object):
     # S E T T I N G S
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
-                         out_signature=Zone.DBUS_SIGNATURE)
+                         out_signature="(sssbsasa(ss)asba(ssss)asasasasa(ss)b)")
     @dbus_handle_exceptions
     def getSettings(self, sender=None): # pylint: disable=W0613
         """get settings for zone
@@ -192,16 +192,34 @@ class FirewallDConfigZone(slip.dbus.service.Object):
             settings = tuple(_settings)
         return settings
 
+    @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
+                         out_signature="a{sv}")
+    @dbus_handle_exceptions
+    def getSettings2(self, sender=None):
+        """get settings for zone
+        """
+        log.debug1("%s.getSettings2()", self._log_prefix)
+        settings = self.config.get_zone_config_dict(self.obj)
+        if settings["target"] == DEFAULT_ZONE_TARGET:
+            settings["target"] = "default"
+        return settings
+
     def _checkDuplicateInterfacesSources(self, settings):
         """Assignment of interfaces/sources to zones is different from other
            zone settings in the sense that particular interface/zone can be
            part of only one zone. So make sure added interfaces/sources have
            not already been bound to another zone."""
-        old_settings = self.config.get_zone_config(self.obj)
-        idx_i = Zone.index_of("interfaces")
-        idx_s = Zone.index_of("sources")
-        added_ifaces = set(settings[idx_i]) - set(old_settings[idx_i])
-        added_sources = set(settings[idx_s]) - set(old_settings[idx_s])
+        old_settings = self.config.get_zone_config_dict(self.obj)
+        old_ifaces = set(old_settings["interfaces"]) if "interfaces" in old_settings else set()
+        old_sources = set(old_settings["sources"]) if "sources" in old_settings else set()
+        if isinstance(settings, tuple):
+            added_ifaces = set(settings[Zone.index_of("interfaces")]) - old_ifaces
+            added_sources = set(settings[Zone.index_of("sources")]) - old_sources
+        else: # dict
+            new_ifaces = set(settings["interfaces"]) if "interfaces" in settings else set()
+            new_sources = set(settings["sources"]) if "sources" in settings else set()
+            added_ifaces = new_ifaces - old_ifaces
+            added_sources = new_sources - old_sources
 
         for iface in added_ifaces:
             if self.parent.getZoneOfInterface(iface):
@@ -211,7 +229,7 @@ class FirewallDConfigZone(slip.dbus.service.Object):
                 raise FirewallError(errors.ZONE_CONFLICT, source) # or move to new zone ?
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
-                         in_signature=Zone.DBUS_SIGNATURE)
+                         in_signature="(sssbsasa(ss)asba(ssss)asasasasa(ss)b)")
     @dbus_handle_exceptions
     def update(self, settings, sender=None):
         """update settings for zone
@@ -226,6 +244,21 @@ class FirewallDConfigZone(slip.dbus.service.Object):
             settings = tuple(_settings)
         self._checkDuplicateInterfacesSources(settings)
         self.obj = self.config.set_zone_config(self.obj, settings)
+        self.Updated(self.obj.name)
+
+    @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE,
+                         in_signature="a{sv}")
+    @dbus_handle_exceptions
+    def update2(self, settings, sender=None):
+        """update settings for zone
+        """
+        settings = dbus_to_python(settings)
+        log.debug1("%s.update2('...')", self._log_prefix)
+        self.parent.accessCheck(sender)
+        if "target" in settings and settings["target"] == "default":
+            settings["target"] = DEFAULT_ZONE_TARGET
+        self._checkDuplicateInterfacesSources(settings)
+        self.obj = self.config.set_zone_config_dict(self.obj, settings)
         self.Updated(self.obj.name)
 
     @dbus_service_method(config.dbus.DBUS_INTERFACE_CONFIG_ZONE)

src/firewall/server/firewalld.py
@@ -47,7 +47,6 @@ from firewall.dbus_utils import dbus_to_python, \
     dbus_introspection_prepare_properties, \
     dbus_introspection_add_properties
 from firewall.core.io.functions import check_config
-from firewall.core.io.zone import Zone
 from firewall.core.io.ipset import IPSet
 from firewall.core.io.icmptype import IcmpType
 from firewall.core.io.helper import Helper
@@ -894,13 +893,37 @@ class FirewallD(slip.dbus.service.Object):
 
     @slip.dbus.polkit.require_auth(config.dbus.PK_ACTION_CONFIG_INFO)
     @dbus_service_method(config.dbus.DBUS_INTERFACE, in_signature='s',
-                         out_signature=Zone.DBUS_SIGNATURE)
+                         out_signature="(sssbsasa(ss)asba(ssss)asasasasa(ss)b)")
     @dbus_handle_exceptions
     def getZoneSettings(self, zone, sender=None): # pylint: disable=W0613
         # returns zone settings for zone
         zone = dbus_to_python(zone, str)
         log.debug1("getZoneSettings(%s)", zone)
         return self.fw.zone.get_config_with_settings(zone)
+
+    @slip.dbus.polkit.require_auth(config.dbus.PK_ACTION_CONFIG_INFO)
+    @dbus_service_method(config.dbus.DBUS_INTERFACE_ZONE, in_signature='s',
+                         out_signature="a{sv}")
+    @dbus_handle_exceptions
+    def getZoneSettings2(self, zone, sender=None):
+        zone = dbus_to_python(zone, str)
+        log.debug1("getZoneSettings2(%s)", zone)
+        return self.fw.zone.get_config_with_settings_dict(zone)
+
+    @slip.dbus.polkit.require_auth(config.dbus.PK_ACTION_CONFIG_INFO)
+    @dbus_service_method(config.dbus.DBUS_INTERFACE_ZONE, in_signature='sa{sv}')
+    @dbus_handle_exceptions
+    def setZoneSettings2(self, zone, settings, sender=None):
+        zone = dbus_to_python(zone, str)
+        log.debug1("setZoneSettings2(%s)", zone)
+        self.accessCheck(sender)
+        self.fw.zone.set_config_with_settings_dict(zone, settings, sender)
+        self.ZoneUpdated(zone, settings)
+
+    @dbus.service.signal(config.dbus.DBUS_INTERFACE_ZONE, signature='sa{sv}')
+    @dbus_handle_exceptions
+    def ZoneUpdated(self, zone, settings):
+        log.debug1("zone.ZoneUpdated('%s', '%s')" % (zone, settings))
 
     @slip.dbus.polkit.require_auth(config.dbus.PK_ACTION_INFO)
     @dbus_service_method(config.dbus.DBUS_INTERFACE, in_signature='',

commit ad4d01c4344a92d3a2aed2216263a6168f28764d

fix(zone): list output if runtime removes permanent config
If we have something in the permanent configuration, but then it is
zeroed at runtime the permanent configuration was mistakenly being
returned.

Fixes: 67d057508f81 ("improvement(dbus): new dict based APIs for zones")

src/firewall/core/fw_zone.py
@@ -348,6 +348,11 @@ class FirewallZone(object):
             # omit empty entries
             if value or isinstance(value, bool):
                 conf[key] = value
+            # make sure to remove values that were in permanent, but no longer
+            # in runtime.
+            elif key in conf:
+                del conf[key]
+
         return conf
 
     def set_config_with_settings_dict(self, zone, settings, sender):

src/tests/dbus/zone_runtime_functional.at
@@ -390,6 +390,12 @@ DBUS_CHECK([], [zone.setZoneSettings2], [dnl
      '{"forward_ports": <@a(ssss) @<:@@:>@> dnl
       }'dnl
     ], 0, [ignore])
+dnl clear another field
+DBUS_CHECK([], [zone.setZoneSettings2], [dnl
+    "public" dnl
+     '{"services": <@as @<:@@:>@> dnl
+      }'dnl
+    ], 0, [ignore])
 dnl verify the updates
 DBUS_CHECK([], [zone.getZoneSettings2], ["public"], 0, [dnl
        'description': <'For use in public areas. You do not trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.'>
@@ -400,7 +406,6 @@ DBUS_CHECK([], [zone.getZoneSettings2], ["public"], 0, [dnl
        'masquerade': <false>
        'ports': <@<:@('1234', 'udp')@:>@>
        'protocols': <@<:@'ipv6-icmp'@:>@>
-       'services': <@<:@'mdns', 'ssh'@:>@>
        'short': <'Public'>
        'source_ports': <@<:@('1234', 'udp')@:>@>
        'sources': <@<:@'10.10.10.0/24'@:>@>