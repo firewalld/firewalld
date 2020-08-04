firewalld/firewalld

commit 34bdee40aa61f6f973a76233742d41c20d6c6e0b

feat: implement policy objects internally
Introduce policy objects internally. There are no user visible changes.
Rule layout for zones has not changed. Zones are translated into
multiple policies.

This is the first step in introducing policy objects to users and
therefore supporting output/forward filtering.

src/firewall/functions.py
@@ -27,7 +27,7 @@ __all__ = [ "PY2", "getPortID", "getPortRange", "portStr", "getServiceName",
             "check_single_address", "check_mac", "uniqify", "ppid_of_pid",
             "max_zone_name_len", "checkUser", "checkUid", "checkCommand",
             "checkContext", "joinArgs", "splitArgs",
-            "b2u", "u2b", "u2b_if_py2" ]
+            "b2u", "u2b", "u2b_if_py2", "max_policy_name_len"]
 
 import socket
 import os
@@ -504,6 +504,15 @@ def ppid_of_pid(pid):
     except Exception:
         return None
     return pid
+
+def max_policy_name_len():
+    """
+    iptables limits length of chain to (currently) 28 chars.
+    The longest chain we create is pol_<policy>_allow,
+    which leaves 28 - 10 = 18 chars for <policy>.
+    """
+    from firewall.core.ipXtables import POLICY_CHAIN_PREFIX
+    return 28 - (len(POLICY_CHAIN_PREFIX) + len("_allow"))
 
 def max_zone_name_len():
     """

src/firewall-config.in
@@ -46,7 +46,7 @@ from firewall import config
 from firewall import client
 from firewall import functions
 from firewall.core.base import DEFAULT_ZONE_TARGET, REJECT_TYPES, \
-                               ZONE_SOURCE_IPSET_TYPES
+                               SOURCE_IPSET_TYPES
 from firewall.core.ipset import IPSET_MAXNAMELEN
 from firewall.core.helper import HELPER_MAXNAMELEN
 from firewall.core.io.zone import Zone
@@ -4188,7 +4188,7 @@ class FirewallConfig(object):
                         continue
                     raise
                 self.activate_exception_handler()
-                if settings.getType() not in ZONE_SOURCE_IPSET_TYPES:
+                if settings.getType() not in SOURCE_IPSET_TYPES:
                     continue
                 ipsets[x] = settings
         else:

src/firewall/core/base.py
@@ -22,9 +22,12 @@
 """Base firewall settings"""
 
 DEFAULT_ZONE_TARGET = "{chain}_{zone}"
+DEFAULT_POLICY_TARGET = "CONTINUE"
 
 ZONE_TARGETS = [ "ACCEPT", "%%REJECT%%", "DROP", DEFAULT_ZONE_TARGET,
                  "default" ]
+
+POLICY_TARGETS = [ "ACCEPT", "%%REJECT%%", "DROP", "CONTINUE" ]
 
 SHORTCUTS = {
     "PREROUTING": "PRE",
@@ -49,7 +52,7 @@ REJECT_TYPES = {
 # ipset types that can be used as a source in zones
 # The match-set option will be src or src,src according to the
 # dimension of the ipset.
-ZONE_SOURCE_IPSET_TYPES = [
+SOURCE_IPSET_TYPES = [
     "hash:ip", "hash:ip,port", "hash:ip,mark",
     "hash:net", "hash:net,port", "hash:net,iface",
     "hash:mac"

src/firewall/core/ebtables.py
@@ -52,7 +52,7 @@ for table in BUILT_IN_CHAINS.keys():
 class ebtables(object):
     ipv = "eb"
     name = "ebtables"
-    zones_supported = False # ebtables only supported with direct interface
+    policies_supported = False # ebtables only supported with direct interface
 
     def __init__(self):
         self._command = COMMANDS[self.ipv]

src/firewall/core/fw.py
@@ -42,6 +42,7 @@ from firewall.core.fw_policies import FirewallPolicies
 from firewall.core.fw_ipset import FirewallIPSet
 from firewall.core.fw_transaction import FirewallTransaction
 from firewall.core.fw_helper import FirewallHelper
+from firewall.core.fw_policy import FirewallPolicy
 from firewall.core.fw_nm import nm_get_bus_name, nm_get_interfaces_in_zone
 from firewall.core.logger import log
 from firewall.core.io.firewalld_conf import firewalld_conf
@@ -98,6 +99,7 @@ class Firewall(object):
         self.policies = FirewallPolicies()
         self.ipset = FirewallIPSet(self)
         self.helper = FirewallHelper(self)
+        self.policy = FirewallPolicy(self)
 
         self.__init_vars()
 
@@ -634,6 +636,7 @@ class Firewall(object):
         self.config.cleanup()
         self.direct.cleanup()
         self.policies.cleanup()
+        self.policy.cleanup()
         self._firewalld_conf.cleanup()
         self.__init_vars()
 
@@ -876,6 +879,12 @@ class Firewall(object):
         if self._panic:
             raise FirewallError(errors.PANIC_MODE)
 
+    def check_policy(self, policy):
+        _policy = policy
+        if _policy not in self.policy.get_policies():
+            raise FirewallError(errors.INVALID_POLICY, _policy)
+        return _policy
+
     def check_zone(self, zone):
         _zone = zone
         if not _zone or _zone == "":
@@ -996,8 +1005,11 @@ class Firewall(object):
             # add interfaces to zones again
             for zone in self.zone.get_zones():
                 if zone in _zone_interfaces:
-                    self.zone.set_settings(zone, { "interfaces":
-                                                   _zone_interfaces[zone] })
+
+                    for interface_id in _zone_interfaces[zone]:
+                        self.zone.change_zone_of_interface(zone, interface_id,
+                                                           _zone_interfaces[zone][interface_id]["sender"])
+
                     del _zone_interfaces[zone]
                 else:
                     log.info1("New zone '%s'.", zone)

src/firewall/core/fw_ipset.py
@@ -155,8 +155,8 @@ class FirewallIPSet(object):
 
     # TYPE
 
-    def get_type(self, name):
-        return self.get_ipset(name, applied=True).type
+    def get_type(self, name, applied=True):
+        return self.get_ipset(name, applied=applied).type
 
     # DIMENSION
     def get_dimension(self, name):
@@ -173,8 +173,8 @@ class FirewallIPSet(object):
 
     # OPTIONS
 
-    def get_family(self, name):
-        obj = self.get_ipset(name, applied=True)
+    def get_family(self, name, applied=True):
+        obj = self.get_ipset(name, applied=applied)
         if "family" in obj.options:
             if obj.options["family"] == "inet6":
                 return "ipv6"

commit c6fe749fb75004c30818bcc0696ac23801239d0b

fix(policy): ipXtables: calculate max name len properly
Policy chain names still need the SHORTCUTS (POST, IN, etc) in the chain
name. As such, calculate the max name length appropriately.

This also drops the "pol_" prefix for policy chains. Retaining it would
restrict the policy name max length unreasonably so.

Fixes: 34bdee40aa61 ("feat: implement policy objects internally")

src/firewall/core/ipXtables.py
@@ -32,7 +32,7 @@ from firewall.core.rich import Rich_Accept, Rich_Reject, Rich_Drop, Rich_Mark, \
                                Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock
 import string
 
-POLICY_CHAIN_PREFIX = "pol_"
+POLICY_CHAIN_PREFIX = ""
 
 BUILT_IN_CHAINS = {
     "security": [ "INPUT", "OUTPUT", "FORWARD" ],

src/firewall/functions.py
@@ -508,11 +508,13 @@ def ppid_of_pid(pid):
 def max_policy_name_len():
     """
     iptables limits length of chain to (currently) 28 chars.
-    The longest chain we create is pol_<policy>_allow,
-    which leaves 28 - 10 = 18 chars for <policy>.
+    The longest chain we create is POST_<policy>_allow,
+    which leaves 28 - 11 = 17 chars for <policy>.
     """
     from firewall.core.ipXtables import POLICY_CHAIN_PREFIX
-    return 28 - (len(POLICY_CHAIN_PREFIX) + len("_allow"))
+    from firewall.core.base import SHORTCUTS
+    longest_shortcut = max(map(len, SHORTCUTS.values()))
+    return 28 - (longest_shortcut + len(POLICY_CHAIN_PREFIX) + len("_allow"))
 
 def max_zone_name_len():
     """