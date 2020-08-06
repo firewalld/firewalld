firewalld/firewalld

commit 3a0e79b1cfe4344d21d30eb47c038252d728cc44

fix: core: rich: Catch ValueError on non-numeric priority values
Be a bit more user-friendly by printing:

| Error: INVALID_RULE: invalid 'priority' attribute value 'ab'.

instead of Python's default, which is:

| Error: invalid literal for int() with base 10: 'ab'

Fixes: rhbz 1689429

src/firewall/core/rich.py
@@ -379,7 +379,10 @@ class Rich_Rule(object):
                         raise FirewallError(errors.INVALID_RULE, "'family' attribute cannot have '%s' value. Use 'ipv4' or 'ipv6' instead." % attr_value)
                     self.family = attr_value
                 elif attr_name == 'priority':
-                    self.priority = int(attr_value)
+                    try:
+                        self.priority = int(attr_value)
+                    except ValueError:
+                        raise FirewallError(errors.INVALID_RULE, "invalid 'priority' attribute value '%s'." % attr_value)
                 elif attr_name:
                     if attr_name == 'protocol':
                         err_msg = "wrong 'protocol' usage. Use either 'rule protocol value=...' or  'rule [forward-]port protocol=...'."

src/tests/regression/rhbz1689429.at
@@ -1,0 +1,12 @@
+FWD_START_TEST([rich rule invalid priority])
+AT_KEYWORDS(rich rhbz1689429)
+
+FWD_CHECK([--add-rich-rule='rule priority=foo accept'], 122, [],
+	  [Error: INVALID_RULE: invalid 'priority' attribute value 'foo'.
+])
+FWD_CHECK([--permanent --add-rich-rule='rule priority=foo accept'], 122, [],
+	  [Error: INVALID_RULE: invalid 'priority' attribute value 'foo'.
+])
+FWD_RELOAD
+
+FWD_END_TEST([ignore])

src/tests/regression/regression.at
@@ -31,3 +31,4 @@ m4_include([regression/gh599.at])
 m4_include([regression/rhbz1829104.at])
 m4_include([regression/rhbz1843398.at])
 m4_include([regression/rhbz1839781.at])
+m4_include([regression/rhbz1689429.at])

commit e1562ba92caec988c7cf397b2fa77b8d41592c7e

fix(rich): use correct error code for invalid priority
Fixes: 3a0e79b1cfe4 ("fix: core: rich: Catch ValueError on non-numeric priority values")

src/firewall/core/rich.py
@@ -382,7 +382,7 @@ class Rich_Rule(object):
                     try:
                         self.priority = int(attr_value)
                     except ValueError:
-                        raise FirewallError(errors.INVALID_RULE, "invalid 'priority' attribute value '%s'." % attr_value)
+                        raise FirewallError(errors.INVALID_PRIORITY, "invalid 'priority' attribute value '%s'." % attr_value)
                 elif attr_name:
                     if attr_name == 'protocol':
                         err_msg = "wrong 'protocol' usage. Use either 'rule protocol value=...' or  'rule [forward-]port protocol=...'."

src/tests/regression/rhbz1689429.at
@@ -1,11 +1,11 @@
 FWD_START_TEST([rich rule invalid priority])
 AT_KEYWORDS(rich rhbz1689429)
 
-FWD_CHECK([--add-rich-rule='rule priority=foo accept'], 122, [],
-	  [Error: INVALID_RULE: invalid 'priority' attribute value 'foo'.
+FWD_CHECK([--add-rich-rule='rule priority=foo accept'], 139, [],
+	  [Error: INVALID_PRIORITY: invalid 'priority' attribute value 'foo'.
 ])
-FWD_CHECK([--permanent --add-rich-rule='rule priority=foo accept'], 122, [],
-	  [Error: INVALID_RULE: invalid 'priority' attribute value 'foo'.
+FWD_CHECK([--permanent --add-rich-rule='rule priority=foo accept'], 139, [],
+	  [Error: INVALID_PRIORITY: invalid 'priority' attribute value 'foo'.
 ])
 FWD_RELOAD
 