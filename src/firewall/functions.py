firewalld/firewalld

commit ac5960856991a00ddf7a558e31fd3248c8279a1f

fix(rich): non-printable characters removed from rich rules Fixes: rhbz 1596304 Fixes: #480

src/firewall/functions.py
@@ -27,8 +27,10 @@ __all__ = [ "getPortID", "getPortRange", "portStr", "getServiceName",
             "check_single_address", "check_mac", "uniqify", "ppid_of_pid",
             "max_zone_name_len", "checkUser", "checkUid", "checkCommand",
             "checkContext", "joinArgs", "splitArgs",
-            "max_policy_name_len", "checkTcpMssClamp"]
+            "max_policy_name_len", "checkTcpMssClamp", 
+            "stripNonPrintableCharacters"]
 
+import sys
 import socket
 import os
 import os.path
@@ -38,6 +40,10 @@ import string
 import tempfile
 from firewall.core.logger import log
 from firewall.config import FIREWALLD_TEMPDIR, FIREWALLD_PIDFILE
+
+NOPRINT_TRANS_TABLE = {
+    i: None for i in range(0, sys.maxunicode + 1) if not chr(i).isprintable()
+}
 
 def getPortID(port):
     """ Check and Get port id from port string or port id using socket.getservbyname
@@ -307,6 +313,9 @@ def checkIPnMask(ip):
             if i < 0 or i > 32:
                 return False
     return True
+
+def stripNonPrintableCharacters(rule_str):
+    return rule_str.translate(NOPRINT_TRANS_TABLE)
 
 def checkIP6nMask(ip):
     if "/" in ip:

src/firewall/core/rich.py
@@ -328,6 +328,8 @@ class Rich_Rule(object):
         if not rule_str:
             raise FirewallError(errors.INVALID_RULE, 'empty rule')
 
+        rule_str = functions.stripNonPrintableCharacters(rule_str)
+
         self.priority = 0
         self.family = None
         self.source = None

src/tests/regression/regression.at
@@ -36,3 +36,4 @@ m4_include([regression/rhbz1483921.at])
 m4_include([regression/rhbz1541077.at])
 m4_include([regression/rhbz1855140.at])
 m4_include([regression/rhbz1871298.at])
+m4_include([regression/rhbz1596304.at])

src/tests/regression/rhbz1596304.at
@@ -1,0 +1,24 @@
+FWD_START_TEST([rich rules strip non-printable characters])
+AT_KEYWORDS(rich rhbz1596304)
+
+dnl source address contains a tab character
+FWD_CHECK([--permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="104.243.250.0/22	" port port=80 protocol=tcp accept'],0,ignore)
+FWD_RELOAD
+FWD_CHECK([--list-all | TRIM_WHITESPACE], 0, [m4_strip([dnl
+        public
+        target: default
+        icmp-block-inversion: no
+        interfaces:
+        sources:
+        services: dhcpv6-client ssh
+        ports:
+        protocols:
+        forward: yes
+        masquerade: no
+        forward-ports:
+        source-ports:
+        icmp-blocks:
+        rich rules:
+	rule family="ipv4" source address="104.243.250.0/22" port port="80" protocol="tcp" accept
+    ])])
+FWD_END_TEST

commit a5d2ce58426392d155c5c174254311ee3aaf25d7

fix(rich): limit table to strip non-printables to C0 and C1
Generating the table was taking an unreasonable amount of memory.
Stripping C0 and C1 should cover most scenarios while limiting memory
usage.

Fixes: ac5960856991 ("fix(rich): non-printable characters removed from rich rules")

src/firewall/functions.py
@@ -30,7 +30,6 @@ __all__ = [ "getPortID", "getPortRange", "portStr", "getServiceName",
             "max_policy_name_len", "checkTcpMssClamp", 
             "stripNonPrintableCharacters"]
 
-import sys
 import socket
 import os
 import os.path
@@ -42,7 +41,12 @@ from firewall.core.logger import log
 from firewall.config import FIREWALLD_TEMPDIR, FIREWALLD_PIDFILE
 
 NOPRINT_TRANS_TABLE = {
-    i: None for i in range(0, sys.maxunicode + 1) if not chr(i).isprintable()
+    # Limit to C0 and C1 code points. Building entries for all unicode code
+    # points requires too much memory.
+    # C0 = [0, 31]
+    # C1 = [127, 159]
+    #
+    i: None for i in range(0, 160) if not (i > 31 and i < 127)
 }
 
 def getPortID(port):