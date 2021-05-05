577ce3a8961e1bf3b161c134ca2c6bbfe3a644a5
Eric Garver <eric@garver.life>
2021-05-05 10:36:53 -0400

    chore(dbus): add NotAuthorizedException
    A convenience exception to make sure the string "NotAuthorizedException"
    is part of the name returned by DBusException.get_dbus_name() which is
    used by the UIs to differentiate between auth errors and normal errors.

--- src/firewall/server/dbus.py
+++ src/firewall/server/dbus.py
@@ -8,3 +8,8 @@ from firewall import config
 class FirewallDBusException(dbus.DBusException):
     """FirewallDBusException"""
     _dbus_error_name = "%s.Exception" % config.dbus.DBUS_INTERFACE
+
+class NotAuthorizedException(dbus.DBusException):
+    def __init__(self, action_id, method, *args, **kwargs):
+        self._dbus_error_name = config.dbus.DBUS_INTERFACE + ".NotAuthorizedException"
+        super().__init__("Not Authorized({}): {}".format(method, action_id))
