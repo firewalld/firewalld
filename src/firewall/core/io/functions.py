firewalld/firewalld

commit 3b466b8bf092256dcbd1b541df4924d154ca7386

fix(check-config): use the dictionary variants
It was possible for newer config fields of an io_object to not be
checked. That is, the fields covered by check_config(), i.e. the fields
covered by the tuple based dbus APIs.

src/firewall/core/io/functions.py
@@ -73,7 +73,7 @@ def check_config(fw):
                         obj = readers[reader]["reader"](file, _dir)
                         if reader in ["zone", "policy"]:
                             obj.fw_config = fw_config
-                        obj.check_config(obj.export_config())
+                        obj.check_config_dict(obj.export_config_dict())
                         readers[reader]["add"](obj)
                     except FirewallError as error:
                         raise FirewallError(error.code, "'%s': %s" % (file, error.msg))
@@ -83,7 +83,7 @@ def check_config(fw):
         try:
             obj = Direct(config.FIREWALLD_DIRECT)
             obj.read()
-            obj.check_config(obj.export_config())
+            obj.check_config_dict(obj.export_config_dict())
         except FirewallError as error:
             raise FirewallError(error.code, "'%s': %s" % (config.FIREWALLD_DIRECT, error.msg))
         except Exception as msg:
@@ -92,7 +92,7 @@ def check_config(fw):
         try:
             obj = LockdownWhitelist(config.LOCKDOWN_WHITELIST)
             obj.read()
-            obj.check_config(obj.export_config())
+            obj.check_config_dict(obj.export_config_dict())
         except FirewallError as error:
             raise FirewallError(error.code, "'%s': %s" % (config.LOCKDOWN_WHITELIST, error.msg))
         except Exception as msg:

commit cf9ffb678134dc498c4b08444e17792e340d04a9

fix(functions): --check-config fails if direct.xml exists
Fixes: 3b466b8bf092 ("fix(check-config): use the dictionary variants")

src/firewall/core/io/functions.py
@@ -77,7 +77,7 @@ def check_on_disk_config(fw):
         try:
             obj = Direct(config.FIREWALLD_DIRECT)
             obj.read()
-            obj.check_config_dict(obj.export_config_dict())
+            obj.check_config(obj.export_config())
         except FirewallError as error:
             raise FirewallError(error.code, "'%s': %s" % (config.FIREWALLD_DIRECT, error.msg))
         except Exception as msg:
@@ -86,7 +86,7 @@ def check_on_disk_config(fw):
         try:
             obj = LockdownWhitelist(config.LOCKDOWN_WHITELIST)
             obj.read()
-            obj.check_config_dict(obj.export_config_dict())
+            obj.check_config(obj.export_config())
         except FirewallError as error:
             raise FirewallError(error.code, "'%s': %s" % (config.LOCKDOWN_WHITELIST, error.msg))
         except Exception as msg: