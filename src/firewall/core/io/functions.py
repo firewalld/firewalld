firewalld/firewalld

commit d8ebe1da94d929e1d518be125e52c81d8b0e92f1

feat(policy): masquerade: allow ingress zone to have interface
This only applies to the nftables backend. The iptables backend still
does not allow this (kernel limitation).

Fixes: #926

src/firewall/core/io/functions.py
@@ -20,6 +20,7 @@
 #
 
 import os
+import copy
 
 from firewall import config
 from firewall.errors import FirewallError
@@ -77,6 +78,7 @@ def check_on_disk_config(fw):
                 if file.endswith(".xml"):
                     obj = readers[reader]["reader"](file, _dir)
                     readers[reader]["add"](obj)
+    fw_config.set_firewalld_conf(copy.deepcopy(fw._firewalld_conf))
     fw_config.full_check_config()
 
     if os.path.isfile(config.FIREWALLD_DIRECT):

doc/xml/firewall-cmd.xml.in
@@ -898,6 +898,13 @@
 	    <para>
 	      <emphasis>Note:</emphasis> IP forwarding will be implicitly enabled.
 	    </para>
+            <para>
+              <emphasis>Note (Linux &lt; 5.5):</emphasis>
+                For the iptables backend, a policy may not enable masquerade if
+                an ingress zone has assigned interfaces. This restriction does
+                not exist for the nftables backend, but does require Linux v5.5+
+                to function properly; otherwise it will silently fail.
+            </para>
 	  </listitem>
 	</varlistentry>
 

src/firewall/core/fw.py
@@ -149,6 +149,9 @@ class Firewall(object):
         conf_dict["zones"] = {zone: self.zone.get_zone(zone) for zone in self.zone.get_zones()}
         conf_dict["policies"] = {policy: self.policy.get_policy(policy) for policy in self.policy.get_policies_not_derived_from_zone()}
 
+        conf_dict["conf"] = {}
+        conf_dict["conf"]["FirewallBackend"] = self._firewalld_conf.get("FirewallBackend")
+
         # The runtime might not actually support all the defined icmptypes.
         # This is the case if ipv6 (ip6tables) is disabled. Unfortunately users
         # disable IPv6 and also expect the IPv6 stuff to be silently ignored.

src/firewall/core/fw_config.py
@@ -135,6 +135,9 @@ class FirewallConfig(object):
         conf_dict["zones"] = {zone: self.get_zone(zone) for zone in self.get_zones()}
         conf_dict["policies"] = {policy: self.get_policy_object(policy) for policy in self.get_policy_objects()}
 
+        conf_dict["conf"] = {}
+        conf_dict["conf"]["FirewallBackend"] = self._firewalld_conf.get("FirewallBackend")
+
         return conf_dict
 
     def full_check_config(self, extra_io_objects: Dict[str, List[IO_Object]] = {}):

src/firewall/core/fw_policy.py
@@ -1742,11 +1742,14 @@ class FirewallPolicy(object):
             # prerouting.
             if not self._fw.nftables_enabled:
                 tc.append(("raw", "PREROUTING"))
-            for zone in obj.ingress_zones:
-                if self._fw.zone.get_zone(zone).interfaces:
-                    break
-            else:
+            if self._fw._firewall_backend == "nftables":
                 tc.append(("nat", "POSTROUTING"))
+            else:
+                for zone in obj.ingress_zones:
+                    if self._fw.zone.get_zone(zone).interfaces:
+                        break
+                else:
+                    tc.append(("nat", "POSTROUTING"))
             return tc
         elif "ANY" in obj.ingress_zones:
             # any --> zone
@@ -1769,11 +1772,14 @@ class FirewallPolicy(object):
             # prerouting.
             if not self._fw.nftables_enabled:
                 tc.append(("raw", "PREROUTING"))
-            for zone in obj.ingress_zones:
-                if self._fw.zone.get_zone(zone).interfaces:
-                    break
-            else:
+            if self._fw._firewall_backend == "nftables":
                 tc.append(("nat", "POSTROUTING"))
+            else:
+                for zone in obj.ingress_zones:
+                    if self._fw.zone.get_zone(zone).interfaces:
+                        break
+                else:
+                    tc.append(("nat", "POSTROUTING"))
             for zone in obj.egress_zones:
                 if self._fw.zone.get_zone(zone).interfaces:
                     break

src/firewall/core/io/policy.py
@@ -882,7 +882,8 @@ class Policy(IO_Object):
                         continue
                     if zone not in all_io_objects["zones"]:
                         raise FirewallError(errors.INVALID_ZONE, "Policy '{}': Zone '{}' does not exist.".format(self.name, zone))
-                    if all_io_objects["zones"][zone].interfaces:
+                    if all_io_objects["conf"].get("FirewallBackend") != "nftables" \
+                       and all_io_objects["zones"][zone].interfaces:
                         raise FirewallError(errors.INVALID_ZONE, "Policy '{}': 'masquerade' cannot be used because ingress zone '{}' has assigned interfaces. ".format(self.name, zone))
         elif item == "rich_rules":
             for rule in config:
@@ -902,7 +903,8 @@ class Policy(IO_Object):
                                 continue
                             if zone not in all_io_objects["zones"]:
                                 raise FirewallError(errors.INVALID_ZONE, "Policy '{}': Zone '{}' does not exist.".format(self.name, zone))
-                            if all_io_objects["zones"][zone].interfaces:
+                            if all_io_objects["conf"].get("FirewallBackend") != "nftables" \
+                               and all_io_objects["zones"][zone].interfaces:
                                 raise FirewallError(errors.INVALID_ZONE, "Policy '{}': 'masquerade' cannot be used because ingress zone '{}' has assigned interfaces. ".format(self.name, zone))
                 elif obj.element and isinstance(obj.element, rich.Rich_ForwardPort):
                     if "egress_zones" in all_config:

src/tests/features/masquerade.at
@@ -1,5 +1,5 @@
 FWD_START_TEST([masquerade])
-AT_KEYWORDS(policy masquerade)
+AT_KEYWORDS(policy masquerade gh926)
 
 FWD_CHECK([--permanent --zone=public --add-interface foobar0], 0, [ignore])
 
@@ -79,6 +79,7 @@ FWD_CHECK([--policy=foobar_host --add-rich-rule='rule family=ipv4 source address
 FWD_CHECK([--permanent --policy=foobar_host --remove-ingress-zone internal], 0, [ignore])
 FWD_CHECK([--permanent --policy=foobar_host --remove-egress-zone HOST], 0, [ignore])
 
+m4_if(iptables, FIREWALL_BACKEND, [
 dnl invalid for zone --> zone if ingress zone has interfaces
 dnl
 FWD_CHECK([--permanent --new-policy=foobar_int_to_pub], 0, [ignore])
@@ -91,6 +92,20 @@ FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-rich-rule='rule family=i
 FWD_CHECK([--policy=foobar_int_to_pub --add-masquerade], 112, [ignore], [ignore])
 FWD_CHECK([--policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 112, [ignore], [ignore])
 FWD_CHECK([--permanent --zone=internal --remove-interface foobar1], 0, [ignore])
+], [ dnl nftables or offline
+dnl valid for zone --> zone if ingress zone has only interfaces
+dnl
+FWD_CHECK([--permanent --new-policy=foobar_int_to_pub], 0, [ignore])
+FWD_CHECK([--permanent --zone=internal --add-interface foobar1], 0, [ignore])
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-ingress-zone internal], 0, [ignore])
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-egress-zone public], 0, [ignore])
+FWD_RELOAD
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-masquerade], 0, [ignore], [ignore])
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 0, [ignore], [ignore])
+FWD_CHECK([--policy=foobar_int_to_pub --add-masquerade], 0, [ignore], [ignore])
+FWD_CHECK([--policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 0, [ignore], [ignore])
+FWD_CHECK([--permanent --zone=internal --remove-interface foobar1], 0, [ignore])
+])
 
 dnl valid for zone --> zone if ingress zone has only sources
 dnl
@@ -99,5 +114,19 @@ FWD_RELOAD
 FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-masquerade], 0, [ignore], [ignore])
 FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 0, [ignore], [ignore])
 FWD_CHECK([--permanent --zone=internal --remove-source 10.10.10.0/24], 0, [ignore])
+
+m4_if(iptables, FIREWALL_BACKEND, [], [ dnl nftables or offline
+dnl valid for zone --> zone if ingress zone has sources and interfaces
+dnl
+FWD_CHECK([--permanent --zone=internal --add-source 10.10.10.0/24], 0, [ignore])
+FWD_CHECK([--permanent --zone=internal --add-interface foobar1], 0, [ignore])
+FWD_RELOAD
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-masquerade], 0, [ignore], [ignore])
+FWD_CHECK([--permanent --policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 0, [ignore], [ignore])
+FWD_CHECK([            --policy=foobar_int_to_pub --add-masquerade], 0, [ignore], [ignore])
+FWD_CHECK([            --policy=foobar_int_to_pub --add-rich-rule='rule family=ipv4 source address=10.10.10.0/24 masquerade'], 0, [ignore], [ignore])
+FWD_CHECK([--permanent --zone=internal --remove-source 10.10.10.0/24], 0, [ignore])
+FWD_CHECK([--permanent --zone=internal --remove-interface foobar1], 0, [ignore])
+])
 
 FWD_END_TEST([-e '/ERROR: INVALID_ZONE:/d'])

src/tests/features/policy.at
@@ -1655,6 +1655,7 @@ dnl (nat, postrouting)
 NFT_LIST_RULES([inet], [nat_POSTROUTING_POLICIES_pre], 0, [dnl
     table inet firewalld {
         chain nat_POSTROUTING_POLICIES_pre {
+            iifname "foobar1" jump nat_POST_policy_foobar
         }
     }
 ])
@@ -1875,6 +1876,7 @@ dnl (nat, postrouting)
 NFT_LIST_RULES([inet], [nat_POSTROUTING_POLICIES_pre], 0, [dnl
     table inet firewalld {
         chain nat_POSTROUTING_POLICIES_pre {
+            iifname "foobar1" oifname "foobar0" jump nat_POST_policy_foobar
         }
     }
 ])
@@ -1987,6 +1989,7 @@ dnl (nat, postrouting)
 NFT_LIST_RULES([inet], [nat_POSTROUTING_POLICIES_pre], 0, [dnl
     table inet firewalld {
         chain nat_POSTROUTING_POLICIES_pre {
+            iifname "foobar1" ip daddr 10.10.10.0/24 jump nat_POST_policy_foobar
         }
     }
 ])

commit a63ce626bdabc5858017e39fd94153a161723f84

fix(check_config): use on disk firewalld_conf
Instead of the runtime instance.

Fixes: d8ebe1da94d9 ("feat(policy): masquerade: allow ingress zone to have interface")

src/firewall/core/io/functions.py
@@ -20,7 +20,6 @@
 #
 
 import os
-import copy
 
 from firewall import config
 from firewall.errors import FirewallError
@@ -38,6 +37,19 @@ from firewall.core.io.firewalld_conf import firewalld_conf
 
 def check_on_disk_config(fw):
     fw_config = FirewallConfig(fw)
+
+    try:
+        _firewalld_conf = firewalld_conf(config.FIREWALLD_CONF)
+        _firewalld_conf.read()
+    except FirewallError as error:
+        raise FirewallError(error.code, "'%s': %s" % (config.FIREWALLD_CONF, error.msg))
+    except IOError:
+        # defaults will be filled
+        pass
+    except Exception as msg:
+        raise Exception("'%s': %s" % (config.FIREWALLD_CONF, msg))
+    fw_config.set_firewalld_conf(_firewalld_conf)
+
     readers = {
         "ipset": {
             "reader": ipset_reader,
@@ -78,7 +90,6 @@ def check_on_disk_config(fw):
                 if file.endswith(".xml"):
                     obj = readers[reader]["reader"](file, _dir)
                     readers[reader]["add"](obj)
-    fw_config.set_firewalld_conf(copy.deepcopy(fw._firewalld_conf))
     fw_config.full_check_config()
 
     if os.path.isfile(config.FIREWALLD_DIRECT):
@@ -99,11 +110,3 @@ def check_on_disk_config(fw):
             raise FirewallError(error.code, "'%s': %s" % (config.LOCKDOWN_WHITELIST, error.msg))
         except Exception as msg:
             raise Exception("'%s': %s" % (config.LOCKDOWN_WHITELIST, msg))
-    if os.path.isfile(config.FIREWALLD_CONF):
-        try:
-            obj = firewalld_conf(config.FIREWALLD_CONF)
-            obj.read()
-        except FirewallError as error:
-            raise FirewallError(error.code, "'%s': %s" % (config.FIREWALLD_CONF, error.msg))
-        except Exception as msg:
-            raise Exception("'%s': %s" % (config.FIREWALLD_CONF, msg))