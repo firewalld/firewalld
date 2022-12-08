firewalld/firewalld

commit e1d11e8691bf31d816ce556009a38bd7ebd40444

fix(nftables): drop invalid packets before zone dispatch
Be strict about connection states.

Move the drop off invalid packets to occur before zone and policy
dispatch. Otherwise firewalld may allow these bogus packets through
which has odd results, e.g. packet sent but not masqueraded like other
packets.

Users that do want to allow INVALID packets through could allow them by
marking them NOTRACK before they hit firewalld's rules. This means
adding nftables/iptables rules out-of-band of firewalld. Note that these
packets would not be SNAT'd (masquerade) as that requires conntrack (or
stateless NAT rules, but firewalld doesn't provide those).

Fixes: #961

src/firewall/core/nftables.py
@@ -570,22 +570,6 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": "dnat"}},
                                                          {"accept": None}]}}})
-        default_rules.append({"add": {"rule":  {"family": "inet",
-                                                "table": TABLE_NAME,
-                                                "chain": "filter_%s" % "INPUT",
-                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
-                                                                    "op": "==",
-                                                                    "right": "lo"}},
-                                                         {"accept": None}]}}})
-        for dispatch_suffix in ["POLICIES_pre", "ZONES", "POLICIES_post"]:
-            default_rules.append({"add": {"chain": {"family": "inet",
-                                                    "table": TABLE_NAME,
-                                                    "name": "filter_%s_%s" % ("INPUT", dispatch_suffix)}}})
-        for dispatch_suffix in ["ZONES"]:
-            default_rules.append({"add": {"rule":  {"family": "inet",
-                                                    "table": TABLE_NAME,
-                                                    "chain": "filter_%s" % "INPUT",
-                                                    "expr": [{"jump": {"target": "filter_%s_%s" % ("INPUT", dispatch_suffix)}}]}}})
         if log_denied != "off":
             default_rules.append({"add": {"rule":  {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -602,6 +586,22 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": {"set": ["invalid"]}}},
                                                          {"drop": None}]}}})
+        default_rules.append({"add": {"rule":  {"family": "inet",
+                                                "table": TABLE_NAME,
+                                                "chain": "filter_%s" % "INPUT",
+                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
+                                                                    "op": "==",
+                                                                    "right": "lo"}},
+                                                         {"accept": None}]}}})
+        for dispatch_suffix in ["POLICIES_pre", "ZONES", "POLICIES_post"]:
+            default_rules.append({"add": {"chain": {"family": "inet",
+                                                    "table": TABLE_NAME,
+                                                    "name": "filter_%s_%s" % ("INPUT", dispatch_suffix)}}})
+        for dispatch_suffix in ["ZONES"]:
+            default_rules.append({"add": {"rule":  {"family": "inet",
+                                                    "table": TABLE_NAME,
+                                                    "chain": "filter_%s" % "INPUT",
+                                                    "expr": [{"jump": {"target": "filter_%s_%s" % ("INPUT", dispatch_suffix)}}]}}})
         if log_denied != "off":
             default_rules.append({"add": {"rule":  {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -628,6 +628,22 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": "dnat"}},
                                                          {"accept": None}]}}})
+        if log_denied != "off":
+            default_rules.append({"add": {"rule":  {"family": "inet",
+                                                    "table": TABLE_NAME,
+                                                    "chain": "filter_%s" % "FORWARD",
+                                                    "expr": [{"match": {"left": {"ct": {"key": "state"}},
+                                                                        "op": "in",
+                                                                        "right": {"set": ["invalid"]}}},
+                                                             self._pkttype_match_fragment(log_denied),
+                                                             {"log": {"prefix": "STATE_INVALID_DROP: "}}]}}})
+        default_rules.append({"add": {"rule":  {"family": "inet",
+                                                "table": TABLE_NAME,
+                                                "chain": "filter_%s" % "FORWARD",
+                                                "expr": [{"match": {"left": {"ct": {"key": "state"}},
+                                                                    "op": "in",
+                                                                    "right": {"set": ["invalid"]}}},
+                                                         {"drop": None}]}}})
         default_rules.append({"add": {"rule":  {"family": "inet",
                                                 "table": TABLE_NAME,
                                                 "chain": "filter_%s" % "FORWARD",
@@ -651,22 +667,6 @@ class nftables(object):
             default_rules.append({"add": {"chain": {"family": "inet",
                                                     "table": TABLE_NAME,
                                                     "name": "filter_%s_%s" % ("FORWARD", dispatch_suffix)}}})
-        if log_denied != "off":
-            default_rules.append({"add": {"rule":  {"family": "inet",
-                                                    "table": TABLE_NAME,
-                                                    "chain": "filter_%s" % "FORWARD",
-                                                    "expr": [{"match": {"left": {"ct": {"key": "state"}},
-                                                                        "op": "in",
-                                                                        "right": {"set": ["invalid"]}}},
-                                                             self._pkttype_match_fragment(log_denied),
-                                                             {"log": {"prefix": "STATE_INVALID_DROP: "}}]}}})
-        default_rules.append({"add": {"rule":  {"family": "inet",
-                                                "table": TABLE_NAME,
-                                                "chain": "filter_%s" % "FORWARD",
-                                                "expr": [{"match": {"left": {"ct": {"key": "state"}},
-                                                                    "op": "in",
-                                                                    "right": {"set": ["invalid"]}}},
-                                                         {"drop": None}]}}})
         if log_denied != "off":
             default_rules.append({"add": {"rule":  {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -1655,10 +1655,11 @@ class nftables(object):
                                        "chain": "filter_OUTPUT",
                                        "index": 1,
                                        "expr": expr_fragments}}})
+        forward_index = 4 if self._fw.get_log_denied() != "off" else 3
         rules.append({"add": {"rule": {"family": "inet",
                                        "table": TABLE_NAME,
                                        "chain": "filter_FORWARD",
-                                       "index": 2,
+                                       "index": forward_index,
                                        "expr": expr_fragments}}})
         return rules
 

src/tests/cli/firewall-cmd.at
@@ -1577,9 +1577,9 @@ FWD_START_TEST([rich rules priority])
         chain filter_INPUT {
         ct state established,related accept
         ct status dnat accept
+        ct state invalid drop
         iifname "lo" accept
         jump filter_INPUT_ZONES
-        ct state invalid drop
         reject with icmpx admin-prohibited
         }
         }

src/tests/features/policy.at
@@ -711,9 +711,9 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
+            ct state invalid drop
             iifname "lo" accept
             jump filter_INPUT_ZONES
-            ct state invalid drop
             reject with icmpx admin-prohibited
         }
     }
@@ -740,10 +740,10 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
+            ct state invalid drop
             iifname "lo" accept
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
-            ct state invalid drop
             reject with icmpx admin-prohibited
         }
     }

src/tests/features/rfc3964_ipv4.at
@@ -10,11 +10,11 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
     chain filter_FORWARD {
     ct state established,related accept
     ct status dnat accept
+    ct state invalid log prefix "STATE_INVALID_DROP: "
+    ct state invalid drop
     iifname "lo" accept
     ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } log prefix "RFC3964_IPv4_REJECT: " reject with icmpv6 addr-unreachable
     jump filter_FORWARD_ZONES
-    ct state invalid log prefix "STATE_INVALID_DROP: "
-    ct state invalid drop
     log prefix "FINAL_REJECT: "
     reject with icmpx admin-prohibited
     }
@@ -79,10 +79,10 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
     chain filter_FORWARD {
     ct state established,related accept
     ct status dnat accept
-    iifname "lo" accept
-    jump filter_FORWARD_ZONES
     ct state invalid log prefix "STATE_INVALID_DROP: "
     ct state invalid drop
+    iifname "lo" accept
+    jump filter_FORWARD_ZONES
     log prefix "FINAL_REJECT: "
     reject with icmpx admin-prohibited
     }

src/tests/regression/gh258.at
@@ -16,9 +16,9 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
+            ct state invalid drop
             iifname "lo" accept
             jump filter_INPUT_ZONES
-            ct state invalid drop
             reject with icmpx admin-prohibited
         }
     }
@@ -39,10 +39,10 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
+            ct state invalid drop
             iifname "lo" accept
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
-            ct state invalid drop
             reject with icmpx admin-prohibited
         }
     }

src/tests/regression/rhbz1514043.at
@@ -14,10 +14,10 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
-            iifname "lo" accept
-            jump filter_INPUT_ZONES
             ct state invalid log prefix "STATE_INVALID_DROP: "
             ct state invalid drop
+            iifname "lo" accept
+            jump filter_INPUT_ZONES
             log prefix "FINAL_REJECT: "
             reject with icmpx admin-prohibited
         }
@@ -28,11 +28,11 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
+            ct state invalid log prefix "STATE_INVALID_DROP: "
+            ct state invalid drop
             iifname "lo" accept
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } log prefix "RFC3964_IPv4_REJECT: " reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
-            ct state invalid log prefix "STATE_INVALID_DROP: "
-            ct state invalid drop
             log prefix "FINAL_REJECT: "
             reject with icmpx admin-prohibited
         }

commit 251eb229fcb58fe5ded64714147312497ce9e2ce

fix(nftables): allow loopback packets before conntrack checks
Some local proxies can do weird things over the loopback interface that
conntrack does not like, e.g. sport == dport.

Let's assume we can trust anything sent over the loopback interface.

Fixes: #1055
Fixes: e1d11e8691bf ("fix(nftables): drop invalid packets before zone dispatch")

src/firewall/core/nftables.py
@@ -570,6 +570,13 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": "dnat"}},
                                                          {"accept": None}]}}})
+        default_rules.append({"add": {"rule":  {"family": "inet",
+                                                "table": TABLE_NAME,
+                                                "chain": "filter_%s" % "INPUT",
+                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
+                                                                    "op": "==",
+                                                                    "right": "lo"}},
+                                                         {"accept": None}]}}})
         if log_denied != "off":
             default_rules.append({"add": {"rule":  {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -586,13 +593,6 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": {"set": ["invalid"]}}},
                                                          {"drop": None}]}}})
-        default_rules.append({"add": {"rule":  {"family": "inet",
-                                                "table": TABLE_NAME,
-                                                "chain": "filter_%s" % "INPUT",
-                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
-                                                                    "op": "==",
-                                                                    "right": "lo"}},
-                                                         {"accept": None}]}}})
         for dispatch_suffix in ["POLICIES_pre", "ZONES", "POLICIES_post"]:
             default_rules.append({"add": {"chain": {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -628,6 +628,13 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": "dnat"}},
                                                          {"accept": None}]}}})
+        default_rules.append({"add": {"rule":  {"family": "inet",
+                                                "table": TABLE_NAME,
+                                                "chain": "filter_%s" % "FORWARD",
+                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
+                                                                    "op": "==",
+                                                                    "right": "lo"}},
+                                                         {"accept": None}]}}})
         if log_denied != "off":
             default_rules.append({"add": {"rule":  {"family": "inet",
                                                     "table": TABLE_NAME,
@@ -644,13 +651,6 @@ class nftables(object):
                                                                     "op": "in",
                                                                     "right": {"set": ["invalid"]}}},
                                                          {"drop": None}]}}})
-        default_rules.append({"add": {"rule":  {"family": "inet",
-                                                "table": TABLE_NAME,
-                                                "chain": "filter_%s" % "FORWARD",
-                                                "expr": [{"match": {"left": {"meta": {"key": "iifname"}},
-                                                                    "op": "==",
-                                                                    "right": "lo"}},
-                                                         {"accept": None}]}}})
         for dispatch_suffix in ["POLICIES_pre"]:
             default_rules.append({"add": {"chain": {"family": "inet",
                                                     "table": TABLE_NAME,

src/tests/cli/firewall-cmd.at
@@ -1577,8 +1577,8 @@ FWD_START_TEST([rich rules priority])
         chain filter_INPUT {
         ct state established,related accept
         ct status dnat accept
-        ct state invalid drop
         iifname "lo" accept
+        ct state invalid drop
         jump filter_INPUT_ZONES
         reject with icmpx admin-prohibited
         }

src/tests/features/policy.at
@@ -711,8 +711,8 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
-            ct state invalid drop
             iifname "lo" accept
+            ct state invalid drop
             jump filter_INPUT_ZONES
             reject with icmpx admin-prohibited
         }
@@ -740,8 +740,8 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
-            ct state invalid drop
             iifname "lo" accept
+            ct state invalid drop
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
             reject with icmpx admin-prohibited

src/tests/features/rfc3964_ipv4.at
@@ -10,9 +10,9 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
     chain filter_FORWARD {
     ct state established,related accept
     ct status dnat accept
+    iifname "lo" accept
     ct state invalid log prefix "STATE_INVALID_DROP: "
     ct state invalid drop
-    iifname "lo" accept
     ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } log prefix "RFC3964_IPv4_REJECT: " reject with icmpv6 addr-unreachable
     jump filter_FORWARD_ZONES
     log prefix "FINAL_REJECT: "
@@ -79,9 +79,9 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
     chain filter_FORWARD {
     ct state established,related accept
     ct status dnat accept
+    iifname "lo" accept
     ct state invalid log prefix "STATE_INVALID_DROP: "
     ct state invalid drop
-    iifname "lo" accept
     jump filter_FORWARD_ZONES
     log prefix "FINAL_REJECT: "
     reject with icmpx admin-prohibited

src/tests/regression/gh258.at
@@ -16,8 +16,8 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
-            ct state invalid drop
             iifname "lo" accept
+            ct state invalid drop
             jump filter_INPUT_ZONES
             reject with icmpx admin-prohibited
         }
@@ -39,8 +39,8 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
-            ct state invalid drop
             iifname "lo" accept
+            ct state invalid drop
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
             reject with icmpx admin-prohibited

src/tests/regression/rhbz1514043.at
@@ -14,9 +14,9 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
         chain filter_INPUT {
             ct state established,related accept
             ct status dnat accept
+            iifname "lo" accept
             ct state invalid log prefix "STATE_INVALID_DROP: "
             ct state invalid drop
-            iifname "lo" accept
             jump filter_INPUT_ZONES
             log prefix "FINAL_REJECT: "
             reject with icmpx admin-prohibited
@@ -28,9 +28,9 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
         chain filter_FORWARD {
             ct state established,related accept
             ct status dnat accept
+            iifname "lo" accept
             ct state invalid log prefix "STATE_INVALID_DROP: "
             ct state invalid drop
-            iifname "lo" accept
             ip6 daddr { ::/96, ::ffff:0.0.0.0/96, 2002::/24, 2002:a00::/24, 2002:7f00::/24, 2002:a9fe::/32, 2002:ac10::/28, 2002:c0a8::/32, 2002:e000::/19 } log prefix "RFC3964_IPv4_REJECT: " reject with icmpv6 addr-unreachable
             jump filter_FORWARD_ZONES
             log prefix "FINAL_REJECT: "