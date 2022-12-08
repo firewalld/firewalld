firewalld/firewalld

commit 453717291b1d59197fa908237146bbcabc33e695

fix(iptables): drop invalid packets before zone dispatch
See previous commit message for details.

Fixes: #961

src/firewall/core/ipXtables.py
@@ -692,6 +692,9 @@ class ip4tables(object):
         default_rules["filter"] = []
         self.our_chains["filter"] = set()
         default_rules["filter"].append("-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT")
+        if log_denied != "off":
+            default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
+        default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID -j DROP")
         default_rules["filter"].append("-A INPUT -i lo -j ACCEPT")
         default_rules["filter"].append("-N INPUT_direct")
         default_rules["filter"].append("-A INPUT -j INPUT_direct")
@@ -702,13 +705,13 @@ class ip4tables(object):
         for dispatch_suffix in ["ZONES"]:
             default_rules["filter"].append("-A INPUT -j INPUT_%s" % (dispatch_suffix))
         if log_denied != "off":
-            default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
-        default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID -j DROP")
-        if log_denied != "off":
             default_rules["filter"].append("-A INPUT %%LOGTYPE%% -j LOG --log-prefix 'FINAL_REJECT: '")
         default_rules["filter"].append("-A INPUT -j %%REJECT%%")
 
         default_rules["filter"].append("-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT")
+        if log_denied != "off":
+            default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
+        default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID -j DROP")
         default_rules["filter"].append("-A FORWARD -i lo -j ACCEPT")
         default_rules["filter"].append("-N FORWARD_direct")
         default_rules["filter"].append("-A FORWARD -j FORWARD_direct")
@@ -723,9 +726,6 @@ class ip4tables(object):
         for dispatch_suffix in ["POLICIES_post"]:
             default_rules["filter"].append("-N FORWARD_%s" % (dispatch_suffix))
             self.our_chains["filter"].update(set("FORWARD_%s" % (dispatch_suffix)))
-        if log_denied != "off":
-            default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
-        default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID -j DROP")
         if log_denied != "off":
             default_rules["filter"].append("-A FORWARD %%LOGTYPE%% -j LOG --log-prefix 'FINAL_REJECT: '")
         default_rules["filter"].append("-A FORWARD -j %%REJECT%%")
@@ -1511,6 +1511,7 @@ class ip6tables(ip4tables):
         # Inject into FORWARD and OUTPUT chains
         rules.append(["-t", "filter", "-I", "OUTPUT", "4",
                       "-j", chain_name])
-        rules.append(["-t", "filter", "-I", "FORWARD", "4",
+        rules.append(["-t", "filter", "-I", "FORWARD",
+                      "6" if self._fw.get_log_denied() != "off" else "5",
                       "-j", chain_name])
         return rules

src/tests/features/policy.at
@@ -720,18 +720,18 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
 ])
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
 INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
 INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+DROP all ::/0 ::/0 ctstate INVALID
 ACCEPT all ::/0 ::/0
 INPUT_direct all ::/0 ::/0
 INPUT_ZONES all ::/0 ::/0
-DROP all ::/0 ::/0 ctstate INVALID
 REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
 dnl (filter, forward)
@@ -750,19 +750,19 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
 ])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0                                 
 FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
 FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
 ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+DROP all ::/0 ::/0 ctstate INVALID
 ACCEPT all ::/0 ::/0
 FORWARD_direct all ::/0 ::/0
 RFC3964_IPv4 all ::/0 ::/0
 FORWARD_ZONES all ::/0 ::/0
-DROP all ::/0 ::/0 ctstate INVALID
 REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
 dnl (filter, output)

src/tests/features/rfc3964_ipv4.at
@@ -53,12 +53,12 @@ IP6TABLES_LIST_RULES([filter], [RFC3964_IPv4], 0, [dnl
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
-    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all ::/0 ::/0 ctstate INVALID
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
@@ -102,11 +102,11 @@ NFT_LIST_RULES([inet], [filter_OUTPUT], 0, [dnl
 IP6TABLES_LIST_RULES([filter], [RFC3964_IPv4], 1, [ignore], [ignore])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
-    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all ::/0 ::/0 ctstate INVALID
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])

src/tests/regression/gh258.at
@@ -132,10 +132,10 @@ NFT_LIST_RULES([inet], [nat_POSTROUTING_ZONES], 0, [dnl
 
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IPTABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
@@ -146,10 +146,10 @@ IPTABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
 ]])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IPTABLES_LIST_RULES([filter], [FORWARD_ZONES], 0,
@@ -201,10 +201,10 @@ IPTABLES_LIST_RULES([nat], [POSTROUTING_ZONES], 0,
 
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     INPUT_direct all ::/0 ::/0
     INPUT_ZONES all ::/0 ::/0
-    DROP all ::/0 ::/0 ctstate INVALID
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
@@ -215,11 +215,11 @@ IP6TABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
 ]])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
-    DROP all ::/0 ::/0 ctstate INVALID
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD_ZONES], 0,

src/tests/regression/rhbz1514043.at
@@ -41,42 +41,42 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
 
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-    LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     LOG all -- 0.0.0.0/0 0.0.0.0/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
-    LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     LOG all -- 0.0.0.0/0 0.0.0.0/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     INPUT_direct all ::/0 ::/0
     INPUT_ZONES all ::/0 ::/0
-    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all ::/0 ::/0 ctstate INVALID
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
+    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
-    LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
-    DROP all ::/0 ::/0 ctstate INVALID
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
 ])

commit 5d156114e475914a7ea262c38ca8b804a01b8759

fix(iptables): allow loopback packets before conntrack checks
Fixes: 453717291b1d ("fix(iptables): drop invalid packets before zone dispatch")

src/firewall/core/ipXtables.py
@@ -692,10 +692,10 @@ class ip4tables(object):
         default_rules["filter"] = []
         self.our_chains["filter"] = set()
         default_rules["filter"].append("-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT")
+        default_rules["filter"].append("-A INPUT -i lo -j ACCEPT")
         if log_denied != "off":
             default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
         default_rules["filter"].append("-A INPUT -m conntrack --ctstate INVALID -j DROP")
-        default_rules["filter"].append("-A INPUT -i lo -j ACCEPT")
         default_rules["filter"].append("-N INPUT_direct")
         default_rules["filter"].append("-A INPUT -j INPUT_direct")
         self.our_chains["filter"].update(set("INPUT_direct"))
@@ -709,10 +709,10 @@ class ip4tables(object):
         default_rules["filter"].append("-A INPUT -j %%REJECT%%")
 
         default_rules["filter"].append("-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT")
+        default_rules["filter"].append("-A FORWARD -i lo -j ACCEPT")
         if log_denied != "off":
             default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID %%LOGTYPE%% -j LOG --log-prefix 'STATE_INVALID_DROP: '")
         default_rules["filter"].append("-A FORWARD -m conntrack --ctstate INVALID -j DROP")
-        default_rules["filter"].append("-A FORWARD -i lo -j ACCEPT")
         default_rules["filter"].append("-N FORWARD_direct")
         default_rules["filter"].append("-A FORWARD -j FORWARD_direct")
         self.our_chains["filter"].update(set("FORWARD_direct"))

src/tests/features/policy.at
@@ -720,16 +720,16 @@ NFT_LIST_RULES([inet], [filter_INPUT], 0, [dnl
 ])
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
-DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
+DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
 INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
 REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
-DROP all ::/0 ::/0 ctstate INVALID
 ACCEPT all ::/0 ::/0
+DROP all ::/0 ::/0 ctstate INVALID
 INPUT_direct all ::/0 ::/0
 INPUT_ZONES all ::/0 ::/0
 REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
@@ -750,16 +750,16 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
 ])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
-DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 ACCEPT all -- 0.0.0.0/0 0.0.0.0/0                                 
+DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
 FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
 FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
 REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
 ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
-DROP all ::/0 ::/0 ctstate INVALID
 ACCEPT all ::/0 ::/0
+DROP all ::/0 ::/0 ctstate INVALID
 FORWARD_direct all ::/0 ::/0
 RFC3964_IPv4 all ::/0 ::/0
 FORWARD_ZONES all ::/0 ::/0

src/tests/features/rfc3964_ipv4.at
@@ -53,9 +53,9 @@ IP6TABLES_LIST_RULES([filter], [RFC3964_IPv4], 0, [dnl
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all ::/0 ::/0
     LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all ::/0 ::/0 ctstate INVALID
-    ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
@@ -102,9 +102,9 @@ NFT_LIST_RULES([inet], [filter_OUTPUT], 0, [dnl
 IP6TABLES_LIST_RULES([filter], [RFC3964_IPv4], 1, [ignore], [ignore])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all ::/0 ::/0
     LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all ::/0 ::/0 ctstate INVALID
-    ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "

src/tests/regression/gh258.at
@@ -132,8 +132,8 @@ NFT_LIST_RULES([inet], [nat_POSTROUTING_ZONES], 0, [dnl
 
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
@@ -146,8 +146,8 @@ IPTABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
 ]])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
-    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
+    DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
     FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
     REJECT all -- 0.0.0.0/0 0.0.0.0/0 reject-with icmp-host-prohibited
@@ -201,8 +201,8 @@ IPTABLES_LIST_RULES([nat], [POSTROUTING_ZONES], 0,
 
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
-    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
+    DROP all ::/0 ::/0 ctstate INVALID
     INPUT_direct all ::/0 ::/0
     INPUT_ZONES all ::/0 ::/0
     REJECT all ::/0 ::/0 reject-with icmp6-adm-prohibited
@@ -215,8 +215,8 @@ IP6TABLES_LIST_RULES([filter], [INPUT_ZONES], 0,
 ]])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
-    DROP all ::/0 ::/0 ctstate INVALID
     ACCEPT all ::/0 ::/0
+    DROP all ::/0 ::/0 ctstate INVALID
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0

src/tests/regression/rhbz1514043.at
@@ -41,9 +41,9 @@ NFT_LIST_RULES([inet], [filter_FORWARD], 0, [dnl
 
 IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
-    ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_direct all -- 0.0.0.0/0 0.0.0.0/0
     INPUT_ZONES all -- 0.0.0.0/0 0.0.0.0/0
     LOG all -- 0.0.0.0/0 0.0.0.0/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
@@ -51,9 +51,9 @@ IPTABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ])
 IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all -- 0.0.0.0/0 0.0.0.0/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     LOG all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all -- 0.0.0.0/0 0.0.0.0/0 ctstate INVALID
-    ACCEPT all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_direct all -- 0.0.0.0/0 0.0.0.0/0
     FORWARD_ZONES all -- 0.0.0.0/0 0.0.0.0/0
     LOG all -- 0.0.0.0/0 0.0.0.0/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
@@ -61,9 +61,9 @@ IPTABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
 ])
 IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all ::/0 ::/0
     LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all ::/0 ::/0 ctstate INVALID
-    ACCEPT all ::/0 ::/0
     INPUT_direct all ::/0 ::/0
     INPUT_ZONES all ::/0 ::/0
     LOG all ::/0 ::/0 LOG flags 0 level 4 prefix "FINAL_REJECT: "
@@ -71,9 +71,9 @@ IP6TABLES_LIST_RULES([filter], [INPUT], 0, [dnl
 ])
 IP6TABLES_LIST_RULES([filter], [FORWARD], 0, [dnl
     ACCEPT all ::/0 ::/0 ctstate RELATED,ESTABLISHED,DNAT
+    ACCEPT all ::/0 ::/0
     LOG all ::/0 ::/0 ctstate INVALID LOG flags 0 level 4 prefix "STATE_INVALID_DROP: "
     DROP all ::/0 ::/0 ctstate INVALID
-    ACCEPT all ::/0 ::/0
     FORWARD_direct all ::/0 ::/0
     RFC3964_IPv4 all ::/0 ::/0
     FORWARD_ZONES all ::/0 ::/0