94795a3a78f076f87cde7d2d3028881d39c4b926
Donald Yandt <10255876+TorontoMedia@users.noreply.github.com>
2023-01-20 08:48:05 -0500

    fix(fw_policy): raise exceptions
    (cherry picked from commit 405228a36d18675cb9849370e87af01ebf5b0572)

--- src/firewall/core/fw_policy.py
+++ src/firewall/core/fw_policy.py
@@ -1863,8 +1863,7 @@ class FirewallPolicy(object):
         elif "ANY" in obj.ingress_zones:
             # any --> zone
             return [("nat", "POSTROUTING")]
-        else:
-            return FirewallError("Invalid policy: %s" % (policy))
+        raise FirewallError("Invalid policy: %s" % (policy))
 
     def policy_base_chain_name(self, policy, table, policy_prefix, isSNAT=False):
         obj = self._fw.policy.get_policy(policy)
@@ -1922,4 +1921,4 @@ class FirewallPolicy(object):
                     return "PRE_" + suffix
             elif table in ["mangle", "raw"]:
                 return "PRE_" + suffix
-        return FirewallError("Can't convert policy to chain name: %s, %s, %s" % (policy, table, isSNAT))
+        raise FirewallError("Can't convert policy to chain name: %s, %s, %s" % (policy, table, isSNAT))
