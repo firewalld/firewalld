firewalld/firewalld

commit 11567b74317e4d9de1a1aee4f92b5f6f3a0b9422

New firewall.core.icmp providing names and types for icmp and icmpv6 values
This is needed to be able to parse icmp and icmpv6/ipv6-icmp names and types
for additional ipset types.

src/firewall/core/icmp.py
@@ -1,0 +1,104 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# Authors:
+# Thomas Woerner <twoerner@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__all__ = [ "ICMP_TYPES", "ICMPV6_TYPES",
+            "check_icmp_type", "check_icmpv6_type" ]
+
+ICMP_TYPES = {
+     "echo-reply": "0/0",
+     "pong": "0/0",
+     "network-unreachable": "3/0",
+     "host-unreachable": "3/1",
+     "protocol-unreachable": "3/2",
+     "port-unreachable": "3/3",
+     "fragmentation-needed": "3/4",
+     "source-route-failed": "3/5",
+     "network-unknown": "3/6",
+     "host-unknown": "3/7",
+     "network-prohibited": "3/9",
+     "host-prohibited": "3/10",
+     "TOS-network-unreachable": "3/11",
+     "TOS-host-unreachable": "3/12",
+     "communication-prohibited": "3/13",
+     "host-precedence-violation": "3/14",
+     "precedence-cutoff": "3/15",
+     "source-quench": "4/0",
+     "network-redirect": "5/0",
+     "host-redirect": "5/1",
+     "TOS-network-redirect": "5/2",
+     "TOS-host-redirect": "5/3",
+     "echo-request": "8/0",
+     "ping": "8/0",
+     "router-advertisement": "9/0",
+     "router-solicitation": "10/0",
+     "ttl-zero-during-transit": "11/0",
+     "ttl-zero-during-reassembly": "11/1",
+     "ip-header-bad": "12/0",
+     "required-option-missing": "12/1",
+     "timestamp-request": "13/0",
+     "timestamp-reply": "14/0",
+     "address-mask-request": "17/0",
+     "address-mask-reply": "18/0",
+}
+
+ICMPV6_TYPES = {
+    "no-route": "1/0",
+    "communication-prohibited": "1/1",
+    "address-unreachable": "1/3",
+    "port-unreachable": "1/4",
+    "packet-too-big": "2/0",
+    "ttl-zero-during-transit": "3/0",
+    "ttl-zero-during-reassembly": "3/1",
+    "bad-header": "4/0",
+    "unknown-header-type": "4/1",
+    "unknown-option": "4/2",
+    "echo-request": "128/0",
+    "ping": "128/0",
+    "echo-reply": "129/0",
+    "pong": "129/0",
+    "router-solicitation": "133/0",
+    "router-advertisement": "134/0",
+    "neighbour-solicitation": "135/0",
+    "neigbour-solicitation": "135/0",
+    "neighbour-advertisement": "136/0",
+    "neigbour-advertisement": "136/0",
+    "redirect": "137/0",
+}
+
+def check_icmp_name(_name):
+    if _name in ICMP_TYPES:
+        return True
+    return False
+
+def check_icmp_type(_type):
+    if _type in ICMP_TYPES.values():
+        return True
+    return False
+
+def check_icmpv6_name(_name):
+    if _name in ICMP_TYPES:
+        return True
+    return False
+
+def check_icmpv6_type(_type):
+    if _type in ICMPV6_TYPES.values():
+        return True
+    return False

src/Makefile.am
@@ -47,6 +47,7 @@ nobase_dist_python_DATA = \
 	firewall/core/io/lockdown_whitelist.py \
 	firewall/core/io/service.py \
 	firewall/core/io/zone.py \
+	firewall/core/icmp.py \
 	firewall/core/ipset.py \
 	firewall/core/ipXtables.py \
 	firewall/core/logger.py \

commit e58b4a7d51e730c2ec1d04c5942f8d608b1f6e79

fix(icmp): fix check_icmpv6_name() to use correct IPv6 names
This was only called from IPSet.check_entry() to validate the input.
Also, some of the IPv4 names are the same as for IPv6. Overall, the
impact of this was probably low.

Fixes: 11567b74317e ('New firewall.core.icmp providing names and types for icmp and icmpv6 values')

src/firewall/core/icmp.py
@@ -94,7 +94,7 @@ def check_icmp_type(_type):
     return False
 
 def check_icmpv6_name(_name):
-    if _name in ICMP_TYPES:
+    if _name in ICMPV6_TYPES:
         return True
     return False
 