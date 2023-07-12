deaa6ce51d1c8721c5d69ecbdc94e90aa4705492
Thomas Haller <thaller@redhat.com>
2023-07-12 14:17:38 +0200

    fix(ipset): fix configuring "timeout","maxelem" values for ipsets with nftables
    With
    
        firewall-cmd --permanent --delete-ipset=testipset || :
        firewall-cmd --permanent --new-ipset=testipset --type=hash:ip --option=maxelem=65536 --option=family=inet --option=hashsize=4096 --option=timeout=14400
        firewall-cmd --reload
    
    firewalld would send the JSON request
    
        {
          "add": {
            "set": {
              "family": "inet",
              "table": "firewalld",
              "name": "testipset",
              "type": "ipv4_addr",
              "flags": [
                "interval"
              ],
              "timeout": "14400",
              "size": "65536"
            }
          }
        },
    
    But the "timeout","size" keys are NUMBER types in libnftables-json. They are
    silently ignored otherwise ([1]). The fix is to pass them as numbers.
    
    Try also:
    
        nft delete table inet testtable &>/dev/null || :
        nft add table inet testtable
        echo '
            {
              "nftables": [
                {
                  "metainfo": {
                    "json_schema_version": 1
                  }
                },
                {
                  "add": {
                    "set": {
                      "family": "inet",
                      "table": "testtable",
                      "name": "testipset",
                      "type": "ipv4_addr",
                      "flags": [
                        "interval"
                      ],
                      "timeout": "14400",
                      "size": "65536"
                    }
                  }
                },
                {
                  "flush": {
                    "set": {
                      "family": "inet",
                      "table": "testtable",
                      "name": "testipset"
                    }
                  }
                }
              ]
            }
        ' | nft -j -f -
        nft list ruleset | grep -C6 testipset
    
    [1] https://git.netfilter.org/nftables/tree/src/parser_json.c?id=1335ade24e55199069b8ae79e34746a59ae48c01#n1440
    
    https://github.com/firewalld/firewalld/issues/699
    https://github.com/firewalld/firewalld/issues/908
    https://bugzilla.redhat.com/show_bug.cgi?id=2055330
    
    Fixes: #699
    Fixes: 1582c5dd736a ('feat: nftables: convert to libnftables JSON interface')

--- src/firewall/core/nftables.py
+++ src/firewall/core/nftables.py
@@ -1618,9 +1618,9 @@ class nftables:
 
         if options:
             if "timeout" in options:
-                set_dict["timeout"] = options["timeout"]
+                set_dict["timeout"] = int(options["timeout"])
             if "maxelem" in options:
-                set_dict["size"] = options["maxelem"]
+                set_dict["size"] = int(options["maxelem"])
 
         return [{"add": {"set": set_dict}}]
 
