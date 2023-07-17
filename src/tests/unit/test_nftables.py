43f7d7afc0e60f43e6af4a395aee6e1d4d9c78d0
Thomas Haller <thaller@redhat.com>
2023-07-17 17:44:21 +0200

    improvement(nftables): add unit tests for firewall.core.nftables._entry_to_json()

---
+++ src/tests/unit/test_nftables.py
@@ -0,0 +1,99 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import firewall.core.nftables
+
+
+###############################################################################
+
+
+def test_nftables_entry_to_json():
+    assert firewall.core.nftables.nftables._entry_to_json(
+        "1::2/1,eth0,http", "hash:net,iface,port", "ipv6"
+    ) == [
+        {
+            "concat": [
+                {
+                    "prefix": {
+                        "addr": "1::2",
+                        "len": 1,
+                    },
+                },
+                "eth0",
+                "tcp",
+                "http",
+            ],
+        },
+    ]
+
+    assert firewall.core.nftables.nftables._entry_to_json(
+        "1::2/1,eth0,http,1::00-2::,1:0::-3::0/64,udp:70-60",
+        "hash:net,iface,port,net,net,port",
+        "ipv6",
+    ) == [
+        {
+            "concat": [
+                {
+                    "prefix": {
+                        "addr": "1::2",
+                        "len": 1,
+                    },
+                },
+                "eth0",
+                "tcp",
+                "http",
+                {
+                    "range": [
+                        "1::00",
+                        "2::",
+                    ],
+                },
+                {
+                    "range": [
+                        "1:0::",
+                        "3::0/64",
+                    ],
+                },
+                "udp",
+                {
+                    "range": [
+                        "70",
+                        "60",
+                    ],
+                },
+            ],
+        },
+    ]
+
+    assert firewall.core.nftables.nftables._entry_to_json(
+        "1.2.3.0/24,AA:00:11:22:33:44,0xFc5,eth0,999",
+        "hash:ip,mac,mark,iface,mark",
+        "ipv4",
+    ) == [
+        {
+            "concat": [
+                {
+                    "prefix": {
+                        "addr": "1.2.3.0",
+                        "len": 24,
+                    },
+                },
+                "AA:00:11:22:33:44",
+                "0xFc5",
+                "eth0",
+                "999",
+            ],
+        },
+    ]
+
+    assert firewall.core.nftables.nftables._entry_to_json(
+        "1.2.3.0-1.2.5.0/24",
+        "hash:net",
+        "ipv4",
+    ) == [
+        {
+            "range": [
+                "1.2.3.0",
+                "1.2.5.0/24",
+            ],
+        },
+    ]
