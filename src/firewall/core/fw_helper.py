e56371bb3836e30d3d54fb8534a68fa7aa5e5509
Thomas Haller <thaller@redhat.com>
2023-07-21 17:22:59 +0200

    improvement(helper): cleanup check_helper() function

--- src/firewall/core/fw_helper.py
+++ src/firewall/core/fw_helper.py
@@ -24,22 +24,26 @@ class FirewallHelper:
     def cleanup(self):
         self._helpers.clear()
 
-    def check_helper(self, name):
-        if name not in self.get_helpers():
-            raise FirewallError(errors.INVALID_HELPER, name)
-
-    def query_helper(self, name):
-        return name in self.get_helpers()
-
     def get_helpers(self):
-        return sorted(self._helpers.keys())
+        return sorted(self._helpers)
 
     def has_helpers(self):
         return len(self._helpers) > 0
 
-    def get_helper(self, name):
-        self.check_helper(name)
-        return self._helpers[name]
+    def query_helper(self, name):
+        return self.has_helper(name)
+
+    def check_helper(self, name):
+        return self.get_helper(name).name
+
+    def has_helper(self, name):
+        return self.get_helper(name, required=False) is not None
+
+    def get_helper(self, name, required=True):
+        obj = self._helpers.get(name)
+        if obj is None and required:
+            raise FirewallError(errors.INVALID_HELPER, name)
+        return obj
 
     def add_helper(self, obj):
         self._helpers[obj.name] = obj
