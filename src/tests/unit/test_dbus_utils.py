48926fe8f294a2072dee67214bb7103cb3da9bbf
Thomas Haller <thaller@redhat.com>
2023-07-27 10:51:23 +0200

    improvement(dbus_utils): extend dbus_to_python() and add dbus_to_python_args() helper
    dbus_to_python() now also supports as expected types the D-Bus types
    (for example, dbus.ObjectPath). The benefit is of course, to assert that
    the type is "o" and not merely "s" (both are converted to str).
    
    Also add dbus_to_python_args(), which extracts a duple of D-Bus
    arguments, checks the types and returns the corresponding python values.

---
+++ src/tests/unit/test_dbus_utils.py
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import dbus
+import pytest
+
+import firewall.dbus_utils
+
+
+def test_dbus_to_python():
+
+    assert firewall.dbus_utils.dbus_to_python_args(
+        [dbus.String("a"), dbus.String("b")], str, str
+    ) == ("a", "b")
+
+    assert firewall.dbus_utils.dbus_to_python_args(
+        [dbus.String("a"), dbus.ObjectPath("/b")], str, str
+    ) == ("a", "/b")
+
+    assert firewall.dbus_utils.dbus_to_python_args(
+        [dbus.String("a"), dbus.ObjectPath("/b")],
+        dbus.String,
+        dbus.ObjectPath,
+    ) == ("a", "/b")
+
+    assert firewall.dbus_utils.dbus_to_python_args([]) == ()
+
+    with pytest.raises(TypeError):
+        firewall.dbus_utils.dbus_to_python_args([dbus.String("a")], dbus.ObjectPath)
+
+    assert firewall.dbus_utils.dbus_to_python_args([dbus.String("")], dbus.String) == (
+        "",
+    )
+
+    with pytest.raises(TypeError):
+        firewall.dbus_utils.dbus_to_python_args([dbus.String("a")])
+
+    with pytest.raises(TypeError):
+        firewall.dbus_utils.dbus_to_python_args([dbus.String("a")], str, str)
+
+    assert firewall.dbus_utils.dbus_to_python_args([dbus.Int16(5)], int) == (5,)
+    assert firewall.dbus_utils.dbus_to_python_args([dbus.Int16(5)], dbus.Int16) == (5,)
+
+    assert firewall.dbus_utils.dbus_to_python_args([dbus.Boolean(True)], int) == (True,)
+    assert firewall.dbus_utils.dbus_to_python_args([dbus.Boolean(True)], bool) == (
+        True,
+    )
+    assert firewall.dbus_utils.dbus_to_python_args(
+        [dbus.Boolean(True)], dbus.Boolean
+    ) == (True,)
+
+    with pytest.raises(TypeError):
+        firewall.dbus_utils.dbus_to_python_args([dbus.Int16(5)], bool)
