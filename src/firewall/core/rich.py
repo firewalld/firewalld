ebcdcdfa45304c5ddf81b0b7a46744a3c24e0a42
Eric Garver <eric@garver.life>
2023-08-03 13:52:33 -0400

    chore(rich): do not derive Rich_SourcePort from Rich_Port
    It's a very small object and this avoids bugs were we try to use
    isinstance(rich_rule, Rich_Port), which would be true for a
    Rich_SourcePort object.

--- src/firewall/core/rich.py
+++ src/firewall/core/rich.py
@@ -90,7 +90,11 @@ class Rich_Port:
     def __str__(self):
         return 'port port="%s" protocol="%s"' % (self.port, self.protocol)
 
-class Rich_SourcePort(Rich_Port):
+class Rich_SourcePort:
+    def __init__(self, port, protocol):
+        self.port = port
+        self.protocol = protocol
+
     def __str__(self):
         return 'source-port port="%s" protocol="%s"' % (self.port,
                                                         self.protocol)
