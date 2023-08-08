firewalld/firewalld

commit 3f9393741ccee4659af99efe4747059660f69d94

docs(rich): update docs to support tcp-mss-clamp
Fixes: #493

Made tcp-mss-clamp mutually exclusive action and valid without value

src/firewall/core/io/policy.py
@@ -85,8 +85,14 @@ def common_startElement(obj, name, attrs):
                             str(obj._rule))
                 obj._rule_error = True
                 return True
-            obj._rule.element = rich.Rich_Tcp_Mss_Clamp(attrs["value"])
-    
+            _value="pmtu"
+            if "value" in attrs:
+                _value = attrs["value"]
+            obj._rule.element = rich.Rich_Tcp_Mss_Clamp(_value)
+        else:
+            log.warning("Invalid rule: tcp-mss-clamp '%s' outside of rule",
+                        attrs["value"])
+
     elif name == "icmp-block":
         if obj._rule:
             if obj._rule.element:

doc/xml/firewalld.richlanguage.xml
@@ -178,6 +178,22 @@ protocol value="protocol value"
     </refsect2>
 
     <refsect2>
+      <title>Tcp-Mss-Clamp</title>
+      <para>
+        <programlisting>
+tcp-mss-clamp="value=pmtu|value=number >= 536|None"
+        </programlisting>
+      </para>
+      <para>
+        The tcp-mss-clamp sets the maximum segment size in the rule.
+      </para>
+      <para>
+        The tcp-mss-clamp has an optional attribute value can be either be set to "pmtu" or a number greater than or equal to 536. If attribute
+	value is not present then the maximum segment size is automatically set to "pmtu".
+      </para>
+    </refsect2>
+
+    <refsect2>
       <title>ICMP-Block</title>
       <para>
 	<programlisting>

doc/xml/policy_zone_descriptions.xml
@@ -130,6 +130,25 @@
     </variablelist>
 </refsect2>
 
+<refsect2 id="tcp-mss-clamp">
+    <title>tcp-mss-clamp</title>
+    <para>
+        Is an optional empty-element tag and can be used several times. If left empty
+	maximum segment size is set to 'pmtu'. This tag has exactly one optional attribute:
+      </para>
+      <variablelist>
+        <varlistentry>
+          <term>value="<replaceable>string</replaceable>"</term>
+          <listitem>
+            <para>
+              Value can set maximum segment size to 'pmtu' (Path Maximum Transmission Unit) or
+	      a user-defined value that is greater than or equal to 536.
+            </para>
+          </listitem>
+        </varlistentry>
+    </variablelist>
+</refsect2>
+
 <refsect2 id="masquerade">
     <title>masquerade</title>
     <para>

src/firewall/core/fw_policy.py
@@ -1589,7 +1589,7 @@ class FirewallPolicy(object):
 
                 rules = backend.build_policy_tcp_mss_clamp_rules(
                             enable, policy, tcp_mss_clamp_value, None, rule)
-                transaction.add_rules(backend, rules) 
+                transaction.add_rules(backend, rules)
 
             # MASQUERADE
             elif type(rule.element) == Rich_Masquerade:

src/firewall/core/ipXtables.py
@@ -29,7 +29,7 @@ from firewall.functions import tempFile, readfile, splitArgs, check_mac, portStr
 from firewall import config
 from firewall.errors import FirewallError, INVALID_PASSTHROUGH, INVALID_RULE, UNKNOWN_ERROR, INVALID_ADDR
 from firewall.core.rich import Rich_Accept, Rich_Reject, Rich_Drop, Rich_Mark, \
-                               Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock
+                               Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock, Rich_Tcp_Mss_Clamp
 import string
 
 POLICY_CHAIN_PREFIX = ""
@@ -995,7 +995,7 @@ class ip4tables(object):
         return []
 
     def _rich_rule_chain_suffix(self, rich_rule):
-        if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock]:
+        if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock, Rich_Tcp_Mss_Clamp]:
             # These are special and don't have an explicit action
             pass
         elif rich_rule.action:
@@ -1005,7 +1005,7 @@ class ip4tables(object):
             raise FirewallError(INVALID_RULE, "No rule action specified.")
 
         if rich_rule.priority == 0:
-            if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort] or \
+            if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_Tcp_Mss_Clamp] or \
                type(rich_rule.action) in [Rich_Accept, Rich_Mark]:
                 return "allow"
             elif type(rich_rule.element) in [Rich_IcmpBlock] or \
@@ -1080,14 +1080,13 @@ class ip4tables(object):
     def _rich_rule_action(self, policy, rich_rule, enable, table, rule_fragment):
         if not rich_rule.action:
             return []
-        
-        add_del = { True: "-A", False: "-D" }[enable] 
-        
+
+        add_del = { True: "-A", False: "-D" }[enable]
+
         _policy = self._fw.policy.policy_base_chain_name(policy, table, POLICY_CHAIN_PREFIX)
 
         chain_suffix = self._rich_rule_chain_suffix(rich_rule)
         chain = "%s_%s" % (_policy, chain_suffix)
-        
         if type(rich_rule.action) == Rich_Accept:
             rule_action = [ "-j", "ACCEPT" ]
         elif type(rich_rule.action) == Rich_Reject:
@@ -1109,7 +1108,7 @@ class ip4tables(object):
         rule += self._rich_rule_priority_fragment(rich_rule)
         rule += rule_fragment + rule_action
         rule += self._rule_limit(rich_rule.action.limit)
-       
+
         return rule
 
     def _rich_rule_destination_fragment(self, rich_dest):
@@ -1210,37 +1209,32 @@ class ip4tables(object):
 
         return rules
 
-    def build_policy_tcp_mss_clamp_rules(self, enable, policy, tcp_mss_clamp_value, destination=None, rich_rule=None):   
-        add_del = { True: "-A", False: "-D" }[enable]
+    def build_policy_tcp_mss_clamp_rules(self, enable, policy, tcp_mss_clamp_value, destination=None, rich_rule=None):
         table = "filter"
         _policy = self._fw.policy.policy_base_chain_name(policy, table, POLICY_CHAIN_PREFIX)
-       
-        chain_suffix = self._rich_rule_chain_suffix(rich_rule)
-        chain = "%s_%s" % (_policy, chain_suffix)
-        
-        rule_fragment = ["-p", "tcp"]
-        if destination:
-            rule_fragment += [ "-d", destination ]
+        add_del = { True: "-A", False: "-D" }[enable]
+
+        rule_fragment = []
         if rich_rule:
+            chain_suffix = self._rich_rule_chain_suffix(rich_rule)
+            rule_fragment += self._rich_rule_priority_fragment(rich_rule)
             rule_fragment += self._rich_rule_destination_fragment(rich_rule.destination)
             rule_fragment += self._rich_rule_source_fragment(rich_rule.source)
 
-        if tcp_mss_clamp_value == "pmtu":
+        rules = []
+        rule_fragment = ["-p", "tcp"]
+        if tcp_mss_clamp_value == "pmtu" or tcp_mss_clamp_value is None:
             rule_fragment += ["--tcp-flags", "SYN,RST", "SYN","-j", "TCPMSS", "--clamp-mss-to-pmtu"]
         else:
             rule_fragment += ["--tcp-flags", "SYN,RST", "SYN", "-j", "TCPMSS", "--set-mss", tcp_mss_clamp_value]
-        
-        rule = ["-t", table, add_del, chain]
-        rule += self._rich_rule_priority_fragment(rich_rule)
-        rule += rule_fragment
-        rule += self._rule_limit(rich_rule.action.limit)
 
-        rules = []
         if rich_rule:
-            rules.append(self._rich_rule_log(policy, rich_rule, enable, table, rule_fragment))
-            rules.append(self._rich_rule_audit(policy, rich_rule, enable, table, rule_fragment))
-            rules.append(rule)
-
+            chain_suffix = self._rich_rule_chain_suffix(rich_rule)
+            rule_fragment += self._rich_rule_priority_fragment(rich_rule)
+            rule_fragment += self._rich_rule_destination_fragment(rich_rule.destination)
+            rule_fragment += self._rich_rule_source_fragment(rich_rule.source)
+        rules.append(["-t", "filter", add_del, "%s_%s" % (_policy, chain_suffix)]
+                     + rule_fragment)
         return rules
 
     def build_policy_source_ports_rules(self, enable, policy, proto, port,

src/firewall/core/nftables.py
@@ -30,7 +30,8 @@ from firewall.errors import FirewallError, UNKNOWN_ERROR, INVALID_RULE, \
                             INVALID_ICMPTYPE, INVALID_TYPE, INVALID_ENTRY, \
                             INVALID_PORT
 from firewall.core.rich import Rich_Accept, Rich_Reject, Rich_Drop, Rich_Mark, \
-                               Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock
+                               Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock, \
+                               Rich_Tcp_Mss_Clamp
 from nftables.nftables import Nftables
 
 TABLE_NAME = "firewalld"
@@ -1065,7 +1066,7 @@ class nftables(object):
                           "per": rich_to_nft[limit.value[i+1]]}}
 
     def _rich_rule_chain_suffix(self, rich_rule):
-        if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock]:
+        if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_IcmpBlock, Rich_Tcp_Mss_Clamp]:
             # These are special and don't have an explicit action
             pass
         elif rich_rule.action:
@@ -1075,7 +1076,7 @@ class nftables(object):
             raise FirewallError(INVALID_RULE, "No rule action specified.")
 
         if rich_rule.priority == 0:
-            if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort] or \
+            if type(rich_rule.element) in [Rich_Masquerade, Rich_ForwardPort, Rich_Tcp_Mss_Clamp] or \
                type(rich_rule.action) in [Rich_Accept, Rich_Mark]:
                 return "allow"
             elif type(rich_rule.element) in [Rich_IcmpBlock] or \
@@ -1115,7 +1116,6 @@ class nftables(object):
         if not rich_rule or rich_rule.priority == 0:
             return {}
         return {"%%RICH_RULE_PRIORITY%%": rich_rule.priority}
-
 
     def _rich_rule_log(self, policy, rich_rule, enable, table, expr_fragments):
         if not rich_rule.log:
@@ -1342,37 +1342,31 @@ class nftables(object):
 
     def build_policy_tcp_mss_clamp_rules(self, enable, policy, tcp_mss_clamp_value, 
                                       destination=None, rich_rule=None):
-        table = "filter" 
-        
+        table = "filter"
+        _policy = self._fw.policy.policy_base_chain_name(policy, table, POLICY_CHAIN_PREFIX)
+        add_del = { True: "add", False: "delete" }[enable]
+
         expr_fragments = []
-        if rich_rule:
-            expr_fragments.append(self._rich_rule_family_fragment(rich_rule.family))
-        if destination:
-            expr_fragments.append(self._rule_addr_fragment("daddr", destination))
         if rich_rule:
             expr_fragments.append(self._rich_rule_destination_fragment(rich_rule.destination))
             expr_fragments.append(self._rich_rule_source_fragment(rich_rule.source))
-
-        expr_fragments.append({"match": {"op": "==",
-                                         "left": {"meta": {"key": "oifname"}},
-                                         "right": "ppp0"}})
+            chain_suffix = self._rich_rule_chain_suffix(rich_rule)
 
         expr_fragments.append({"match": {"op": "in",
                                          "left": {"payload": {"protocol": "tcp","field": "flags"}},
                                          "right": "syn"}})
 
-        if tcp_mss_clamp_value == "pmtu":
+        if tcp_mss_clamp_value == "pmtu" or tcp_mss_clamp_value is None:
             expr_fragments.append({"mangle": {"key": {"tcp option": {"name": "maxseg","field": "size"}},
                                               "value": {"rt": {"key": "mtu" }}}})
         else:
             expr_fragments.append({"mangle": {"key": {"tcp option": {"name": "maxseg","field": "size"}},
-                                              "value": tcp_mss_clamp_value}}) 
-
+                                              "value": tcp_mss_clamp_value}})
         rules = []
-        if rich_rule:
-            rules.append(self._rich_rule_log(policy, rich_rule, enable, table, expr_fragments))
-            rules.append(self._rich_rule_audit(policy, rich_rule, enable, table, expr_fragments))
-            rules.append(self._rich_rule_action(policy, rich_rule, enable, table, expr_fragments))
+        rules.append({add_del: {"rule": {"family": "inet",
+                                "table": TABLE_NAME,
+                                "chain": "filter_%s_%s" % (_policy, chain_suffix),
+                                "expr": expr_fragments}}})
 
         return rules
 

src/firewall/core/rich.py
@@ -117,16 +117,14 @@ class Rich_IcmpType(object):
         self.name = name
 
     def __str__(self):
-
         return 'icmp-type name="%s"' % (self.name)
- 
+
 class Rich_Tcp_Mss_Clamp(object):
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return 'tcp-mss-clamp value="%s"' % (self.value)
-
 
 class Rich_ForwardPort(object):
     def __init__(self, port, protocol, to_port, to_address):
@@ -298,7 +296,7 @@ class Rich_Rule(object):
     def _lexer(self, rule_str):
         """ Lexical analysis """
         tokens = []
-        
+
         for r in functions.splitArgs(rule_str):
             if "=" in r:
                 attr = r.split('=')
@@ -428,10 +426,12 @@ class Rich_Rule(object):
                     raise FirewallError(errors.INVALID_RULE, "invalid 'protocol' element")
             elif in_element == 'tcp-mss-clamp':
                 if attr_name == 'value':
-                    self.element = Rich_Tcp_Mss_Clamp(attr_value)
-                    in_elements.pop()   #tcp-mss-clamp
+                    attrs[attr_name] = attr_value
                 else:
-                    raise FirewallError(errors.INVALID_RULE, "invalid 'tcp-mss-clamp' element")
+                    self.element = Rich_Tcp_Mss_Clamp(attrs.get('value'))
+                    in_elements.pop()
+                    attrs.clear()
+                    index = index -1
             elif in_element == 'service':
                 if attr_name == 'name':
                     self.element = Rich_Service(attr_value)
@@ -568,7 +568,8 @@ class Rich_Rule(object):
 
         if type(self.element) not in [ Rich_IcmpBlock,
                                        Rich_ForwardPort,
-                                       Rich_Masquerade ]:
+                                       Rich_Masquerade,
+                                       Rich_Tcp_Mss_Clamp ]:
             if self.log is None and self.audit is None and \
                     self.action is None:
                 raise FirewallError(errors.INVALID_RULE, "no action, no log, no audit")
@@ -625,7 +626,7 @@ class Rich_Rule(object):
         elif type(self.element) == Rich_Protocol:
             if not functions.checkProtocol(self.element.value):
                 raise FirewallError(errors.INVALID_PROTOCOL, self.element.value)
-        
+
         # masquerade
         elif type(self.element) == Rich_Masquerade:
             if self.action is not None:
@@ -670,22 +671,24 @@ class Rich_Rule(object):
                 raise FirewallError(errors.INVALID_RULE, "forward-port and action")
 
         # source-port
-        elif type(self.element) == Rich_SourcePort:	
-            if not functions.check_port(self.element.port):	
-                raise FirewallError(errors.INVALID_PORT, self.element.port)	
-            if self.element.protocol not in [ "tcp", "udp", "sctp", "dccp" ]:	
+        elif type(self.element) == Rich_SourcePort:
+            if not functions.check_port(self.element.port):
+                raise FirewallError(errors.INVALID_PORT, self.element.port)
+            if self.element.protocol not in [ "tcp", "udp", "sctp", "dccp" ]:
                 raise FirewallError(errors.INVALID_PROTOCOL, self.element.protocol)
-        
+
         # tcp-mss-clamp
         elif type(self.element) == Rich_Tcp_Mss_Clamp:
+            if self.action is not None:
+                raise FirewallError(errors.INVALID_RULE, "tcp-mss-clamp and %s are mutually exclusive" % self.action)
             if self.element.value:
                 if not functions.checkTcpMssClamp(self.element.value):
                     raise FirewallError(errors.INVALID_RULE, self.element.value)
-        
-        # other element and not empty?	
-        elif self.element is not None:	
-            raise FirewallError(errors.INVALID_RULE, "Unknown element %s" % 	
-                                type(self.element))	
+
+        # other element and not empty?
+        elif self.element is not None:
+            raise FirewallError(errors.INVALID_RULE, "Unknown element %s" % 
+                                type(self.element))
 
         # log
         if self.log is not None:

src/firewall/functions.py
@@ -351,10 +351,12 @@ def checkTcpMssClamp(tcp_mss_clamp_value):
     if tcp_mss_clamp_value:
         if tcp_mss_clamp_value.isdigit():
             if int(tcp_mss_clamp_value) < 536:
-                return False                
+                return False
+        elif tcp_mss_clamp_value == "None":
+            return True
         elif tcp_mss_clamp_value != "pmtu":
             return False
-    return True 
+    return True
 
 def checkInterface(iface):
     """ Check interface string

commit 27c2f07e67648d1346fdcb9625a620b3718455e2

fix(core): fix exception while parsing invalid "tcp-mss-clamp" in policy
Otherwise, having a policy like

  <?xml version="1.0" encoding="utf-8"?>
  <policy priority="100" target="ACCEPT">
    <ingress-zone name="FedoraServer"/>
    <egress-zone name="external"/>
    <tcp-mss-clamp/>
  </policy>

results in a crash:

  Aug 08 14:22:27 7291245c7ebc firewalld[58363]: Traceback (most recent call last):
                                                   File "/usr/lib/python3.11/site-packages/firewall/server/decorators.py", line 64, in _impl
                                                     return func(*args, **kwargs)
                                                            ^^^^^^^^^^^^^^^^^^^^^
                                                   File "/usr/lib/python3.11/site-packages/firewall/server/firewalld.py", line 320, in reload
                                                     self.fw.reload()
                                                   File "/usr/lib/python3.11/site-packages/firewall/core/fw.py", line 1127, in reload
                                                     check_on_disk_config(self)
                                                   File "/usr/lib/python3.11/site-packages/firewall/core/io/functions.py", line 90, in check_on_disk_config
                                                     obj = readers[reader]["reader"](file, _dir)
                                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                                                   File "/usr/lib/python3.11/site-packages/firewall/core/io/policy.py", line 1073, in policy_reader
                                                     parser.parse(source)
                                                   File "/usr/lib64/python3.11/xml/sax/expatreader.py", line 111, in parse
                                                     xmlreader.IncrementalParser.parse(self, source)
                                                   File "/usr/lib64/python3.11/xml/sax/xmlreader.py", line 125, in parse
                                                     self.feed(buffer)
                                                   File "/usr/lib64/python3.11/xml/sax/expatreader.py", line 217, in feed
                                                     self._parser.Parse(data, isFinal)
                                                   File "/builddir/build/BUILD/Python-3.11.4/Modules/pyexpat.c", line 416, in StartElement
                                                   File "/usr/lib64/python3.11/xml/sax/expatreader.py", line 333, in start_element
                                                     self._cont_handler.startElement(name, AttributesImpl(attrs))
                                                   File "/usr/lib/python3.11/site-packages/firewall/core/io/policy.py", line 991, in startElement
                                                     if common_startElement(self, name, attrs):
                                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                                                   File "/usr/lib/python3.11/site-packages/firewall/core/io/policy.py", line 114, in common_startElement
                                                     attrs["value"])
                                                     ~~~~~^^^^^^^^^
                                                   File "/usr/lib64/python3.11/xml/sax/xmlreader.py", line 318, in __getitem__
                                                     return self._attrs[name]
                                                            ~~~~~~~~~~~^^^^^^
                                                 KeyError: 'value'
  Aug 08 14:22:29 7291245c7ebc firewalld[58363]: DEBUG1: Loading policy file '/usr/lib/firewalld/policies/allow-host-ipv6.xml'
  Aug 08 14:22:29 7291245c7ebc firewalld[58363]: ERROR: Failed to load policy file 'allow-host-ipv6.xml': 'value'

https://lists.fedorahosted.org/archives/list/firewalld-users@lists.fedorahosted.org/message/7J423T2P5R3Y6ASNCN4HDPVHZUVHSYGD/

Fixes: 3f9393741cce

src/firewall/core/io/policy.py
@@ -110,8 +110,10 @@ def common_startElement(obj, name, attrs):
                 _value = attrs["value"]
             obj._rule.element = rich.Rich_Tcp_Mss_Clamp(_value)
         else:
-            log.warning("Invalid rule: tcp-mss-clamp '%s' outside of rule",
-                        attrs["value"])
+            s = ""
+            if "value" in attrs:
+                s = f" (value='{attrs['value']})'"
+            log.warning("Invalid rule: tcp-mss-clamp%s outside of rule", s)
 
     elif name == "icmp-block":
         if obj._rule: