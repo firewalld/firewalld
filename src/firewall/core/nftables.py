ba28e22ef44648f9389e1df088178641813c48a9
Eric Garver <eric@garver.life>
2023-08-10 15:52:02 -0400

    chore(nftables): remove created_tables dict
    The dict created_tables is no longer used.
    
    (cherry picked from commit 724da361171f518c73efaa898db082a743b0d310)

--- src/firewall/core/nftables.py
+++ src/firewall/core/nftables.py
@@ -171,7 +171,6 @@ class nftables(object):
         self.rich_rule_priority_counts = {}
         self.policy_priority_counts = {}
         self.zone_source_index_cache = {}
-        self.created_tables = {"inet": []}
 
         self.nftables = Nftables()
         self.nftables.set_echo_output(True)
@@ -421,9 +420,6 @@ class nftables(object):
         self.policy_priority_counts = {}
         self.zone_source_index_cache = {}
 
-        if TABLE_NAME in self.created_tables["inet"]:
-            self.created_tables["inet"].remove(TABLE_NAME)
-
         return self._build_delete_table_rules(TABLE_NAME)
 
     def _build_set_policy_rules_ct_rules(self, enable):
@@ -447,7 +443,6 @@ class nftables(object):
         if policy == "PANIC":
             rules.append({"add": {"table": {"family": "inet",
                                             "name": TABLE_NAME_POLICY}}})
-            self.created_tables["inet"].append(TABLE_NAME_POLICY)
 
             # Use "raw" priority for panic mode. This occurs before
             # conntrack, mangle, nat, etc
@@ -462,7 +457,6 @@ class nftables(object):
         elif policy == "DROP":
             rules.append({"add": {"table": {"family": "inet",
                                             "name": TABLE_NAME_POLICY}}})
-            self.created_tables["inet"].append(TABLE_NAME_POLICY)
 
             # To drop everything except existing connections we use
             # "filter" because it occurs _after_ conntrack.
@@ -483,9 +477,6 @@ class nftables(object):
                     rules.append(rule)
 
             rules += self._build_delete_table_rules(TABLE_NAME_POLICY)
-
-            if TABLE_NAME_POLICY in self.created_tables["inet"]:
-                self.created_tables["inet"].remove(TABLE_NAME_POLICY)
         else:
             raise FirewallError(UNKNOWN_ERROR, "not implemented")
 
@@ -505,7 +496,6 @@ class nftables(object):
         default_tables = []
         default_tables.append({"add": {"table": {"family": "inet",
                                                  "name": TABLE_NAME}}})
-        self.created_tables["inet"].append(TABLE_NAME)
         return default_tables
 
     def build_default_rules(self, log_denied="off"):
