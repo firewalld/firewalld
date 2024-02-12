firewalld/firewalld

commit 555ae1307a3e27a325862ce02038158cff0df4b2

New rich language usable in zones
The rich language extends the currect zone elements (service, port, icmp-block,
masquerade and forward-port) with additional source and destination addresses,
logging, actions and limits for logs and actions.

The use of destination addresses is not possible with all elements. This
depends on the use of destination addresses for example in services.

Logging can be done with with the LOG and also with the AUDIT target.
A new chain is added to all zones: <zone>_log. This will be jumped into
before the deny chain to be able to have a proper ordering.

Examples:

  <rule>
    <protocol value="ah"/>
    <accept/>
  </rule>

  <rule>
    <service name="ftp"/>
    <audit type="ACCEPT">
      <limit value="1/m"/>
    </audit>
    <accept/>
  </rule>

  <rule family="ipv4">
    <source address="192.168.0.0/24"/>
    <service name="tftp"/>
    <log prefix="tftp" level="info">
      <limit value="1/m"/>
    </log>
    <accept/>
  </rule>

  <rule family="ipv6">
    <source address="1:2:3:4:6::"/>
    <service name="radius"/>
    <log prefix="dns" level="info">
      <limit value="3/m"/>
    </log>
    <reject>
      <limit value="20/m"/>
    </reject>
  </rule>

  <rule family="ipv6">
    <source address="1:2:3:4:6::"/>
    <forward-port to-addr="1::2:3:4:7" to-port="4012" protocol="tcp" port="4011"/>
  </rule>

New files:

* src/firewall/core/rich.py

New functions:

* src/firewall/core/fw.py
  - handle_rules2 - handle rules where table and chain are provided extra
  - check_address - checks ipv4 and ipv4 addresses and ranges

* src/firewall/core/fw_zone.py
  - handle_cmr - handle chains, modules and rules for a zone
  - check_rule - check if rules is valid
  - __rule_id - creates unique id for rule
  - __rule_source - adds source args to the command
  - __rule_destination - adds destination args to the command
  - __rule_limit - adds limit args to the command
  - __rule_log - duplicates command and adds log args to the command,

src/firewall/core/rich.py
@@ -1,0 +1,228 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Red Hat, Inc.
+#
+# Authors:
+# Thomas Woerner <twoerner@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from firewall.core.logger import log
+
+class Rich_Source(object):
+    def __init__(self, addr, invert=False):
+        self.addr = addr
+        self.invert = invert
+
+    def __str__(self):
+        return 'source %saddress="%s"' % ("NOT " if self.invert else "",
+                                          self.addr)
+
+class Rich_Destination(Rich_Source):
+    def __str__(self):
+        return 'destination %saddress="%s"' % ("NOT " if self.invert else "",
+                                               self.addr)
+
+class Rich_Service(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'service name="%s"' % (self.name)
+
+class Rich_Port(object):
+    def __init__(self, port, protocol):
+        self.port = port
+        self.protocol = protocol
+
+    def __str__(self):
+        return 'port port="%s" protocol="%s"' % (self.port, self.protocol)
+
+class Rich_Protocol(object):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return 'protocol value="%s"' % (self.value)
+
+class Rich_Masquerade(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'masquerade'
+
+class Rich_IcmpBlock(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'icmp-block name="%s"' % (self.name)
+
+class Rich_ForwardPort(object):
+    def __init__(self, port, protocol, to_port, to_address):
+        self.port = port
+        self.protocol = protocol
+        self.to_port = to_port
+        self.to_address = to_address
+
+    def __str__(self):
+        return 'forward-port port="%s" protocol="%s"%s%s' % \
+            (self.port, self.protocol,
+             ' to-port="%s"' % self.to_port if self.to_port != "" else '',
+             ' to-address="%s"' % self.to_address if self.to_address != "" else '')
+
+class Rich_Log(object):
+    def __init__(self, prefix=None, level=None):
+        #TODO check default level in iptables
+        self.prefix = prefix
+        self.level = level
+        self.limit = None
+
+    def __str__(self):
+        return 'log%s%s%s' % \
+            (' prefix="%s"' % (self.prefix) if self.prefix else "",
+             ' level="%s"' % (self.level) if self.level else "",
+             " { %s }" % self.limit if self.limit else "")
+
+class Rich_Audit(object):
+    def __init__(self, _type):
+        #TODO check default level in iptables
+        self.type = str(_type)
+        self.limit = None
+
+    def __str__(self):
+        return 'audit type="%s"%s' % \
+            (self.type, " { %s }" % self.limit if self.limit else "")
+
+class Rich_Accept(object):
+    def __init__(self):
+        self.limit = None
+
+    def __str__(self):
+        return "accept%s" % (" { %s }" % self.limit if self.limit else "")
+
+class Rich_Reject(object):
+    def __init__(self, _type=None):
+        self.type = _type
+        self.limit = None
+
+    def __str__(self):
+        return "reject%s%s" % (' type="%s"' if self.type else "",
+                               " { %s }" % self.limit if self.limit else "")
+
+class Rich_Drop(Rich_Accept):
+    def __str__(self):
+        return "drop%s" % (" { %s }" % self.limit if self.limit else "")
+
+
+class Rich_Limit(object):
+    def __init__(self, value):
+        self.value = value
+
+    def check(self):
+        splits = None
+        if "/" in self.value:
+            splits = value.split("/")
+        if not splits or len(splits) != 2:
+            raise FirewallError(INVALID_LIMIT,
+                                "Invalid value (rate/duration): %s" % 
+                                self.value)
+        (rate, duration) = splits
+        try:
+            rate = int(rate)
+        except:
+            raise FirewallError(INVALID_LIMIT, "Invalid rate: %s" % rate)
+        if rate < 0:
+            raise FirewallError(INVALID_LIMIT, "Invalid rate: %s" % rate)
+
+        if duration not in [ "s", "m", "h", "d" ]:
+            raise FirewallError(INVALID_LIMIT, "Invalid duration: %s" %
+                                duration)
+
+        mult = 1
+        if self.duration == "s":
+            mult = 1
+        elif self.duration == "m":
+            mult = 60
+        elif self.duration == "h":
+            mult = 60*60
+        elif self.duration == "d":
+            mult = 24*60*60
+
+        if 10000 * mult / r == 0:
+            raise FirewallError(INVALID_LIMIT,
+                                "Limit rate/duration too fast %d/%s" % \
+                                    (rate, duration))
+
+    def __str__(self):
+        return 'limit rate="%s"' % (self.value)
+
+    def command(self):
+        return ''
+
+class Rich_Rule(object):
+    def __init__(self, family=None):
+        if family != None:
+            self.family = str(family)
+        else:
+            self.family = None
+
+        self.source = None
+        self.destination = None
+        self.element = None
+        self.log = None
+        self.audit = None
+        self.action = None
+
+    def check(self):
+        if self.family != None and self.family not in [ "ipv4", "ipv6" ]:
+            log.error('Invalid family "%s"' % self.family)
+            return False
+
+        if self.log == None and self.audit == None and \
+                self.action == None and \
+                type(self.element) not in [ Rich_IcmpBlock,
+                                            Rich_ForwardPort,
+                                            Rich_Masquerade ]:
+            log.error("Invalid rule: %s: No action, log or audit" % \
+                          str(self._rule))
+            return False
+
+        return True
+
+    def __str__(self):
+        ret = "rule "
+        if self.family:
+            ret += 'family="%s" ' % self.family
+        ret += "{ "
+        if self.source:
+            ret += "%s " % self.source
+        if self.destination:
+            ret += "%s " % self.destination
+        ret += "%s " % self.element
+        if self.log:
+            ret += "%s " % self.log
+        if self.audit:
+            ret += "%s " % self.audit
+        if self.action:
+            ret += "%s " % self.action
+        ret += "}"
+
+        return ret
+
+#class Rich_RawRule(object):
+#class Rich_RuleSet(object):
+#class Rich_AddressList(object):

src/firewall/core/fw.py
@@ -292,6 +292,34 @@ class Firewall:
                 return (rules[:i], msg) # cleanup rules and error message
         return None
 
+    def handle_rules2(self, rules, enable, insert=False):
+        if insert:
+            append_delete = { True: "-I", False: "-D", }
+        else:
+            append_delete = { True: "-A", False: "-D", }
+
+        # appends rules
+        # returns None if all worked, else (cleanup rules, error message)
+        i = 0
+        for i in xrange(len(rules)):
+            if len(rules[i]) == 5:
+                (ipv, table, chain, rule, insert) = rules[i]
+            else:
+                (ipv, table, chain, rule) = rules[i]
+
+            # drop insert rule number if it exists
+            if insert and not enable and isinstance(rule[1], int):
+                rule.pop(1)
+
+            # run
+            try:
+                self.rule(ipv, [ "-t", table,
+                                 append_delete[enable], chain, ] + rule)
+            except Exception as msg:
+                log.error(msg)
+                return (rules[:i], msg) # cleanup rules and error message
+        return None
+
     def handle_chains(self, rules, enable):
         new_delete = { True: "-N", False: "-X" }
 
@@ -453,6 +481,16 @@ class Firewall:
     def check_ip(self, ip):
         if not functions.checkIP(ip):
             raise FirewallError(INVALID_ADDR, ip)
+
+    def check_address(self, ipv, source):
+        if ipv == "ipv4":
+            if not functions.checkIPnMask(source):
+                raise FirewallError(INVALID_ADDR, source)
+        elif ipv == "ipv6":
+            if not functions.checkIP6nMask(source):
+                raise FirewallError(INVALID_ADDR, source)
+        else:
+            raise FirewallError(INVALID_IPV)
 
     def check_icmptype(self, icmp):
         self.icmptype.check_icmptype(icmp)

commit 5e585ba2f092389b2a8fec17e3cef1ab57b49045

fix(rich): fix range check for large rule limit
Fixes: 555ae1307a3e ('New rich language usable in zones')

src/firewall/core/rich.py
@@ -458,7 +458,7 @@ class Rich_Limit(_Rich_Entry):
         elif duration == "d":
             mult = 24 * 60 * 60
 
-        if 10000 * mult / rate == 0:
+        if 10000 * mult // rate == 0:
             raise FirewallError(errors.INVALID_LIMIT, "%s too fast" % self.value)
 
         if rate == 1 and duration == "d":

src/tests/unit/test_rich_rule.py
@@ -99,6 +99,7 @@ EXP_RULE_LIST = [
     ),
     ExpRule(
         'rule family="ipv6" source address="1:2:3:4:6::" port port="4011" protocol="tcp" log prefix="port 4011: " level="info" limit value="4000000/m" drop',
+        invalid=True,
     ),
     ExpRule(
         'rule family="ipv6" source address="1:2:3:4:6::" forward-port port="4011" protocol="tcp" to-port="4012" to-addr="1::2:3:4:7"'