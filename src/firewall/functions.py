firewalld/firewalld

commit 160ac584aea2ce8de2c10c5db687ef7c5bbda961

improvement(functions): add wrong_args_for_callable() helper

src/firewall/functions.py
@@ -657,3 +657,16 @@ def joinArgs(args):
 
 def splitArgs(_string):
     return shlex.split(_string)
+
+
+def wrong_args_for_callable(fcn, *a, **kw):
+    import inspect
+
+    # Check whether fcn(*a, **kw) will fail due to invalid
+    # arguments.
+
+    try:
+        inspect.bind(fcn, *a, **kw)
+    except TypeError:
+        return False
+    return True

commit 1fda8ab2d6a5ead87baf8608aa987608e19fd6fb

fix(functions): fix functions.wrong_args_for_callable()
This was totally bogus.

https://github.com/firewalld/firewalld/issues/1310

Fixes: 160ac584aea2 ('improvement(functions): add wrong_args_for_callable() helper')

src/firewall/functions.py
@@ -665,8 +665,10 @@ def wrong_args_for_callable(fcn, *a, **kw):
     # Check whether fcn(*a, **kw) will fail due to invalid
     # arguments.
 
+    sig = inspect.signature(fcn)
+
     try:
-        inspect.bind(fcn, *a, **kw)
+        sig.bind(*a, **kw)
     except TypeError:
         return False
     return True