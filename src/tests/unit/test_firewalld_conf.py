firewalld/firewalld

commit 0019371a8f42d376ac9cce79cc5e1e7d2049f021

feat(fw): add ReloadPolicy option in firewalld.conf
One interesting aspect is that during `firewall-cmd --reload`, the code
first sets the policy to "DROP", before reloading "firewalld.conf". That
means, changing the value only takes effect after the next reload. But
that seems expected as we set the policy before starting to reload.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2149039

src/tests/unit/test_firewalld_conf.py
@@ -1,0 +1,68 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import firewall.core.io.firewalld_conf
+import firewall.config
+
+
+def test_reload_policy():
+    def t(value, expected_valid=True, **kw):
+
+        expected = {
+            "INPUT": "DROP",
+            "FORWARD": "DROP",
+            "OUTPUT": "DROP",
+        }
+        for k, v in kw.items():
+            assert k in expected
+            expected[k] = v
+
+        try:
+            parsed = (
+                firewall.core.io.firewalld_conf.firewalld_conf._parse_reload_policy(
+                    value
+                )
+            )
+        except ValueError:
+            assert not expected_valid
+            return
+
+        assert parsed == expected
+        assert expected_valid
+
+        unparsed = (
+            firewall.core.io.firewalld_conf.firewalld_conf._unparse_reload_policy(
+                parsed
+            )
+        )
+        parsed2 = firewall.core.io.firewalld_conf.firewalld_conf._parse_reload_policy(
+            unparsed
+        )
+        assert parsed2 == parsed
+
+    t(None)
+    t("")
+    t("  ")
+    t(" input: ACCept ", INPUT="ACCEPT")
+    t(
+        "forward:DROP,  forward : REJEct; input: ACCept ",
+        INPUT="ACCEPT",
+        FORWARD="REJECT",
+    )
+    t(" accept ", INPUT="ACCEPT", FORWARD="ACCEPT", OUTPUT="ACCEPT")
+    t("REJECT", INPUT="REJECT", FORWARD="REJECT", OUTPUT="REJECT")
+    t("forward=REJECT", FORWARD="REJECT")
+    t("forward=REJECT , input=accept", FORWARD="REJECT", INPUT="ACCEPT")
+    t("forward=REJECT , xinput=accept", expected_valid=False)
+    t("forward=REJECT, ACCEPT", expected_valid=False)
+
+    def _norm(reload_policy):
+        parsed = firewall.core.io.firewalld_conf.firewalld_conf._parse_reload_policy(
+            reload_policy
+        )
+        return firewall.core.io.firewalld_conf.firewalld_conf._unparse_reload_policy(
+            parsed
+        )
+
+    assert firewall.config.FALLBACK_RELOAD_POLICY == _norm(
+        firewall.config.FALLBACK_RELOAD_POLICY
+    )

config/firewalld.conf
@@ -66,6 +66,14 @@ FirewallBackend=nftables
 # Default: yes
 FlushAllOnReload=yes
 
+# ReloadPolicy
+# Policy during reload. By default all traffic except for established
+# connections is dropped while the rules are updated. Set to "DROP", "REJECT"
+# or "ACCEPT". Alternatively, specify it per table, like
+# "OUTPUT:ACCEPT,INPUT:DROP,FORWARD:REJECT".
+# Default: ReloadPolicy=INPUT:DROP,FORWARD:DROP,OUTPUT:DROP
+ReloadPolicy=INPUT:DROP,FORWARD:DROP,OUTPUT:DROP
+
 # RFC3964_IPv4
 # As per RFC 3964, filter IPv6 traffic with 6to4 destination addresses that
 # correspond to IPv4 addresses that should not be routed over the public

doc/xml/firewalld.conf.xml
@@ -186,6 +186,22 @@
         </varlistentry>
 
         <varlistentry>
+            <term><option>ReloadPolicy</option></term>
+            <listitem>
+                <para>
+                    The policy during reload. By default, all traffic except
+                    established connections is dropped while reloading the
+                    firewall rules. This can be overridden for INPUT, FORWARD
+                    and OUTPUT. The accepted values are "DROP", "REJECT" and
+                    "ACCEPT", which then applies to all tables. Alternatively,
+                    the policy can be specified per table, like
+                    "INPUT:REJECT,FORWARD:DROP,OUTPUT:ACCEPT".
+                    Defaults to "INPUT:DROP,FORWARD:DROP,OUTPUT:DROP".
+                </para>
+            </listitem>
+        </varlistentry>
+
+        <varlistentry>
             <term><option>RFC3964_IPv4</option></term>
             <listitem>
                 <para>

src/firewall/config/__init__.py.in
@@ -125,6 +125,7 @@ FALLBACK_LOG_DENIED = "off"
 FALLBACK_AUTOMATIC_HELPERS = "no"
 FALLBACK_FIREWALL_BACKEND = "nftables"
 FALLBACK_FLUSH_ALL_ON_RELOAD = True
+FALLBACK_RELOAD_POLICY = "INPUT:DROP,FORWARD:DROP,OUTPUT:DROP"
 FALLBACK_RFC3964_IPV4 = True
 FALLBACK_ALLOW_ZONE_DRIFTING = False
 FALLBACK_NFTABLES_FLOWTABLE = "off"

src/firewall/core/fw.py
@@ -1107,7 +1107,13 @@ class Firewall:
         else:
             transaction = use_transaction
 
-        log.debug1("Setting policy to '%s'", policy)
+        log.debug1(
+            "Setting policy to '%s'%s",
+            policy,
+            f" (ReloadPolicy={firewalld_conf._unparse_reload_policy(policy_details)})"
+            if policy == "DROP"
+            else "",
+        )
 
         for backend in self.enabled_backends():
             rules = self._set_policy_build_rules(backend, policy, policy_details)
@@ -1263,7 +1269,10 @@ class Firewall:
             _ipset_objs.append(self.ipset.get_ipset(_name))
 
         if not _panic:
-            self.set_policy("DROP")
+            reload_policy = firewalld_conf._parse_reload_policy(
+                self._firewalld_conf.get("ReloadPolicy")
+            )
+            self.set_policy("DROP", policy_details=reload_policy)
 
         self.flush()
         self.cleanup()

src/firewall/core/io/firewalld_conf.py
@@ -25,6 +25,7 @@ valid_keys = [
     "AutomaticHelpers",
     "FirewallBackend",
     "FlushAllOnReload",
+    "ReloadPolicy",
     "RFC3964_IPv4",
     "AllowZoneDrifting",
     "NftablesFlowtable",
@@ -81,6 +82,7 @@ class firewalld_conf:
         self.set(
             "FlushAllOnReload", "yes" if config.FALLBACK_FLUSH_ALL_ON_RELOAD else "no"
         )
+        self.set("ReloadPolicy", config.FALLBACK_RELOAD_POLICY)
         self.set("RFC3964_IPv4", "yes" if config.FALLBACK_RFC3964_IPV4 else "no")
         self.set(
             "AllowZoneDrifting", "yes" if config.FALLBACK_ALLOW_ZONE_DRIFTING else "no"
@@ -247,6 +249,17 @@ class firewalld_conf:
                 )
             self.set("FlushAllOnReload", str(config.FALLBACK_FLUSH_ALL_ON_RELOAD))
 
+        value = self.get("ReloadPolicy")
+        try:
+            value = self._parse_reload_policy(value)
+        except ValueError:
+            log.warning(
+                "ReloadPolicy '%s' is not valid, using default value '%s'",
+                value,
+                config.FALLBACK_RELOAD_POLICY,
+            )
+            self.set("ReloadPolicy", config.FALLBACK_RELOAD_POLICY)
+
         value = self.get("RFC3964_IPv4")
         if not value or value.lower() not in ["yes", "true", "no", "false"]:
             if value is not None:
@@ -381,3 +394,45 @@ class firewalld_conf:
             raise IOError("Failed to create '%s': %s" % (self.filename, msg))
         else:
             os.chmod(self.filename, 0o600)
+
+    @staticmethod
+    def _parse_reload_policy(value):
+        valid = True
+        result = {
+            "INPUT": "DROP",
+            "FORWARD": "DROP",
+            "OUTPUT": "DROP",
+        }
+        if value:
+            value = value.strip()
+            v = value.upper()
+            if v in ("ACCEPT", "REJECT", "DROP"):
+                for k in result:
+                    result[k] = v
+            else:
+                for a in value.replace(";", ",").split(","):
+                    a = a.strip()
+                    if not a:
+                        continue
+                    a2 = a.replace("=", ":").split(":", 2)
+                    if len(a2) != 2:
+                        valid = False
+                        continue
+                    k = a2[0].strip().upper()
+                    if k not in result:
+                        valid = False
+                        continue
+                    v = a2[1].strip().upper()
+                    if v not in ("ACCEPT", "REJECT", "DROP"):
+                        valid = False
+                        continue
+                    result[k] = v
+
+        if not valid:
+            raise ValueError("Invalid ReloadPolicy")
+
+        return result
+
+    @staticmethod
+    def _unparse_reload_policy(value):
+        return ",".join(f"{k}:{v}" for k, v in value.items())

src/tests/features/features.at
@@ -23,3 +23,4 @@ m4_include([features/iptables_no_flush_on_shutdown.at])
 m4_include([features/zone_priority.at])
 m4_include([features/nftables_flowtable.at])
 m4_include([features/nftables_counters.at])
+m4_include([features/reloadpolicy.at])

src/tests/features/reloadpolicy.at
@@ -1,0 +1,12 @@
+FWD_START_TEST([check ReloadPolicy])
+AT_KEYWORDS(reloadpolicy rhbz2149039)
+
+AT_CHECK([sed -i 's/^ReloadPolicy=.*/ReloadPolicy=INPUT:REJECT,FORWARD:ACCEPT/' ./firewalld.conf])
+dnl call RELOAD twice, to see more action about the ReloadPolicy.
+FWD_RELOAD()
+FWD_RELOAD()
+
+AT_CHECK([sed -i 's/^ReloadPolicy=.*/ReloadPolicy=REJECT/' ./firewalld.conf])
+FWD_RELOAD()
+
+FWD_END_TEST()

commit 2948e8596dab6efded3ba591d9262fa132e310b2

fix(config): expose "ReloadPolicy" on D-Bus
Fixes: 0019371a8f42 ('feat(fw): add ReloadPolicy option in firewalld.conf')

src/firewall/server/config.py
@@ -84,6 +84,7 @@ CONFIG_PROPERTIES = {
     "AllowZoneDrifting": ConfigPropertiesTuple("readwrite", True, True, dbus.String),
     "NftablesFlowtable": ConfigPropertiesTuple("readwrite", False, False, dbus.String),
     "NftablesCounters": ConfigPropertiesTuple("readwrite", False, False, dbus.String),
+    "ReloadPolicy": ConfigPropertiesTuple("read", False, False, dbus.String),
 }
 
 

src/tests/dbus/firewalld.conf.at
@@ -30,6 +30,7 @@ string "MinimalMark" : variant int32 100
 string "NftablesCounters" : variant string "no"
 string "NftablesFlowtable" : variant string "off"
 string "RFC3964_IPv4" : variant string "yes"
+string "ReloadPolicy" : variant string "INPUT:DROP,FORWARD:DROP,OUTPUT:DROP"
 ])
 
 m4_define([_helper], [

src/tests/unit/test_firewalld_conf.py
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-import firewall.core.io.firewalld_conf
 import firewall.config
+import firewall.core.io.firewalld_conf
+import firewall.server.config
 
 
 def get_valid_key(key):
@@ -94,6 +95,10 @@ def test_config_keys():
             assert type(keytype._default) is str
 
         assert keytype.default == keytype.normalize(keytype._default, strict=True)
+
+    assert sorted(firewall.server.config.CONFIG_PROPERTIES.keys()) == sorted(
+        firewall.core.io.firewalld_conf.valid_keys
+    )
 
 
 def test_config_normalize():