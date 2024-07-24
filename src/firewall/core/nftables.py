firewalld/firewalld

commit becd083fc2905921651af73cb15ce8c9aba9203b

feat(nftables): table ownership
This allows using the nftables table flags "owner" and "persist". When
these are in use ONLY firewalld will be able to change the firewalld
table/rules. All other processes will be blocked. This enhances
firewalld robustness by guaranteeing that firewalld's rules can not be
modified without it's knowledge, e.g. by other entities.

This also allows firewalld and nftables services to coexist. An "nft
flush ruleset" will NOT flush the "firewalld" table.

Fixes: RHEL-17002

src/firewall/core/nftables.py
@@ -42,6 +42,7 @@ from nftables.nftables import Nftables
 
 TABLE_NAME = "firewalld"
 TABLE_NAME_POLICY = TABLE_NAME + "_" + "policy_drop"
+TABLE_NAME_PROBE = TABLE_NAME + "_" + "probe"
 POLICY_CHAIN_PREFIX = "policy_"
 
 # Map iptables (table, chain) to hooks and priorities.
@@ -227,6 +228,7 @@ class nftables:
     def __init__(self, fw):
         self._fw = fw
         self.restore_command_exists = True
+        self.supports_table_owner = False
         self.available_tables = []
         self.rule_to_handle = {}
         self.rule_ref_count = {}
@@ -236,6 +238,57 @@ class nftables:
         self.nftables = Nftables()
         self.nftables.set_echo_output(True)
         self.nftables.set_handle_output(True)
+
+    def _probe_support_table_owner(self):
+        try:
+            rules = {
+                "nftables": [
+                    {"metainfo": {"json_schema_version": 1}},
+                    {
+                        "add": {
+                            "table": {
+                                "family": "inet",
+                                "name": TABLE_NAME_PROBE,
+                                "flags": ["owner", "persist"],
+                            }
+                        }
+                    },
+                ]
+            }
+
+            rc, output, _ = self.nftables.json_cmd(rules)
+            if rc:
+                raise ValueError("nftables probe table owner failed")
+
+            # old nftables versions would ignore table flags in JSON, so we
+            # must parse back and verify the flags are set.
+            rules = {
+                "nftables": [
+                    {"metainfo": {"json_schema_version": 1}},
+                    {"list": {"table": {"family": "inet", "name": TABLE_NAME_PROBE}}},
+                ]
+            }
+            self.nftables.set_echo_output(False)
+            rc, output, _ = self.nftables.json_cmd(rules)
+            self.nftables.set_echo_output(True)
+            flags = output["nftables"][1]["table"]["flags"]
+
+            self.set_rule(
+                {"delete": {"table": {"family": "inet", "name": TABLE_NAME_PROBE}}},
+                self._fw.get_log_denied(),
+            )
+
+            if "owner" not in flags or "persist" not in flags:
+                raise ValueError("nftables probe table owner failed")
+
+            log.debug2("nftables: probe_support(): owner flag is supported.")
+            self.supports_table_owner = True
+        except:
+            log.debug2("nftables: probe_support(): owner flag is NOT supported.")
+            self.supports_table_owner = False
+
+    def probe_support(self):
+        self._probe_support_table_owner()
 
     def _set_rule_sort_policy_dispatch(self, rule, policy_dispatch_index_cache):
         for verb in ["add", "insert", "delete"]:
@@ -499,14 +552,25 @@ class nftables:
         # Tables always exist in nftables
         return [table] if table else IPTABLES_TO_NFT_HOOK.keys()
 
+    def _build_add_table_rules(self, table):
+        rule = {"add": {"table": {"family": "inet", "name": table}}}
+
+        if (
+            table == TABLE_NAME
+            and self._fw._nftables_table_owner
+            and self.supports_table_owner
+        ):
+            rule["add"]["table"]["flags"] = ["owner", "persist"]
+
+        return [rule]
+
     def _build_delete_table_rules(self, table):
         # To avoid nftables returning ENOENT we always add the table before
         # deleting to guarantee it will exist.
         #
         # In the future, this add+delete should be replaced with "destroy", but
         # that verb is too new to rely upon.
-        return [
-            {"add": {"table": {"family": "inet", "name": table}}},
+        return self._build_add_table_rules(table) + [
             {"delete": {"table": {"family": "inet", "name": table}}},
         ]
 
@@ -546,9 +610,7 @@ class nftables:
         # a higher priority than our base chains is sufficient.
         rules = []
         if policy == "PANIC":
-            rules.append(
-                {"add": {"table": {"family": "inet", "name": TABLE_NAME_POLICY}}}
-            )
+            rules.extend(self._build_add_table_rules(TABLE_NAME_POLICY))
 
             # Use "raw" priority for panic mode. This occurs before
             # conntrack, mangle, nat, etc
@@ -569,9 +631,7 @@ class nftables:
                     }
                 )
         elif policy == "DROP":
-            rules.append(
-                {"add": {"table": {"family": "inet", "name": TABLE_NAME_POLICY}}}
-            )
+            rules.extend(self._build_add_table_rules(TABLE_NAME_POLICY))
 
             # To drop everything except existing connections we use
             # "filter" because it occurs _after_ conntrack.
@@ -638,11 +698,7 @@ class nftables:
         return list(supported)
 
     def build_default_tables(self):
-        default_tables = []
-        default_tables.append(
-            {"add": {"table": {"family": "inet", "name": TABLE_NAME}}}
-        )
-        return default_tables
+        return self._build_add_table_rules(TABLE_NAME)
 
     def build_default_rules(self, log_denied="off"):
         default_rules = []

config/firewalld.conf
@@ -101,3 +101,11 @@ NftablesFlowtable=off
 # debugging and comes with a small performance cost.
 # Defaults to "no".
 NftablesCounters=no
+
+# NftablesTableOwner
+# If set to yes, the generated nftables rule set will be owned exclusively by
+# firewalld. This prevents other entities from mistakenly (or maliciously)
+# modifying firewalld's rule set. If you intentionally modify firewalld's
+# rules, then you will have to set this to "no".
+# Defaults to "yes".
+NftablesTableOwner=yes

doc/xml/firewalld.conf.xml
@@ -253,6 +253,19 @@
             </listitem>
         </varlistentry>
 
+        <varlistentry>
+            <term><option>NftablesTableOwner</option></term>
+            <listitem>
+                <para>
+                  If set to yes, the generated nftables rule set will be owned exclusively by
+                  firewalld. This prevents other entities from mistakenly (or maliciously)
+                  modifying firewalld's rule set. If you intentionally modify firewalld's
+                  rules, then you will have to set this to "no".
+                  Defaults to "yes".
+                </para>
+            </listitem>
+        </varlistentry>
+
     </variablelist>
 
   </refsect1>

doc/xml/firewalld.dbus.xml
@@ -2604,6 +2604,17 @@
               </para>
             </listitem>
           </varlistentry>
+          <varlistentry id="FirewallD1.config.Properties.NftablesTableOwner">
+            <term>NftablesTableOwner - s - (rw)</term>
+            <listitem>
+              <para>
+                If set to yes, the generated nftables rule set will be owned exclusively by
+                firewalld. This prevents other entities from mistakenly (or maliciously)
+                modifying firewalld's rule set. If you intentionally modify firewalld's
+                rules, then you will have to set this to "no".
+              </para>
+            </listitem>
+          </varlistentry>
         </variablelist>
       </refsect3>
     </refsect2>

src/firewall/config/__init__.py.in
@@ -138,3 +138,4 @@ FALLBACK_RFC3964_IPV4 = True
 FALLBACK_ALLOW_ZONE_DRIFTING = False
 FALLBACK_NFTABLES_FLOWTABLE = "off"
 FALLBACK_NFTABLES_COUNTERS = False
+FALLBACK_NFTABLES_TABLE_OWNER = True

src/firewall/core/fw.py
@@ -114,6 +114,7 @@ class Firewall:
         self._allow_zone_drifting = config.FALLBACK_ALLOW_ZONE_DRIFTING
         self._nftables_flowtable = config.FALLBACK_NFTABLES_FLOWTABLE
         self._nftables_counters = config.FALLBACK_NFTABLES_COUNTERS
+        self._nftables_table_owner = config.FALLBACK_NFTABLES_TABLE_OWNER
 
         if self._offline:
             self.ip4tables_enabled = False
@@ -376,6 +377,17 @@ class Firewall:
                 "option, will therefore not be used"
             )
 
+        self.nftables_backend.probe_support()
+
+        if (
+            self._nftables_table_owner
+            and not self.nftables_backend.supports_table_owner
+        ):
+            log.info1(
+                "Configuration has NftablesTableOwner=True, but it's "
+                "not supported by nftables. Table ownership will be disabled."
+            )
+
     def _start_load_firewalld_conf(self):
         # load firewalld config
         log.debug1("Loading firewalld config file '%s'", config.FIREWALLD_CONF)
@@ -462,6 +474,16 @@ class Firewall:
                 else:
                     self._nftables_counters = True
                 log.debug1("NftablesCounters is set to '%s'", self._nftables_counters)
+
+            if self._firewalld_conf.get("NftablesTableOwner"):
+                value = self._firewalld_conf.get("NftablesTableOwner")
+                if value.lower() in ["no", "false"]:
+                    self._nftables_table_owner = False
+                else:
+                    self._nftables_table_owner = True
+                log.debug1(
+                    "NftablesTableOwner is set to '%s'", self._nftables_table_owner
+                )
 
         self.config.set_firewalld_conf(copy.deepcopy(self._firewalld_conf))
 

src/firewall/core/io/firewalld_conf.py
@@ -30,6 +30,7 @@ valid_keys = [
     "AllowZoneDrifting",
     "NftablesFlowtable",
     "NftablesCounters",
+    "NftablesTableOwner",
     # Deprecated keys:
     "Lockdown",
 ]
@@ -91,6 +92,10 @@ class firewalld_conf:
         self.set("NftablesFlowtable", config.FALLBACK_NFTABLES_FLOWTABLE)
         self.set(
             "NftablesCounters", "yes" if config.FALLBACK_NFTABLES_COUNTERS else "no"
+        )
+        self.set(
+            "NftablesTableOwner",
+            "yes" if config.FALLBACK_NFTABLES_TABLE_OWNER else "no",
         )
 
     def sanity_check(self):

src/firewall/server/config.py
@@ -77,6 +77,7 @@ CONFIG_PROPERTIES = {
     "AllowZoneDrifting": ConfigPropertiesTuple("readwrite", True, True),
     "NftablesFlowtable": ConfigPropertiesTuple("readwrite", False, False),
     "NftablesCounters": ConfigPropertiesTuple("readwrite", False, False),
+    "NftablesTableOwner": ConfigPropertiesTuple("readwrite", False, False),
 }
 
 
@@ -664,6 +665,10 @@ class FirewallDConfig(DbusServiceObject):
             if value is None:
                 value = "yes" if config.FALLBACK_NFTABLES_COUNTERS else "no"
             return dbus.String(value)
+        elif prop == "NftablesTableOwner":
+            if value is None:
+                value = "yes" if config.FALLBACK_NFTABLES_TABLE_OWNER else "no"
+            return dbus.String(value)
         else:
             raise dbus.exceptions.DBusException(
                 "org.freedesktop.DBus.Error.InvalidArgs: "
@@ -748,6 +753,7 @@ class FirewallDConfig(DbusServiceObject):
                     "FlushAllOnReload",
                     "RFC3964_IPv4",
                     "NftablesCounters",
+                    "NftablesTableOwner",
                 ]:
                     if new_value.lower() not in ["yes", "no", "true", "false"]:
                         raise FirewallError(

commit c77bdf3f7156c4296c30b82734a781c807ef6112

fix(nftables): make sure probe table is deleted
In some cases the probe table may be left around.

Fixes: becd083fc290 ("feat(nftables): table ownership")

src/firewall/core/nftables.py
@@ -273,11 +273,6 @@ class nftables:
             self.nftables.set_echo_output(True)
             flags = output["nftables"][1]["table"]["flags"]
 
-            self.set_rule(
-                {"delete": {"table": {"family": "inet", "name": TABLE_NAME_PROBE}}},
-                self._fw.get_log_denied(),
-            )
-
             if "owner" not in flags or "persist" not in flags:
                 raise ValueError("nftables probe table owner failed")
 
@@ -286,6 +281,14 @@ class nftables:
         except:
             log.debug2("nftables: probe_support(): owner flag is NOT supported.")
             self.supports_table_owner = False
+
+        try:
+            self.set_rule(
+                {"delete": {"table": {"family": "inet", "name": TABLE_NAME_PROBE}}},
+                self._fw.get_log_denied(),
+            )
+        except:
+            pass
 
     def probe_support(self):
         self._probe_support_table_owner()